35806dce4f535f19527b3ef17b057f47228f320c
McToluene <xclusiveniggaz10@live.com>
2020-06-02 23:58:31 +0100

    HNG-01892 making my changes

---
+++ scripts/01892-mctoluene.js
@@ -0,0 +1,5 @@
+const FULL_NAME = "Ojulari Abdulhamid";
+const HNG_ID = "01892";
+const LANGUAGE = "Javascript";
+const output = `Hello World, this is ${FULL_NAME} with HNGi7 ID ${HNG_ID} using ${LANGUAGE} for stage 2 task`;
+console.log(output);
