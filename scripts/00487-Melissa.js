1fa51ef2b72d59d1dc42102d984ea69c840e4ab1
Melissa_U <mugrai@gmail.com>
2020-06-02 16:34:05 -0700

    Update variable in script to file

--- scripts/00487-Melissa.js
+++ scripts/00487-Melissa.js
@@ -3,12 +3,12 @@
 var helloWorld = {}; // create object
 
 name = "Melissa Ugrai";  /// name property
-hngId = "00487";    /// HNGi7 property
+hngId = "hng-00487";    /// HNGi7 ID property
 language = "JavaScript";   /// language property
 
 var runScript =  function()   ///run function to add string together
 {
-  console.log( "Hello World, this is " + this.name + " with HNGi7 " + this.hngId +" using " + this.language + " for Stage 2 task.");  /// console. log with string combination
+  console.log( "Hello World, this is " + this.name + " with " + this.hngId +" using " + this.language + " for Stage 2 task.");  /// console. log with string combination
 };  /// end of function
 
 runScript();  ///call function to run
