2613d7504a55ac833d03321b9d5e0a3921b68c4a
Gabrielle Kurah <49395894+Gabicle@users.noreply.github.com>
2020-06-03 00:23:04 +0100

    Update 01388-Maelle.js

--- scripts/01388-Maelle.js
+++ scripts/01388-Maelle.js
@@ -1,5 +1,5 @@
 const fullName = "Gabrielle Kurah";
-const hngID = "01388";
+const hngID = "HNG-01388";
 const language = "Javascript";
 const message =  `Hello World, this is ${fullName} with HNGi7 ID ${hngID} using ${language} for stage 2 task`;
 
