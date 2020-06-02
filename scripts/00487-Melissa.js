d4a3d210f29502f539fa81fba6312f7c2e4efc43
Melissa_U <mugrai@gmail.com>
2020-06-02 16:27:42 -0700

    Added my script to file

---
+++ scripts/00487-Melissa.js
@@ -0,0 +1,14 @@
+///Object Method
+
+var helloWorld = {}; // create object
+
+name = "Melissa Ugrai";  /// name property
+hngId = "00487";    /// HNGi7 property
+language = "JavaScript";   /// language property
+
+var runScript =  function()   ///run function to add string together
+{
+  console.log( "Hello World, this is " + this.name + " with HNGi7 " + this.hngId +" using " + this.language + " for Stage 2 task.");  /// console. log with string combination
+};  /// end of function
+
+runScript();  ///call function to run
