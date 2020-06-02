f6c2b68e971254abb9deb8ba1faa4a10bbf236fc
izaakwalz <iblack.xyz@gmail.com>
2020-06-03 00:59:12 +0100

    Added 03001-walz.js

---
+++ scripts/03001-walz.js
@@ -0,0 +1,7 @@
+const HNG_ID = '03001';
+const full_name = 'Izaak Chukwuma';
+const language = 'JavaScript';
+
+console.log(
+  `Hello World, this is ${full_name} with HNGi7 ID hng-${HNG_ID} using ${language} for stage 2 task `
+);
