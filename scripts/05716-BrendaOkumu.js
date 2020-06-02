8dc30c39d5a84eedf68a0c3be1ccada2034c84af
brendaokumu <32571251+brendaokumu@users.noreply.github.com>
2020-06-03 01:50:51 +0300

    hello world script

---
+++ scripts/05716-BrendaOkumu.js
@@ -0,0 +1,9 @@
+/*simple script to display hello world*/
+newFunction();
+
+function newFunction() {
+    let name = prompt("What is your full name?");
+    let id = prompt("What is your HNGi7 ID?");
+    let lang = prompt("What is your language for task 2?");
+    alert(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
+}
