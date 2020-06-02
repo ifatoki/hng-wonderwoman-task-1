56362172027b7e8cd02269d37bf02655f04fac50
Gabicle <gskurah@yahoo.co.uk>
2020-06-03 00:16:54 +0100

    Added Script (JS)

---
+++ scripts/01388-Maelle.js
@@ -0,0 +1,6 @@
+const fullName = "Gabrielle Kurah";
+const hngID = "01388";
+const language = "Javascript";
+const message =  `Hello World, this is ${fullName} with HNGi7 ID ${hngID} using ${language} for stage 2 task`;
+
+console.log(message);
