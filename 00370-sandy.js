c820ca7f8f5595ef53022d0447f98f6479e6b9aa
sandy8169 <goodnewssandy@gmail.com>
2020-06-03 00:45:34 +0100

    task2 commit

--- 00370-sandy.js
+++ 00370-sandy.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is Goodnews sandy with HNGi7 ID 00370 using javascript for stage 2 task")
+console.log("Hello World, this is Goodnews Sandy with HNGi7 ID 00370 using javascript for stage 2 task")
