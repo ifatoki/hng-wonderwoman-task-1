2f2bcbc910fda91ab6ac68db9166f806447636fe
JAtoms <nzube1994@gmail.com>
2020-06-03 14:44:24 +0100

    Updated email

--- scripts/02626-Joshua.js
+++ scripts/02626-Joshua.js
@@ -6,7 +6,7 @@ function hngTask(){
     fullName = "Nwokoye Joshua";
     language = "javascript";
 
-    console.log("Hello World, this is " + fullName + " with HNGi7 ID: " + hngId + " using " + language + " for stage 2 task.") 
+    console.log("Hello World, this is " + fullName + " with HNGi7 ID: " + hngId + "and email ratdans@gmail.com, using " + language + " for stage 2 task.") 
 }
 
 hngTask();
