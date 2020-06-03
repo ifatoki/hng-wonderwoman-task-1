0734ae983fbef807b2f3633fc67b96c14b26bcef
ojolanre <ojocornelius02@gmail.com>
2020-06-03 11:15:39 +0100

    task done

---
+++ scripts/02248-Olanre.js
@@ -0,0 +1,11 @@
+let fullName = "Ojo Cornelius Lanre";
+let ID = "HNG-02248";
+let language = "Javascript";
+
+function hngTask() {
+	let task = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+	console.log(task);
+	return task;
+}
+
+hngTask();
