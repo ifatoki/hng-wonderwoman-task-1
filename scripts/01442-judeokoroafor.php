5b3c3f6c75ccff20fa92a4eed973a1d8370f85d9
jude okoroafor <judeokoroafor@gmail.com>
2020-06-03 02:08:49 +0100

    Task 1

---
+++ scripts/01442-judeokoroafor.php
@@ -0,0 +1,8 @@
+<?php
+$fullName = "Jude Okoroafor";
+$id = "HNG-01442";
+$language = "php";
+
+echo ('Hello World, this is ' .$fullName. ' with HNGi7 ID ' .$id. ' using ' .$language . ' for stage 2 task');
+
+?>
