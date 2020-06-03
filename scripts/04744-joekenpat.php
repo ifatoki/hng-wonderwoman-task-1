8d98fabc3f4490069d3defe50c7f50481ea56565
Patrick Joel <joekenpat@gmail.com>
2020-06-03 10:31:08 +0100

    added script for user joekenpat

---
+++ scripts/04744-joekenpat.php
@@ -0,0 +1,7 @@
+<?php
+$fname = "Patrick Joel Kenneth";
+$hng_id ="HNG-04744";
+$lang ="PHP";
+$out_string= sprintf("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task", $fname, $hng_id, $lang);
+echo $out_string;
+?>
