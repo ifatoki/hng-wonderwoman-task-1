417498cfd99946daa74cab90ced28a4db30152dc
Oghenkome Akaka {S} <OghenkomeAka@accessbankplc.com>
2020-06-03 13:19:13 +0100

    changed email address position in output string

--- scripts/03933-kome.js
+++ scripts/03933-kome.js
@@ -2,6 +2,6 @@ var myFullName = 'Oghenekome Akaka';
 var myHngId = 'HNG-03933';
 var myProgLanguage = 'javascript';
 var myEmail = 'oghenekomeakaka@gmail.com'
-var introMessage = 'Hello World, this is ' + myFullName + ' , my email address is ' + myEmail + ' with HNGi7 ID ' + myHngId + ' using ' + myProgLanguage + ' for stage 2 task' + '<br>';
+var introMessage = 'Hello World, this is ' + myFullName + ' with HNGi7 ID ' + myHngId + ' using ' + myProgLanguage + ' for stage 2 task ' + myEmail;
 
 console.log(introMessage);
