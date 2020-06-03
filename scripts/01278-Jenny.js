f16f7bbeb9a8193e8d0d5f02d76b1b322a115872
jenyfah <66225658+jenyfah@users.noreply.github.com>
2020-06-03 21:13:50 +0100

    Update 01278-Jenny.js

--- scripts/01278-Jenny.js
+++ scripts/01278-Jenny.js
@@ -10,6 +10,4 @@ function myFunction()
 
  let email = "osejoseph33@gmail.com"
 
- console.log(`Hello world, this is ${myName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
-
-}
+ console.log(`Hello world, this is ${myName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)}
