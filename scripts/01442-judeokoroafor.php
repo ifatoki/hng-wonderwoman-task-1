6e485666c9e6fc77a318f935e50bb83af838a1cc
jude okoroafor <judeokoroafor@gmail.com>
2020-06-03 02:26:34 +0100

    Task 1

--- scripts/01442-judeokoroafor.php
+++ scripts/01442-judeokoroafor.php
@@ -2,7 +2,8 @@
 $fullName = "Jude Okoroafor";
 $id = "HNG-01442";
 $language = "php";
+$stageNum = 2;
 
-echo ('Hello World, this is ' .$fullName. ' with HNGi7 ID ' .$id. ' using ' .$language . ' for stage 2 task');
+echo ("Hello World, this is " .$fullName. " with HNGi7 ID " .$id. " using " .$language . " for stage " .$stageNum . " task");
 
 ?>
