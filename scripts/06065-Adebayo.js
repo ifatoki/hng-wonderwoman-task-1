87e005a3e7159b92ae0dfbb49281007ad9dbcf58
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2020-06-03 16:09:58 +0100

    Chore:(Added abebayo.js)

---
+++ scripts/06065-Adebayo.js
@@ -0,0 +1,7 @@
+let member = "Adebayo Ilerioluwa";
+let ID = "HNG-06065";
+let lang = "JavaScript"
+
+let task = `Hello World this is ${member} with HNGi7 ID ${ID} using ${lang} for stage 2 task.`;
+
+console.log(task);
