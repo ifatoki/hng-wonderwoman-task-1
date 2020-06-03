74abcaa353010e6159315c79944c1b207b304e5d
Nisha Mehra <64705808+nishamehra@users.noreply.github.com>
2020-06-03 02:03:52 -0700

    Update 01532-Nisha.py

--- scripts/01532-Nisha.py
+++ scripts/01532-Nisha.py
@@ -1,3 +1,3 @@
 name = "Nisha Mehra"
 hngid = "HNG-01532"
-print("Hello World, this is " + name + " with HNGi 7 ID " + hngid + " using python for stage 2 task")
+print("Hello World, this is " + name + " with HNGi7 ID " + hngid + " using python for stage 2 task")
