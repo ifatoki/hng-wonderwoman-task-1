ef19e205b5c5c6eb3314008d7cbc775fa95b3a0f
ogunsolu007 <63315246+ogunsolu007@users.noreply.github.com>
2020-06-03 22:37:11 +0100

    Update 04828-ogunsolu.js

--- scripts/04828-ogunsolu.js
+++ scripts/04828-ogunsolu.js
@@ -2,6 +2,6 @@
 	let name = 'ogunsolu qudus';
 	let ID = 'HNG-04828';
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using Javascript for stage 2 task`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using Javascript for stage 2 task ogunsolu30@gmail.com`);
 
 
