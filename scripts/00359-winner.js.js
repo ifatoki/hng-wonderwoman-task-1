91cf9736854875fd9ee37d780405ea766ac00233
Vicwin13 <vnwimo13@gmail.com>
2020-06-03 02:37:09 +0100

    made some changes

--- scripts/00359-winner.js.js
+++ scripts/00359-winner.js.js
@@ -1,7 +1,8 @@
 let member = "Victor Nwimo";
 let ID = "HNG-00359";
+let lang = "JavaScript"
 
-let task = `Hello World this is ${member} with HNGi7 ID ${ID} for stage 2 task.`;
+let task = `Hello World this is ${member} with HNGi7 ID ${ID} using ${lang} for stage 2 task.`;
 
 console.log(task);
 
