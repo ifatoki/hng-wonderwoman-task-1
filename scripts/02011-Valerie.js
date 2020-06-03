4b4b2b7c23f2911fa1c024792f56c1b6d1a6f6f0
ooakhu <ooakhu@gmail.com>
2020-06-03 05:26:03 -0400

    updated email position

--- scripts/02011-Valerie.js
+++ scripts/02011-Valerie.js
@@ -1,6 +1,6 @@
-const stageTwoTask = (name, id, email, language) => {
+const stageTwoTask = (name, id, language, email) => {
     if (name === 'Valerie Oakhu' && id === 'HNG-02011' && language === 'Javascript' && email === 'ooakhu@gmail.com') {
-        return (`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
+        return (`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`);
     }
 }
-console.log(stageTwoTask('Valerie Oakhu', 'HNG-02011', 'ooakhu@gmail.com', 'Javascript'));
+console.log(stageTwoTask('Valerie Oakhu', 'HNG-02011', 'Javascript', 'ooakhu@gmail.com'));
