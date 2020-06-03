a696c97b1d564db02e2fa305436814b785cd4250
Linda Okorie <lindaokorie27@gmail.com>
2020-06-03 19:20:05 +0100

    appended email to string

--- scripts/01560-dauntless.js
+++ scripts/01560-dauntless.js
@@ -1,3 +1,1 @@
-let email = "lindaokorie27@gmail.com";
-
-console.log("Hello World, this is Linda Okorie with HNGi7 ID HNG-01560 using Javascript for stage 2 task.");
+console.log("Hello World, this is Linda Okorie with HNGi7 ID HNG-01560 using Javascript for stage 2 task lindaokorie27@gmail.com .");
