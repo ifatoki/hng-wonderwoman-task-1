0d9bc92254c0a6b6c01705efe494b330a4da2e5e
startoffs <adelekesays@gmail.com>
2020-06-03 13:55:32 +0100

    edited

--- scripts/HNG-02083-startoffs.js
+++ scripts/HNG-02083-startoffs.js
@@ -1,6 +1,6 @@
 let fullName = "Morounfolu Adesanya";
 let HNGi7_ID = "HNG-02083";
 let language = "Javascript";
-console.log(
-  `Hello World, this is ${fullName} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2 task`
-);
+let email = "adelekesays@gmail.com";
+const MESSAGE = `Hello World,this is ${fullName} with HNGi7 ID ${HNGi7_ID} and email ${email} using ${language} for stage 2 task.`;
+console.log(MESSAGE);
