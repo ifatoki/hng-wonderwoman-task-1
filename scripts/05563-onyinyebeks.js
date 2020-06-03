81537d2b806bb7e26dedfe0b39bf1f49ae68f57f
goodness chris-ugari <goodnesschrisugari@yahoo.com>
2020-06-03 14:23:19 +0100

    Added email to output

--- scripts/05563-onyinyebeks.js
+++ scripts/05563-onyinyebeks.js
@@ -2,4 +2,4 @@ let name = "Goodness Chris-Ugari";
 let id = "HNG-05563";
 let email = "goodnesschrisugari@yahoo.com";
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using JavaScript for stage 2 task`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using JavaScript for stage 2 task`);
