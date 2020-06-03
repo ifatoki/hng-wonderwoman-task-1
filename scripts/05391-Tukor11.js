e1a3a1e0585ea2a95334096c2e74aac018e1569f
ifenna sylvester <tukor11@icloud.com>
2020-06-03 16:40:56 +0100

    Update 05391-Tukor11.js

--- scripts/05391-Tukor11.js
+++ scripts/05391-Tukor11.js
@@ -1,5 +1,5 @@
 const nme = "Sylvester";
 const hngid = "HNG-05391";
 console.log(
-  `Hello World, this is ${nme} with HNGi7 ID ${hngid} using JavaScript for stage 2 task`
+  `Hello World, this is ${nme} with HNGi7 ID ${hngid} using JavaScript for stage 2 task tukor11@icloud.com`
 );
