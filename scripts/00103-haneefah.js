34879ce65e94547f3dd3c68929dae797a3b94a49
HaneefahAliu <62837727+HaneefahAliu@users.noreply.github.com>
2020-06-03 15:49:26 +0100

    Update 00103-haneefah.js

--- scripts/00103-haneefah.js
+++ scripts/00103-haneefah.js
@@ -2,9 +2,10 @@ var fname = "Haneefah";
 var lname = "Aliu";
 var ID = "HNG-00103"
 var language = "javascript";
+var email = "aliuhaneefah@gmail.com";
 
 
-console.log("Hello World, this is " + fname +" " + lname + " with HNGi7 ID " +  ID + " using " + language + " for stage 2 task ");
+console.log("Hello World, this is " + fullname + " " + " with HNGi7 ID " +  ID + " using " + language + " for stage 2 task " + " " + email);
 
 
 
