d01906cb74924c0a07371a9584fcdbf04191d330
Newton <kamaunewton78@gmail.com>
2020-06-03 22:01:15 +0300

    my first task for the team wonderwoman

---
+++ scripts/03823-newton.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HNG7</title>
+</head>
+<body>
+    <?php
+    $name = "Newton Kamau ";
+    $hng_id = "HNG-03823";
+    echo "Hello world, this is ". $name . " with " . $hng_id . " using php for stage two task .";
+    ?>
+</body>
+</html>
