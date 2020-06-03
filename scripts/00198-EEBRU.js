edca45ada77f1981c7cff0bf38a9404d89222111
Ibrahim <Alaoopeyemi101@gmail.com>
2020-06-03 14:40:44 +0100

    Update 00198-EEBRU.js

---
+++ scripts/00198-EEBRU.js
@@ -0,0 +1,9 @@
+let obj = {
+    name: "Ibrahim Alao",
+    id: "HNG-00198",
+    email: "alaoopeyemi101@gmail.com",
+    fn: function(){
+           return `Hello World, this is ${this.name} with HNGi7 ID ${this.id} using Javascript for stage 2 task`;
+    }
+}
+obj.fn();
