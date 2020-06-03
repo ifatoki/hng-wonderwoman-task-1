98b3444f4156b3f85cdb032f994fbe4d466eaaa1
Peter-Xidian <mayowaodebode@gmail.com>
2020-06-03 14:04:13 +0100

    Update 00245-Mayowa.js

--- scripts/00245-Mayowa.js
+++ scripts/00245-Mayowa.js
@@ -1,7 +1,7 @@
 let a = 'Hello World, this is Mayowa Odebode with HNGi7 ID HNG-';
 let id = '00245 ';
-let b = 'using Javascript for stage 2 task.';
+let b = 'using Javascript for stage 2 task';
 
 let profile = a + id + b;
 
-console.log(profile);
+console.log(profile + ' mayowaodebode@gmail.com');
