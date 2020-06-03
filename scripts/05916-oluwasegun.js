fe980ef54a71e52d5edda58a8f7ee003f706ab96
Petersheg <43689575+Petersheg@users.noreply.github.com>
2020-06-03 01:25:16 +0100

    Added a javascript file to the develop branch

---
+++ scripts/05916-oluwasegun.js
@@ -0,0 +1,12 @@
+/*
+    A javascript file that output my dedails as shown bellow
+    "Hello world, this is Peter Oluwasegun with HNGi7 ID 192511 using Javascript for stage 2 task."
+*/ 
+
+function outPutDetails(name,id,lang){
+
+    console.log(`
+        Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.
+    `);
+}
+outPutDetails('Peter Oluwasegun','HNG-05916','JavaScrip Language');
