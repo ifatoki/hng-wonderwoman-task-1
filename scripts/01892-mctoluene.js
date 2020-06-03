5af02e0cbd7c410073e41d60e5732f0af1c4e603
McToluene <xclusiveniggaz10@live.com>
2020-06-03 14:01:28 +0100

    Email added

--- scripts/01892-mctoluene.js
+++ scripts/01892-mctoluene.js
@@ -1,5 +1,7 @@
 const FULL_NAME = "Ojulari Abdulhamid";
 const HNG_ID = "HNG-01892";
 const LANGUAGE = "Javascript";
-const output = `Hello World, this is ${FULL_NAME} with HNGi7 ID ${HNG_ID} using ${LANGUAGE} for stage 2 task`;
+const EMAIL = "toluenelarry@gmail.com";
+
+const output = `Hello World, this is ${FULL_NAME} with HNGi7 ID ${HNG_ID} using ${LANGUAGE} for stage 2 task ${EMAIL}`;
 console.log(output);
