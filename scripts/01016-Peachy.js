ca020783dab0711db74642ca469b2fe6028724f0
Peachy <temmynladejesu@gmail.com>
2020-06-03 12:59:16 +0100

    Added 01016-Peachy.js file

---
+++ scripts/01016-Peachy.js
@@ -0,0 +1,6 @@
+const fullName = 'Temilade Opanuga';
+const hngId = "HNG-01016";
+const lang = "JavaScript";
+
+const result = (`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${lang} for stage 2 task`);
+console.log (result);
