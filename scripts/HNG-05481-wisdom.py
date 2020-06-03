3ad458c9ed8ed863bc1684d59efb17b56293b209
Wisdom Obinna <39682482+zibest@users.noreply.github.com>
2020-06-03 15:57:50 +0100

    Add files via upload

---
+++ scripts/HNG-05481-wisdom.py
@@ -0,0 +1,7 @@
+
+full_name = "WISDOM OBINNA"
+HNGi7_ID = "HNG-05481"
+email = "kcwisdom7@gmail.com"
+language = "Python"
+
+print("HelloWorld, this is", full_name, "with HNGi7[ID]:", HNGi7_ID, "using", language, "for stage 2 task", email)
