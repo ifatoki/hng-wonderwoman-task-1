8bb122149a93df0cb61e9a435db8cebdef277a63
Codeleeq <princefemibabs@gmail.com>
2020-06-03 19:05:43 +0100

    my change using console.log and email address

--- scripts/HNG-05367-itsmaleeq.js
+++ scripts/HNG-05367-itsmaleeq.js
@@ -1,1 +1,1 @@
-document.write("Hello World, this is BABAJIDE ONAYEMI with HNGi7 ID HNG-05367 using javascript for stage 2 task princefemibabs@gmail.com");
+console.log("Hello World, this is BABAJIDE ONAYEMI with HNGi7 ID HNG-05367 using javascript for stage 2 task princefemibabs@gmail.com");
