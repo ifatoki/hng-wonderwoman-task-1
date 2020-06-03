35651f48b04590720f0525fcf1cd3d1f07610fa1
Sholance <ohksamie@gmail.com>
2020-06-03 17:12:34 +0100

    Commiting the task for team wonderwoman

---
+++ scripts/05530-shola.js
@@ -0,0 +1,7 @@
+let language = "javascript";
+let email ="ohksam@gmail.com";
+let id ="HNG-05530";
+let name ="Kolade Victor";
+let output =`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
+
+console.log(output);
