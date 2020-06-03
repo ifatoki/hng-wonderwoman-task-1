de76c73391c220baa56c8c3feb036fcc1b0684d2
Femi Opanuga <femathic@gmail.com>
2020-06-03 17:00:31 +0100

    Add email to scripts/04233-femathic.js file

--- scripts/04233-femathic.js
+++ scripts/04233-femathic.js
@@ -2,4 +2,4 @@ const fullName = 'Oluwafemi Opanuga';
 const hngId = "HNG-04233";
 const email = "femathic@gmail.com"
 
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using JavaScript for stage 2 task`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using JavaScript for stage 2 task ${email}`);
