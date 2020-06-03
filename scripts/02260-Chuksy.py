232b5c255c51f40b28e0533498af2e581a5c2f56
Your Name <onuhchukwuma12@gmail.com>
2020-06-03 12:24:51 +0100

    Just added my file. Please check it out

---
+++ scripts/02260-Chuksy.py
@@ -0,0 +1,9 @@
+def my_hngi7_details():
+    hngid = 'HNG-02260'
+    fullname = 'Onuh Chukwuma'
+    language = 'Python'
+    print(f'Hello World, this is {fullname} with HNGi7 ID {hngid} using {language} for stage 2 task.', flush = True)
+    
+my_hngi7_details()
+
+
