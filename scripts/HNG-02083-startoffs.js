3cd4994289865bda24323466049ef6c63ea1f8f9
startoffs <adelekesays@gmail.com>
2020-06-03 10:18:46 +0100

    Morounfolu Adesanya

---
+++ scripts/HNG-02083-startoffs.js
@@ -0,0 +1,6 @@
+let fullName = "Morounfolu Adesanya";
+let HNGi7_ID = "HNG-02083";
+let language = "Javascript";
+console.log(
+  `Hello World, this is ${fullName} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2 task`
+);
