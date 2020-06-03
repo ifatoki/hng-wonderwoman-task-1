fcf6c836692423ba8b339006bfb994d12b4a3aa5
salami17 <muhahid18@gmail.com>
2020-06-03 23:56:30 +0100

    your commit here

---
+++ scripts/00553-CApitanMA.py
@@ -0,0 +1,5 @@
+name='Muhammad-ahid Abdulsalam'
+ID='HNG-00553'
+language='python'
+email='muhahid18@gmail.com'
+print("Hello world,this is %s with HNGI7 ID %s and email %s using %s for stage 2 task "%(name,ID,email,language))
