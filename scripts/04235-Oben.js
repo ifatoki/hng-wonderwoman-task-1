c54b0ad85e8e03e1f6deb425145c3af6623f19a5
benndip <obentabiayuk1@gmail.com>
2020-06-03 21:25:36 +0100

    added 04235-Oben.js

---
+++ scripts/04235-Oben.js
@@ -0,0 +1,10 @@
+let myName = "Oben Tabendip Tabiayuk"
+let myId = "HNG-04235"
+let language = "Javascript"
+let email = "obentabiayuk1@gmail.com"
+
+const info = () => {
+   console.log(`Hello World, this is ${myName} with HNGi7 ID ${myId} using ${language} for stage 2 task ${email}`);
+}
+
+info();
