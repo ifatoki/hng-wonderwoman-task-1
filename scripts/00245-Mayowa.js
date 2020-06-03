1085441780c510f3e6f7c0e8ff4a599c9b416e00
Peter-Xidian <mayowaodebode@gmail.com>
2020-06-03 08:23:19 +0100

    Update 00245-Mayowa.js

--- scripts/00245-Mayowa.js
+++ scripts/00245-Mayowa.js
@@ -1,4 +1,4 @@
-let a = 'Hello World, this is Mayowa Odebode with HNGi7 ID: HNG-';
+let a = 'Hello World, this is Mayowa Odebode with HNGi7 ID HNG-';
 let id = '00245 ';
 let b = 'using Javascript for stage 2 task.';
 
