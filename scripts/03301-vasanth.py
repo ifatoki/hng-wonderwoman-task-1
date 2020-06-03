ea618c31abe24766c402c40e5d02d9ca9dcc3208
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2020-06-03 18:59:20 +0530

    this is my first commit of task2

---
+++ scripts/03301-vasanth.py
@@ -0,0 +1,7 @@
+#python is awesome
+#i am loving this internship
+
+vas="Vasanth Kumar Cheepurupalli";
+Idd="HNG-03301";
+email="cheepurupalli.vasanthkumar@gmail.com";
+print("Hello World, this is "+vas+" with HNGi7 ID "+Idd+" and email "+email+" using Python for stage 2 task");
