7cb489022596313c44bc537e5dfe7fca7353cf79
Aushtiin <60714861+Aushtiin@users.noreply.github.com>
2020-06-03 10:10:47 +0100

    00300-Aus.js

---
+++ scripts/00300-Aus.js
@@ -0,0 +1,7 @@
+const fullName = "Tangban Austin Bisong";
+const hngId = "HNG-00300";
+const language = "javascript";
+
+console.log(
+	`Hello World, this is ${fullName} with HNGi7 id ${hngId}, using ${language} for stage 2 task.`
+	);
