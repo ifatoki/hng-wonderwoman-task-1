514e6e6e67f1222ced2ef9bfbb0fc12483330f16
Gideon Anyalewechi <giddy@pop-os.localdomain>
2020-06-03 08:24:29 +0100

    Created script for hngi7 team-wonderwoman task one

---
+++ scripts/02709-giddy.py
@@ -0,0 +1,6 @@
+# Variables - name, HNG-id, language
+name = 'Gideon Anyalewechi'
+hng_id = 'HNG-02709'
+language = 'python'
+
+print(f'Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task', flush=True)
