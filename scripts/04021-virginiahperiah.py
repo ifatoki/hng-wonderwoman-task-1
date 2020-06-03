707679d04ad2e72326cd2be13c9e0448cecbc1bb
virginiah894 <virgyperry@gmail.com>
2020-06-03 14:02:37 +0300

    fix an error

--- scripts/04021-virginiahperiah.py
+++ scripts/04021-virginiahperiah.py
@@ -1,4 +1,4 @@
 full_name = 'Virginiah Periah'
 hng_num = '04021'
 used_lang = 'Python'
-print(f" Hello World, this is {full_name} with HNGi7 ID{hng_num} using {used_lang} for stage 2 task.")
+print(f" Hello World, this is {full_name} with HNGi7 ID {hng_num} using {used_lang} for stage 2 task.")
