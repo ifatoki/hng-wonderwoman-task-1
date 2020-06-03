a0eb142bb8fa7f3566c54ef01437ec243bc433a6
hafsah <emekomahafsah@gmail.com>
2020-06-03 20:46:04 +0100

    my script file

---
+++ scripts/01364-hafsah.js
@@ -0,0 +1,7 @@
+const name = 'Hafsah Emekoma';
+const hngid = 'HNG-01364';
+const lang = 'JavaScript';
+const mail = 'hafsyezinne@gmail.com';
+
+const bio = (`Hello World, this is ${name} with HNGi7 ID ${hngid} using ${lang} for stage 2 task ${mail}`);
+console.log(bio);
