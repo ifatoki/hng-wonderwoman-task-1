59e3dd09df781b9d670545c914990be2f537642a
Gbemmiii <62169659+Gbemmiii@users.noreply.github.com>
2020-06-04 00:41:54 +0100

    creating 04692.Aduragbemi.py

---
+++ scripts/04692.Aduragbemi.py
@@ -0,0 +1,7 @@
+full_name = "Anuoluwapo Tayo-Alabi"
+language = "python"
+ID = "HNG-04692"
+email_address = "anualabi2017@gmail.com"
+
+message = "Hello World, this is" + " " + full_name + " " + "with HNGi7 ID" + " " + ID + " " + "using" + " " + language + " " + "for stage 2 task" + " " + email_address
+print(message)
