6f28f6b8189986e5180f7c61ae9bde89d880b658
jenyfah <osejoseph33@gmail.com>
2020-06-03 21:05:29 +0100

    01278-Jenny

---
+++ scripts/01278-Jenny.js
@@ -0,0 +1,15 @@
+myFunction ();
+
+function myFunction()
+
+{let myName = "Jennifer"
+
+ let id = "HNG-01278"
+
+ let language = "JavaScript"
+
+ let email = "osejoseph33@gmail.com"
+
+ console.log(`Hello world, this is ${myName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
+
+}
