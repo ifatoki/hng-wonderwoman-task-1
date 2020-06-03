7073c80d427d5548c9b694f7fd628df504ef18e1
Shafi Akinropo <sakinropo@gmail.com>
2020-06-03 17:41:20 +0100

    Update

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 
+
 $json = $_SERVER["QUERY_STRING"] ?? '';
 
 $files = scandir("scripts/");
 
 unset($files[0]);
 unset($files[1]);
-unset($files[2]);
+
 $output = [];
 $outputJSON = [];
 $data = [];
-$passes = 0;
-$fails = 0;
+
+$success = 0;
+$failure = 0;
+
 foreach ($files as $file) {
     $extension = explode('.', $file);
 
@@ -25,149 +28,166 @@ foreach ($files as $file) {
         case 'py':
             $startScript = "python";
             break;
-        case 'dart':
-            $startScript = "dart";
-            break;
-        case 'java':
-            $startScript = "java";
-
-            exec("javac scripts/" . $file);
-            break;
-
-        default:
-            $startScript = "php";
-            break;
     }
 
-    $f = exec($startScript . " scripts/" . $file);
+    $f = exec($startScript . " scripts/".$file);
 
     @$data[$extension[0]]->content = $f;
     $data[$extension[0]]->status = testFileContent($f);
     $data[$extension[0]]->name = $extension[0];
     $output[] = [$f, testFileContent($f), $extension[0]];
 }
-$outputJSON = $data;
+
+foreach ($output as $status) {
+    if ($status[1] == 'Pass') {
+        $success++;
+    } elseif ($status[1] == 'Fail') {
+        $failure++;
+    }
+}
 
 function testFileContent($string)
 {
-    // $pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
-
-    // if (preg_match('/^Hello\sWorld[,|.|!]?\sthis\sis\s[a-zA-Z]{2,}\s[a-zA-Z]{2,}(\s[a-zA-Z]{2,})?\swith\sHNGi7\sID\s(HNG-\d{3,})\susing\s[a-zA-Z|#]{2,}\sfor\sstage\s2\stask\s(\.[_a-z0-9-]+)*@[a-z0-9-].?$/i', trim($string))) {
-    //     return 'Pass';
-    // }
-
     if (preg_match('/^Hello\sWorld[,|.|!]?\sthis\sis\s[a-zA-Z]{2,}\s[a-zA-Z]{2,}(\s[a-zA-Z]{2,})?\swith\sHNGi7\sID\s(HNG-\d{3,})\susing\s[a-zA-Z|#]{2,}\sfor\sstage\s2\stask\s[a-zA-Z|#]*@[a-z0-9-]+(\.[a-z0-9-]+).?$/i', trim($string))) {
         return 'Pass';
     }
+   
+
     return 'Fail';
 }
 
-foreach ($output as $val) {
-    if ($val[1] == 'Pass') {
-        $passes++;
-    } elseif ($val[1] == 'Fail') {
-        $fails++;
-    }
-}
+ob_end_flush();
 
-if (isset($json) && $json == 'json') {
-    echo json_encode($outputJSON);
-} else {
-    ?>
-    <html>
+    if (isset($json) && $json == 'json') {
+        echo json_encode($output);
+    } else {
+        ?>
 
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Stage 2 Task</title>
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            <link rel="stylesheet" type="text/css" href="css/style.css">
+            <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        </head>
+        
 
-    <body>
-    <div class="container-fluid">
-        <nav class="navbar navbar-dark bg-dark fixed-top">
-                <span class="navbar-text">
-                    HNGi7 Team Sentry
-                </span>
-            <div class="float-right text-white">
-                <small>
-                    Leader: <span class="btn btn-sm btn btn-outline-primary">@E.U</span>
-                </small> &nbsp;
-                <small>
-                    FrontEnd: <span class="btn btn-sm btn btn-outline-success">@dona</span>
-                </small> &nbsp;
-                <small>
-                    DevOps: <span class="btn btn-sm btn btn-outline-info">@Fidele</span>
-                </small> &nbsp;
+        <body>
+            <div class="container-fluid">
+                <nav class="navbar navbar-light  fixed-top" style="background-color: #FFF;">
+                    <span class="navbar-text bold">
+                        HNGi7 Team Wonder Woman
+                    </span>
+                    <div class="float-right text-white">
+                        <small class="text-dark">
+                            Leader: <span class="btn btn-sm btn btn-outline-primary">@Xt</span>
+                        </small> &nbsp;
+                        <small class="text-dark">
+                            FrontEnd: <span class="btn btn-sm btn btn-outline-success">@Haneefah</span>
+                        </small> &nbsp;
+                        <small class="text-dark">
+                            DevOps: <span class="btn btn-sm btn btn-outline-info">@Itunuloluwa</span>
+                        </small> &nbsp;
+                    </div>
+               </nav>
             </div>
-        </nav>
-    </div>
-    <div class="container">
-        <div class="row" style="padding: 6em 0" class="text-center">
-            <div class="col-md-4">
-                <button type="button" class="btn">
-                    Submitted <span class="badge badge-primary"><?php echo($passes + $fails)  ?></span>
-                </button>
-            </div>
-            <div class="col-md-4">
-                <button type="button" class="btn">
-                    Passes <span class="badge badge-success"><?php echo($passes)  ?></span>
-                </button>
-            </div>
-            <div class="col-md-4">
-                <button type="button" class="btn">
-                    Fails <span class="badge badge-danger"><?php echo($fails)  ?></span>
-                </button>
-            </div>
-        </div>
-        <table class="table table-hover center table-striped">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Message</th>
-                <th scope="col">Status</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $row = 0;
-    foreach ($output as $out) {
-        $status = $out[1] == 'Pass' ? 1 : 0;
-        if ($status) {
-            echo <<<EOL
-                                <tr class="table-success">
-                                <th scope="row">$row</th>
-                                <td><b>$out[2]</b></td>
-                                <td>$out[0]</td>
-                                <td>$out[1] ✅</td>
+
+            <div class="container padding-t">
+                <div class="row  border mb-4">
+                      <div class="col-md-4">
+                        <div class="stati turquoise ">
+                          <i class="fa fa-calculator"></i>
+                          <div>
+                            <span class="bold">Total Submitted</span>
+                            <b><?php echo($success + $failure)  ?></b>
+                            
+                          </div> 
+                        </div>
+                      </div> 
+                      <div class="col-md-4">
+                        <div class="stati bg-turquoise ">
+                          <i class="fa fa-check-circle"></i>
+                          <div>
+                            <span class="bold text-white">Total Passes</span>
+                            <b><?php echo $success; ?></b>
+                            
+                          </div> 
+                        </div>
+                      </div> 
+                      <div class="col-md-4">
+                        <div class="stati bg-alizarin  ">
+                          <i class="fa fa-times-circle"></i>
+                          <div>
+                            <span class="bold text-white">Total Fail</span>
+                            <b><?php echo $failure; ?></b>
+                            
+                          </div> 
+                        </div>
+                      </div> 
+                      
+                </div>
+
+                
+                <h4 class="text-light">Script Format:</h4>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr class="bg-dark text-white">
+                                <th>S/N</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Messages</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sn =1;
+                             
+        foreach ($output as  $value) {
+            if ($value[1] == 'Pass') {
+                $color = 'green';
+            } else {
+                $color = 'red';
+            }
+
+
+            $string = $value[0];
+            //extraction of email from string
+                                
+            $email_pattern = '/[a-z0-9_\-\+\.]+@[a-z0-9\-]+\.([a-z]{2,4})(?:\.[a-z]{2})?/i';
+            preg_match_all($email_pattern, $string, $em_matches); ?>
+
+                                <tr class="bold <?php echo $color; ?>">
+                                    <td><?php echo $sn++; ?></td>
+                                    <td><?php
+                                        // echo $value[2];
+                                        echo str_replace("-", " ", $value[2]) ?? ''; ?></td>
+                                    <td><?php echo $em_matches[0][0] ?? ''; ?></td>
+                                    <td><?php echo  $value[0] ?></td>
+                                    <td>
+                                        <?php echo  $value[1] ?>
+                                        <?php
+                                            if ($value[1] == 'Pass') {
+                                                echo "<span class='text-success'>✔</span>";
+                                            } else {
+                                                echo "<span class='text-danger'>✖</span>";
+                                            } ?>
+                                    </td>
                                 </tr>
-                             EOL;
-        } else {
-            echo <<<EOL
-                                <tr class="table-danger">
-                                <th scope="row">$row</th>
-                                <td><b>$out[2]</b></td>
-                                <td>$out[0]</td>
-                                <td>$out[1] ❌</td>
-                                </tr>
-                            EOL;
-        }
-        $row++;
+                            <?php
+        } ?>
+                        </tbody>
 
-        flush();
-        ob_flush();
+                    </table>
+                </div>
+            </div>
+            
 
-        sleep(1); //used this to test the buffering
-    } ?>
+        </body>
 
-            </tbody>
-        </table>
-
-
-    </div>
-
-    </body>
-
-    </html>
-    <?php
-}
-
+        </html>
+<?php
+    } ob_start();
 ?>
