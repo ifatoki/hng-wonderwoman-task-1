61ea8a10c75ee6374fb4647f7e18ea4c63f2e837
Femi Opanuga <femathic@gmail.com>
2020-06-03 02:02:14 +0100

    Add 04233-femathic.js file to scripts

--- scripts/04233-femathic.js
+++ scripts/04233-femathic.js
@@ -1,4 +1,4 @@
 const fullName = 'Oluwafemi Opanuga';
-const hngId = "04233";
+const hngId = "HNG-04233";
 
 console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using JavaScript for stage 2 task`);
