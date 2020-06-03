355b32500d72bec61dbfd533f0426ca546eee7de
goodness obi <goodnessobi@gmail.com>
2020-06-03 07:26:33 +0100

    correct HNG ID format

--- scripts/02015-goody.js
+++ scripts/02015-goody.js
@@ -1,6 +1,6 @@
 let name = 'Goodness Obi';
-let HNGID = 'HNG-02015';
+let HNGID = '02015';
 
 console.log(
-  'Hello World, this is ${name} with HNGi7 ID ${HNGID} using JavaScript for stage 2 task'
+  `Hello World, this is ${name} with HNGi7 ID ${HNGID} using JavaScript for stage 2 task`
 );
