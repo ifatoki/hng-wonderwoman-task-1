482b536a52e520030c869d766675061347d0e7d7
maryaamox <sulaiman.maryam@yahoo.co.uk>
2020-06-03 21:31:41 +0100

    Delete 03774-Maryam.c.c

--- scripts/03774-Maryam.c.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-printf("Hello world, this is Maryam Sulaiman with HNGi7 ID HNG-03774 and email sulaiman.maryam@yahoo.co.uk using C for stage 2 task");
-return 0;
-}
