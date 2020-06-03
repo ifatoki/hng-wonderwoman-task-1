806ae6c79ae4a58b51daebeb81b8f9c9d86b8879
Bernice <bernicejohnsonuche@gmail.com>
2020-06-03 19:25:07 +0100

    Bernice Johnson

---
+++ scripts/00224-bernice.js
@@ -0,0 +1,4 @@
+let script = function(fullName, id, language, email) {
+    return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`
+};
+console.log(script('Bernice Johnson', 'HNG-00224', 'JavaScript', 'bernicejohnsonuche@gmail.com'));
