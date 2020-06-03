06c595fc7cd9312669fe0491520e5c2d156b70dc
Izaak Chukwuma <46270445+izaakwalz@users.noreply.github.com>
2020-06-03 17:52:20 +0100

    Updated 03001-walz.js, added email address

--- scripts/03001-walz.js
+++ scripts/03001-walz.js
@@ -3,5 +3,5 @@ const full_name = 'Izaak Chukwuma';
 const language = 'JavaScript';
 
 console.log(
-  `Hello World, this is ${full_name} with HNGi7 ID hng-${HNG_ID} using ${language} for stage 2 task `
+  `Hello World, this is ${full_name} with HNGi7 ID HNG-${HNG_ID} using ${language} for stage 2 task iblack.xyz@gmail.com`
 );
