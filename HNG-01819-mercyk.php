a3c7f214c4ccf0531eef3466b8395a4e611f1f74
unknown <mercyjemosop@gmail.com>
2020-06-03 13:25:41 +0300

    Added HNG-01819-mercyk.php

--- HNG-01819-mercyk.php
+++ HNG-01819-mercyk.php
@@ -1,5 +1,5 @@
 <?php
-$hello= "Hello world, this is Mercy Kipyegon with HNGi7 ID HNG-01819 using php for task 2 ;
+$hello= "Hello world, this is Mercy Kipyegon with HNGi7 ID HNG-01819 using php for stage 2 task";
 
 echo $hello;
 
