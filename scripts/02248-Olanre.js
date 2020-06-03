4d213e01a25f50df33e1c4be998454ced28d2450
ojolanre <ojocornelius02@gmail.com>
2020-06-03 15:17:33 +0100

    updated with my mail

--- scripts/02248-Olanre.js
+++ scripts/02248-Olanre.js
@@ -1,11 +1,11 @@
 let fullName = "Ojo Cornelius Lanre";
 let ID = "HNG-02248";
 let language = "Javascript";
+let email = "ojoolanrewaju62@gmail.com"
 
 function hngTask() {
-	let task = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+	let task = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task ${email}`;
 	console.log(task);
-	return task;
 }
 
 hngTask();
