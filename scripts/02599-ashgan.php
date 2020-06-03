9ef000001c7aebee1eb8287f237d8dee5214cd95
Ashgan Mustafa <ashganwiki@gmail.com>
2020-06-03 14:24:10 +0200

    Ashgan HNG-02599 stage 2 task

---
+++ scripts/02599-ashgan.php
@@ -0,0 +1,7 @@
+<?php 
+    $name = 'Ashgan Mustafa A. Mohammed';
+    $HNG_ID = '02599' ;
+    $output = 'Hello World, this is '.$name.' with HNGi7 ID HNG-'.$HNG_ID.' using PHP for stage 2 task';  
+    
+    echo $output; 
+?>
