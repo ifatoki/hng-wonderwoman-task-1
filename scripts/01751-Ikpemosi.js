907d9a56ca274a365a02a34e7f08a2197dd29ab4
IkpemosiArnold <61964760+IkpemosiArnold@users.noreply.github.com>
2020-06-03 10:13:09 +0100

    Update 01751-Ikpemosi.js

--- scripts/01751-Ikpemosi.js
+++ scripts/01751-Ikpemosi.js
@@ -1,10 +1,11 @@
 let hngID = "HNG-01751";
 let name = "Ikpemosi Arnold";
 let language ="JavaScript";
+let email = "Ikpemosi@protonmail.com";
 
 function sendData(){
     console.log("Hello World, this is " + name + " with HNGi7 ID " + hngID + " using " + language + " for stage 2 task");
-    var data = JSON.stringify({ "id": hngID, "name" : name, "language" : language});
+    var data = JSON.stringify({ "id": hngID, "name" : name, "language" : language, "email":email});
     return data;
 }
 
