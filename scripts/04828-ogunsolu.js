c2e0b782640b52594ecf11458e6210f0911b6daa
ogunsolu007 <63315246+ogunsolu007@users.noreply.github.com>
2020-06-03 22:11:39 +0100

    Create 04828-ogunsolu.js

---
+++ scripts/04828-ogunsolu.js
@@ -0,0 +1,10 @@
+
+	let name = 'ogunsolu qudus';
+	let ID = 'HNG-04828';
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using Javascript for stage 2 task`);
+
+let firstName = 'John';
+let lastName = 'Smith';
+
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
