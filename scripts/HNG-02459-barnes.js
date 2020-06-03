5347901e513185838a6a0ca56bb5e1d2b2f5e245
jaybarnes33 <ohenesetwumasi@gmail.com>
2020-06-03 12:25:47 +0000

    Add email to output

--- scripts/HNG-02459-barnes.js
+++
@@ -1,9 +0,0 @@
-// HNG  stage1 Task1
-const name = "Oduro Twumasi John Barnes";
-const hngID = "HNG-02459";
-
-function task (name, hngID){
-  console.log("Hello World, this is " + name + " with HNGi7 ID " + hngID +" using javascript for stage 2 task");
-}
-
-task(name,hngID);
