180ef1d53d3d61c0b6f65e568a75ef58805042da
daniel2code <danielnwoke20@gmail.com>
2020-06-03 13:36:22 +0100

    my task commit

---
+++ 00383-Daniel2code.js
@@ -0,0 +1,11 @@
+const data = {
+    name: 'Daniel Nwoke',
+    id: 'HNG-00383',
+    language: 'Javascript',
+    email: 'danielnwoke20@gmail.com'
+
+}
+
+
+
+console.log(`hello world, this is ${data.name} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task. ${data.email}`)
