7018081843e2f2d18c6840413517e8acb80d2c74
Jonyango <jeanadagi@gmail.com>
2020-06-03 15:38:26 +0200

    Added my email address

--- scripts/06309-atieno.py
+++ scripts/06309-atieno.py
@@ -1,11 +1,12 @@
-def printHello(fullname, ID, lang):
-    return "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(fullname, ID, lang)
+def printHello(fullname, ID, lang, email_address):
+    return "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task {}".format(fullname, ID, lang,email_address)
 
 if __name__ == '__main__':
     fullname = "Jinet Onyango"
     ID = "HNG-06309"
     lang = "Python"
-    print(printHello(fullname, ID, lang), flush=True)
+    email_address="jeanadagi@gmail.com"
+    print(printHello(fullname, ID, lang,email_address), flush=True)
 
 
 
