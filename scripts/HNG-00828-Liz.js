b885d2dbea0f2d2239a822769d7fe41348fccf6c
Bassey-Elizabeth <basseyelizabeth569@gmail.com>
2020-06-03 06:43:41 +0100

    updated HNG-00828-Liz.j

--- scripts/HNG-00828-Liz.js
+++ scripts/HNG-00828-Liz.js
@@ -2,7 +2,7 @@
 Script to display my name and id
 ********************************/
 
-const fullName = "Elizbeth Bassey";
+const fullName = "Elizabeth Bassey";
 const id = "HNG-00828";
 const language = "javascript";
 const message = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
