302e7041c2ca245c9bc9d587dab1790e25a94056
Nwani Victory <vickywane@gmail.com>
2020-06-03 10:34:27 +0100

    correction

--- scripts/02442-motdde.js
+++ scripts/02442-motdde.js
@@ -1,5 +1,5 @@
-let name = 'Oluwaseun Oyebade';
-let HNGID = '02442';
+let name = "Oluwaseun Oyebade";
+let HNGID = "02442";
 
 console.log(
   `Hello World, this is ${name} with HNGi7 ID ${HNGID} using JavaScript for stage 2 task`
--- scripts/05211-iamnwani.js
+++ scripts/05211-iamnwani.js
@@ -1,5 +1,5 @@
 let Myname = 'Victory Nwani';
-let MyHNGID = '05211';
+let MyHNGID = 'HNG-05211';
 
 console.log(
   `Hello World, this is ${Myname} with HNGi7 ID ${MyHNGID} using JavaScript for stage 2 task`
