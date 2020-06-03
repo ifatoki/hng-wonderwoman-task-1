dcbafc44ab9722a989353fb38b357dc8c211f65f
miebz <aomieibi@yahoo.com>
2020-06-03 13:35:19 +0100

    update email

--- scripts/HNG-02085-amaps.js
+++ scripts/HNG-02085-amaps.js
@@ -2,5 +2,5 @@ let fullName = "Omieibi Promise Amapakabao";
 let hngId = "HNG-02085";
 let lang = "Javascript";
 let email = "aomieibi@yahoo.com"
-let output = `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${lang} for stage 2 task`;
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${lang} for stage 2 task ${email}`;
 console.log(output);
