0f1b2cc3bdfdece17df1b4e1a0df261949ee9526
virginiah894 <virgyperry@gmail.com>
2020-06-03 14:24:52 +0300

    add email address

--- scripts/04021-virginiahperiah.py
+++ scripts/04021-virginiahperiah.py
@@ -1,4 +1,5 @@
 full_name = 'Virginiah Periah'
 hng_num = '04021'
 used_lang = 'Python'
+email = 'virgyperry@gmail.com'
 print(f" Hello World, this is {full_name} with HNGi7 ID {hng_num} using {used_lang} for stage 2 task.")
