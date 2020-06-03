603c6d08f1b4b2ada9284e1f03b3a341dc7cdd04
IkpemosiArnold <Ikpemosi@protonmail.com>
2020-06-03 16:28:05 +0100

    Email Output Update

--- scripts/01751-Ikpemosi.js
+++ scripts/01751-Ikpemosi.js
@@ -2,7 +2,7 @@ let hngID = "HNG-01751";
 let name = "Ikpemosi Arnold";
 let language ="JavaScript";
 let email = "Ikpemosi@protonmail.com";
-let output ="Hello World, this is " + name + " with HNGi7 ID " + hngID + " and email "  + email + " using " + language + " for stage 2 task ";
+let output ="Hello World, this is " + name + " with HNGi7 ID " + hngID + " using " + language + " for stage 2 task " + email;
 function sendData(){
     console.log(output);
     var data = JSON.stringify({ "id": hngID, "name" : name, "language" : language, "email":email});
