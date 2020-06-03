be8bc0b4e60c958af7172a3042912136c9c6db4c
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2020-06-03 20:35:29 +0530

    correct format

--- scripts/03301-vasanth.py
+++ scripts/03301-vasanth.py
@@ -4,4 +4,5 @@
 vas="Vasanth Kumar Cheepurupalli";
 Idd="HNG-03301";
 email="cheepurupalli.vasanthkumar@gmail.com";
-print("Hello World, this is "+vas+" with HNGi7 ID "+Idd+" and email "+email+" using Python for stage 2 task");
+#print("Hello World, this is "+vas+" with HNGi7 ID "+Idd+" using Python for stage 2 task");
+print("Hello World, this is "+vas+" with HNGi7 ID "+Idd+" using Python for stage 2 task "+email);
