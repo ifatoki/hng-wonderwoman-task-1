1d7ee75644b1323f28179ea0f5267272214f6f5a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2020-06-03 11:13:48 +0400

    Adding email to the output script

--- scripts/Obu-01214.php
+++ scripts/Obu-01214.php
@@ -4,6 +4,6 @@ $myId = "HND-01214";
 $languageUsed = "PHP";
 $email = "obu.junior.emmanuel@gmail.com";
 
-echo "Hello World, this is $myName with HNGi7 ID $myId using $languageUsed for stage 2 task";
+echo "Hello World, this is $myName with HNGi7 ID $myId and email $email using $languageUsed for stage 2 task";
 
 ?>
