9010538936e3319bb7946ab9ff02eaef0205c32a
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2020-06-03 22:34:32 +0100

    fix requested changes

--- scripts/06065-Adebayo.js
+++ scripts/06065-Adebayo.js
@@ -1,7 +1,7 @@
 let member = "Adebayo Ilerioluwa";
 let ID = "HNG-06065";
-let lang = "JavaScript"
+let email = 'adebayorilerioluwa@gmail.com";
+let lang = "JavaScript";
 
-let task = `Hello World this is ${member} with HNGi7 ID ${ID} using ${lang} for stage 2 task.`;
-
+let task = `Hello World, this is ${member} with HNGi7 ID ${ID} using ${lang} for stage 2 task ${email}`;
 console.log(task);
