3315e5cd0d8aecc170488ec3a4e1a9ef651d2ec1
khansa <khansa.amrouni@gmail.com>
2020-06-03 13:22:06 +0100

    adding email address to the text

--- scripts/04187-khansa.py
+++ scripts/04187-khansa.py
@@ -1,1 +1,1 @@
-print('Hello World, this is khansa AMROUNI with HNGi7 ID 04187 using Python for stage 2')
+print('Hello World, this is khansa AMROUNI with HNGi7 ID 04187 using Python for stage 2\n khansa.amrouni@gmail.com')
