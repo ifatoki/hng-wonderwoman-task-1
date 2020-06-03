5ce6ed930b39418304fdb983ca7c98931c6be4e8
Adelodun Adeola <oloritytylopeh@gmail.com>
2020-06-03 22:03:31 +0100

    deleted the duplicate script folder

--- scripts/02916-queenade.php
+++ scripts/02916-queenade.php
@@ -3,8 +3,9 @@
 $my_name = "Adelodun Adeola .M.";
 $hng_id = "HNG-02916";
 $lang = "PHP";
+$email = "amadelodun@gmail.com";
 
-echo "Hello World, this is " . $my_name . " with HNGi7 ID (" . $hng_id . ") using " . $lang . " for stage 2 task." ;
+echo "Hello World, this is " . $my_name . " with HNGi7 ID (" . $hng_id . ") using " . $lang . " for stage 2 task " . $email . ;
 
 ?>
 
