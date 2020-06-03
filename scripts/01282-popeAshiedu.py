6cec380964cc8d89dacef6160e00d44457b77b37
popeashiedu <popeashiedu@gmail.com>
2020-06-03 14:31:29 +0100

    Added email for popeAshiedu

--- scripts/01282-popeAshiedu.py
+++ scripts/01282-popeAshiedu.py
@@ -1,6 +1,7 @@
 full_name = 'Benedict Ashiedu'
 hng_id = 'HNG-01282'
 script_language = 'Python'
+gmail = 'popeashiedu@gmail.com'
 
-statement = f'Hello world, this is {full_name} with HNGi7 ID {hng_id} using {script_language} for stage 2 task'
+statement = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {script_language} for stage 2 task {gmail}'
 print(statement)
