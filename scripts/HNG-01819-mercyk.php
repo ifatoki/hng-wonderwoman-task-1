35b905965dafb07301e45cec147b132561f53efb
unknown <mercyjemosop@gmail.com>
2020-06-03 15:56:52 +0300

    My first task for team Wonderwoman

--- scripts/HNG-01819-mercyk.php
+++ scripts/HNG-01819-mercyk.php
@@ -6,7 +6,7 @@ $name='Mercy Kipyegon';
 $email="mercyjemosop@gmail.com";
 $language="PHP";
 $hello= "Hello world, this is ".$name.", Email ".$email." with HNGi7 ID ".$hngid. " using ".$language. " for stage 2 task, ";
-$str=array($hello, "id"=>$hngid, "name"=>$name, "email"=>$email, "language"=>$language);
+$str=array($hello, "name"=>$name, "id"=>$hngid,  "email"=>$email, "language"=>$language);
 print json_encode($str);
 
 ?>
