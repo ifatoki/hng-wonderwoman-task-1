fbcd3d33a6c0c0efe300ae1e522ce0c384b78e1d
ooakhu <ooakhu@gmail.com>
2020-06-03 05:22:36 -0400

    script now includes email

--- scripts/02011-Valerie.js
+++ scripts/02011-Valerie.js
@@ -1,6 +1,6 @@
-const stageTwoTask = (fullName, id, language) => {
-    if (fullName === 'Valerie Oakhu' && id === 'HNG-02011' && language === 'javascript') {
-        return (`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`);
+const stageTwoTask = (fullName, id, language, email) => {
+    if (fullName === 'Valerie Oakhu' && id === 'HNG-02011' && language === 'javascript' && email === 'ooakhu@gmail.com') {
+        return (`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. And my email address is ${email}.`);
     }
 }
-console.log(stageTwoTask('Valerie Oakhu', 'HNG-02011', 'javascript'));
+console.log(stageTwoTask('Valerie Oakhu', 'HNG-02011', 'javascript', 'ooakhu@gmail.com'));
