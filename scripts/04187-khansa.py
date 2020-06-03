44fdc4dac07d522232e746804e3aa1d7c3ec3014
khansa <khansa.amrouni@gmail.com>
2020-06-03 16:36:22 +0100

    fix script printed text

--- scripts/04187-khansa.py
+++ scripts/04187-khansa.py
@@ -1,1 +1,1 @@
-print('Hello World, this is khansa AMROUNI with HNGi7 ID 04187 using Python for stage 2\n khansa.amrouni@gmail.com')
+print('Hello World, this is khansa AMROUNI with HNGi7 ID 04187 using Python for stage 2 task khansa.amrouni@gmail.com')
