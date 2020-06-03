7318b6f04f0be2d2e6a713899efadd5cd18d03dd
Adenike Awofeso <adenikeawofeso@gmail.com>
2020-06-03 16:26:06 +0100

    First commit with changes to email

--- scripts/02586-Nikkifeso.js
+++ scripts/02586-Nikkifeso.js
@@ -1,4 +1,4 @@
-const script = (name, id, email,language) => {
+const script = (name, id,language,email) => {
     console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`);
  }
 
