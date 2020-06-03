abbcfc3bbeef70bb0aa760dbfe811706573c7572
korlahwarleh <jmkolawole@gmail.com>
2020-06-03 12:47:54 +0100

    Added script 05648-Korlahwarleh90.php (By JIMOH Mofoluwasho K.)

---
+++ scripts/05648-Korlahwarleh90.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$full_name = 'JIMOH Mofoluwasho K.';
+$ID = '05648';
+$language = 'PHP';
+
+
+$statement = 'Hello World, this is '.$full_name .' with HNGi7 ID '.$ID.' using '.$language.' for stage 2 task';
+echo $statement;
+
+?>
