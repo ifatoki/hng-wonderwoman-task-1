52936f0a15ff8afaabed291ecd32ea474969ebe2
Akinsiku Femi <akinsiku13@gmail.com>
2020-06-03 09:43:03 +0100

    Add an introductory script

---
+++ scripts/00504-Femi.js
@@ -0,0 +1,7 @@
+const display = (name, hngID,language) => {
+    return `Hello world, this is ${name} with HNGI7 ID ${hngID} using ${language} for stage 2 task`;
+    
+}
+
+display("Akinsiku OLuwafemi David", "00504", "Javascript");
+
