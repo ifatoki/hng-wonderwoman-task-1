6ba7fca17cd24f99bd3a551b29e26520d9a69661
Startoffs <adelekesays@gmail.com>
2020-06-03 16:34:41 +0100

    Update email corrected

--- scripts/HNG-02083-startoffs.js
+++ scripts/HNG-02083-startoffs.js
@@ -2,5 +2,5 @@ let fullName = "Morounfolu Adesanya";
 let HNGi7_ID = "HNG-02083";
 let language = "Javascript";
 let email = "adelekesays@gmail.com";
-const MESSAGE = `Hello World,this is ${fullName} with HNGi7 ID ${HNGi7_ID} and email ${email} using ${language} for stage 2 task.`;
+const MESSAGE = `Hello World,this is ${fullName} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2 task ${email}`;
 console.log(MESSAGE);
