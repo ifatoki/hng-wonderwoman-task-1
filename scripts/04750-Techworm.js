731e04b0f2256d9e0c82e16cecd7a8321dfd698e
ibe andyson <ibeandyson123@gmail.com>
2020-06-03 05:33:52 -0700

    added email to my scrip t#

--- scripts/04750-Techworm.js
+++ scripts/04750-Techworm.js
@@ -4,9 +4,10 @@ const outPut = () => {
 	var fullName = "Ibe Andyson Andrew";
 	var hngi7_id = "HNG-04750";
 	var language = "Javascript";
+	var email = "ibeandyson123@gmail.com"
 
 	console.log(
-		`Hello World, this is ${fullName} with HNGi7 ID ${hngi7_id} using ${language} for stage 2 task`
+		`Hello World, this is ${fullName} with HNGi7 ID ${hngi7_id} using ${language} for stage 2 task ${email}`
 	);
 };
 outPut();
