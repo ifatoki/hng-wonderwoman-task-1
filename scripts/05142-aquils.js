1aa5a8ea76891564f3935e4a6d908fb7afcd33c1
Aquila Afuadajo <aquilaafuadajo@gmail.com>
2020-06-03 22:20:26 +0100

    changing position of email again

--- scripts/05142-aquils.js
+++ scripts/05142-aquils.js
@@ -1,4 +1,4 @@
 (function() {
-  let sentence = "Hello World, this is Emmanuel Afuadajo with HNGi7 ID HNG-05142 and email aquilaafuadajo@gmail.com using javascript for stage 2 task"
+  let sentence = "Hello World, this is Emmanuel Afuadajo with HNGi7 ID HNG-05142 using javascript for stage 2 task aquilaafuadajo@gmail.com"
   return sentence
 })()
