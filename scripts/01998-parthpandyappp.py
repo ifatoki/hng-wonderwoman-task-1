0eef3ce64cbf08966a2517f982a700befc182a34
parthpandyappp <parthpandyappp@gmail.com>
2020-06-03 16:37:21 +0530

    Adding my script for stage 2

---
+++ scripts/01998-parthpandyappp.py
@@ -0,0 +1,7 @@
+My_name = "Parth Pandya"
+Lan_used = "Python"
+HNGi7_Id = "HNG-01998"
+
+print(f"Hello World, this is {My_name} with HNGi7 ID {HNGi7_Id} using {Lan_used} for stage 2 task", flush=True)
+
+
