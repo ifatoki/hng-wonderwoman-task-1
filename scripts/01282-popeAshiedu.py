edd6e784c11292b60f1c0f8a19c15664565f0c5a
popeashiedu <popeashiedu@gmail.com>
2020-06-03 22:59:49 +0100

    Added email in correct position

--- scripts/01282-popeAshiedu.py
+++ scripts/01282-popeAshiedu.py
@@ -2,6 +2,6 @@ full_name = 'Benedict Ashiedu'
 hng_id = 'HNG-01282'
 script_language = 'Python'
 gmail = 'popeashiedu@gmail.com'
-
-statement = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} and email {gmail} using {script_language} for stage 2 task'
+variables = ['Benedict Ashiedu', 'HNG-01282', 'Python',  'popeashiedu@gmail.com']
+statement = f'Hello World, this is {variables[0]} with HNGi7 ID {variables[1]} using {variables[2]} for stage 2 task {variables[3]} '
 print(statement)
