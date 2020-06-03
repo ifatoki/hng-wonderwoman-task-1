59b75aa4413bdd89bf8f7f5d8f916325268d90e5
testrungirl <laigbodioh@gmail.com>
2020-06-03 08:54:42 +0100

    Task 2: printed a sentence on Console via JS

---
+++ scripts/04760-Emike.js
@@ -0,0 +1,6 @@
+//Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task 
+const fullName = "Emike Aigbodioh";
+const ID = "04760";
+const language = "JavaScript"
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task.`);
