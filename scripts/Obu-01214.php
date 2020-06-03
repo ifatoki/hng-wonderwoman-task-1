7631cdd017d63b3fe14100e87853cf785d6beb77
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2020-06-03 14:49:35 +0400

    changed the position of email

--- scripts/Obu-01214.php
+++ scripts/Obu-01214.php
@@ -1,9 +1,9 @@
 <?php
 $myName = "Emmnanuel Obu Junior";
-$myId = "HND-01214";
+$myId = "HNG-01214";
 $languageUsed = "PHP";
 $email = "obu.junior.emmanuel@gmail.com";
 
-echo "Hello World, this is $myName with HNGi7 ID $myId and email $email using $languageUsed for stage 2 task";
+echo "Hello World, this is $myName with HNGi7 ID $myId using $languageUsed for stage 2 task $email";
 
 ?>
