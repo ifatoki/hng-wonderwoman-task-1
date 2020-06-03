5ea2d79bb6b08aa9514f4087a111165ec40775b1
Phil Takon <philtakon@Phils-MacBook-Air.local>
2020-06-03 21:15:18 +0100

    my Hng7i task Tutu

---
+++ scripts/03122-Tutu_.py
@@ -0,0 +1,12 @@
+"""
+File: pyramid.py
+----------------
+HNG first python task
+"""
+
+def main():
+    print('Hello World, this is Takon Judith with HNGi7 ID HNG-03122 using python for stage 2 task')
+
+
+if __name__ == '__main__':
+    main()
