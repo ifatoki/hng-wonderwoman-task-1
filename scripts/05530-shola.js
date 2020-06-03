f6b426bae521cae2234c7089afc6e4eedeac0d28
Victor Samuel <54833975+sholance@users.noreply.github.com>
2020-06-03 19:36:13 +0100

    email address update

--- scripts/05530-shola.js
+++ scripts/05530-shola.js
@@ -2,6 +2,6 @@ let language = "javascript";
 let email ="ohksam@gmail.com";
 let id ="HNG-05530";
 let name ="Kolade Victor";
-let output =`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
+let output =`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
 
 console.log(output);
