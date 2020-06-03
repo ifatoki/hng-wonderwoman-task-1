7ef10d97362e7655b39586bfb518e2fd5c6d3363
ogunsolu007 <63315246+ogunsolu007@users.noreply.github.com>
2020-06-03 22:23:05 +0100

    Update 04828-ogunsolu.js

--- scripts/04828-ogunsolu.js
+++ scripts/04828-ogunsolu.js
@@ -4,7 +4,4 @@
 
 console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using Javascript for stage 2 task`);
 
-let firstName = 'John';
-let lastName = 'Smith';
 
-console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
