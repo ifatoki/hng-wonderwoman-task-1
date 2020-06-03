853ddd1aa99920fa63bb81935c4c273ce36de657
Kehinde Adebayo <33664118+004Kenny@users.noreply.github.com>
2020-06-03 15:56:16 +0100

    Add files via upload

---
+++ scripts/03101-Kehinde.js
@@ -0,0 +1,8 @@
+const firstName = "Kehinde";
+const lastName = "Adebayo";
+const email_address= "adebayo.kenny240@gmail.com";
+
+const val_id = 03101
+
+console.log("Hello World, this is" + firstName + " " + lastName + "with HNG-ID:"+val_id + 
+	"using Javascript for stage 2 task. " + email_address);
