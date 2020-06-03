515dd980e2e99b2edb44dcc2a94bb6507e7984b2
dijayst <45679945+dijayst@users.noreply.github.com>
2020-06-03 18:25:15 +0100

    second task

---
+++ scripts/03205-olubiyisther.py
@@ -0,0 +1,5 @@
+hello="Hello world"
+name="Olubiyi Imoleayo Esther"
+hng ="hngi id 03205"
+using="python"
+print(hello +",\n this is "+ name + ",\n with " + hng + ",\n using " + using + " for stage 2 task .")
