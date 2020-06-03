b490d69336cacb8a88c9ce97962d35f376193920
Codeleeq <princefemibabs@gmail.com>
2020-06-03 09:26:06 +0100

    corrected version. so as not to fail

--- scripts/HNG-05367-itsmaleeq.js
+++ scripts/HNG-05367-itsmaleeq.js
@@ -1,1 +1,1 @@
-document.write("Hello World, this is ABULMALEEK BABAJIDE with HNGi7 ID HNG-05367 using javascript for stage 2 task");
+document.write("Hello World, this is BABAJIDE ONAYEMI with HNGi7 ID HNG-05367 using javascript for stage 2 task");
