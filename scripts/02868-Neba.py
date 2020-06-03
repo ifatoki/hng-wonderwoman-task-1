d761ac21cd40aa476869cd357235a134b130cd3c
Roland-coder <nebaroland9@gmail.com>
2020-06-03 13:42:38 +0200

    Neba's script

--- scripts/02868-Neba.py
+++ scripts/02868-Neba.py
@@ -1,7 +1,7 @@
 def task1(fullname, ID, language):
     return "Hello World, this is " + fullname + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task"
 
-fullname = "Neba"
+fullname = "Neba Roland"
 ID = "HNG-02868"
 language = "Python"
 
