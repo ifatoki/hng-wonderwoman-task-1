8cf27b013fc8f2c0643a2367f9978747758938ee
Petersheg <43689575+Petersheg@users.noreply.github.com>
2020-06-03 13:13:31 +0100

    Update 05916.oluwasegun.js
    Add email address to the output .

--- scripts/05916-oluwasegun.js
+++ scripts/05916-oluwasegun.js
@@ -3,10 +3,10 @@
     "Hello world, this is Peter Oluwasegun with HNGi7 ID 192511 using Javascript for stage 2 task."
 */ 
 
-function outPutDetails(name,id,lang){
+function outPutDetails(name,id,lang,email){
 
     console.log(`
-        Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.
+        Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task ${email}.
     `);
 }
-outPutDetails('Peter Oluwasegun','HNG-05916','JavaScrip');
+outPutDetails('Peter Oluwasegun','HNG-05916','JavaScript','Petersheg@gmail.com');
