689b3b8cb7202db7575abb877bd8433cb0e39b41
Adelodun Adeola <oloritytylopeh@gmail.com>
2020-06-03 02:24:03 +0100

    modified the code a bit

--- scripts/02916-queenade.php
+++ scripts/02916-queenade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HNGi7 Task 2</title>
-</head>
-<body>
 <?php
 
 $my_name = "Adelodun Adeola .M.";
@@ -15,5 +7,4 @@ $lang = "PHP";
 echo "Hello World, this is " . $my_name . " with HNGi7 ID (" . $hng_id . ") using " . $lang . " for stage 2 task." ;
 
 ?>
-</body>
-</html>
+
