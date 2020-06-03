dd74ce6f04538aac120af199425426727a8d11b8
Akinsiku Femi <akinsiku13@gmail.com>
2020-06-03 10:09:31 +0100

    Add email parameter to script

--- scripts/00504-Femi.js
+++ scripts/00504-Femi.js
@@ -1,7 +1,7 @@
-const display = (name, hngID,language) => {
-    return `Hello world, this is ${name} with HNGI7 ID ${hngID} using ${language} for stage 2 task`;
+const display = (name, hngID,language,email) => {
+    return `Hello World,this is ${name} with HNGi7 ID ${hngID} and email ${email} using ${language} for stage 2 task`;
     
 }
 
-display("Akinsiku OLuwafemi David", "00504", "Javascript");
+display("Akinsiku OLuwafemi David", "HNG-00504", "Javascript", "akinsiku.o@yahoo.com");
 
