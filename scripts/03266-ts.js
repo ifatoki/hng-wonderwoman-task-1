6704432fbee6398a35944194ba00fa57c00c0964
sulaimon23 <tsulaimon21@yahoo.com>
2020-06-03 21:19:42 +0100

    ts commit

---
+++ scripts/03266-ts.js
@@ -0,0 +1,7 @@
+let name = 'Taiwo Sulaimon';
+let id = 'HNG-03266';
+let mail = 'tsulaimon96@gmail.com';
+
+let text = `Hello World, this is ${name} with HNGi7 ID ${id} using javascript for stage 2 task ${mail}`
+
+console.log(mail)
