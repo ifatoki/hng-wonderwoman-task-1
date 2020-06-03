e1ba9f9082ae3cd19dbfded830711b26205c7ed4
Adexandria <adeolaaderibigbe09@gmail.com>
2020-06-03 13:41:21 +0100

    Added my file

--- scripts/02604-Adeolaade.php
+++ scripts/02604-Adeolaade.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello World, this is Adeola Aderibigbe with HNGi7 ID HNG-02604 using php for stage 2 task";
+echo "Hello World, this is Adeola Aderibigbe with HNGi7 ID HNG-02604 using php for stage 2 task adeolaaderibigbe09@gmail.com";
 ?>
