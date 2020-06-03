01a7ff59fb137fead27df154481c4a9e5f381892
jenyfah <66225658+jenyfah@users.noreply.github.com>
2020-06-03 22:29:34 +0100

    Update 01278-Jenny.js

--- scripts/01278-Jenny.js
+++ scripts/01278-Jenny.js
@@ -1,8 +1,8 @@
 myFunction();
 
 function myFunction()
-{let myName = "Jennifer";
+ let myName = "Jennifer";
  let id = "HNG-01278";
  let language = "JavaScript";
  let email = "osejoseph33@gmail.com";
- console.log(`Hello world, this is ${myName} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`)}
+ console.log(`Hello world, this is ${myName} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`)
