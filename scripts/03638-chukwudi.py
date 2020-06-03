05e01e7448bfa95b2d14a27565365420be2a93e3
Wowo-chukwudi <woworoseline@gmail.com>
2020-06-03 13:58:29 +0000

    Updated task for team wonderwoman

--- scripts/03638-chukwudi.py
+++ scripts/03638-chukwudi.py
@@ -1,1 +1,6 @@
-print('Hello World, this is Wowo Chukwudi with HNGi7 ID HNG-03638 using Python for stage 2 task')
+name = 'Wowo Chukwudi'
+hngi7_id = 'HNG-03638'
+email_address = 'woworoseline@gmail.com'
+language_used = 'python'
+
+print(f'Hello World, this is {name} with HNGi7 ID {hngi7_id} using {language_used} for stage 2 task {email_address}')
