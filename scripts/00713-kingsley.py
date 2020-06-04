a98dc693e2782642e78b2bd0e692f15ce0a48678
okpara kingsley <buteng2000@yahoo.com>
2020-06-04 22:14:31 +0100

    hngi7 task one

--- scripts/00713-kingsley.py
+++ scripts/00713-kingsley.py
@@ -3,4 +3,4 @@ id ='HNG-00713'
 language ='python'
 email ='buteng2000@yahoo.com'
 output=f'Hello world,this is {name} with HNGi7 ID {id} using {language} for stage 2 task {email}'
-print =(output)
+print(output)
