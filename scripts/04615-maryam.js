120f5d909e64b5cf72c70b7de07ac979b12341dc
Maryam Awesu <maryamawesu29@gmail.com>
2020-06-04 22:16:55 +0100

    Task 2 reviewed

--- scripts/04615-maryam.js
+++ scripts/04615-maryam.js
@@ -6,4 +6,4 @@ const _id = `HNG-04615`;
 
 const _language_ = `JavaScript`;
 
-console.log(`Hello World, this is ${_name} with HNGi7 ID ${_id} and email ${_email} using ${_language_} for stage 2 task`);
+console.log(`Hello World, this is ${_name} with HNGi7 ID ${_id} and using ${_language_} for stage 2 task ${_email}`);
