5af00b8eec7f816205d836a23c9b698a92a23dd3
Halimah <64546668+devhalimah@users.noreply.github.com>
2020-06-04 14:43:29 -0700

    Update HNG-05337-halimah.php

--- scripts/HNG-05337-halimah.php
+++ scripts/HNG-05337-halimah.php
@@ -6,6 +6,6 @@ $language = "PHP";
 $email_address = "abdulazeezhaleemah@gmail.com";
 
 
-echo "Hello World, this is " .' '. $full_name .' '. " with HNGi7 ID " .' '. $id .' '. " using " .' '. $language .' '. " for stage 2 task " .' '. $email_address;
+echo "Hello World, this is ".$full_name." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email_address;
 
 ?>
