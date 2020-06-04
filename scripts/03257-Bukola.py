709179a2752e6e4a643eb6a14a5236fa87656cfe
B-ID <66232758+B-ID@users.noreply.github.com>
2020-06-04 16:45:45 +0100

    Update 03257-Bukola.py

--- scripts/03257-Bukola.py
+++ scripts/03257-Bukola.py
@@ -1,8 +1,8 @@
 def stage2():
-    first_name = "Bukola Idowu"
+    full_name = "Bukola Idowu"
     ID = "HNG-03257"
     language = "python"
     email = "idowu98official@gmail.com"
-    print("Hello world, this is " + full_name + " with HNG17 ID " + str(ID) + " using " + language + "for stage 2 task" + email)
+    print("Hello world, this is " + full_name + " with HNG17 ID " + str(ID) + " using " + language + " for stage 2 task " + email)
 
 stage2()
