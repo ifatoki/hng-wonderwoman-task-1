123c91d74d6a3292c7bac19d68b15af2da328dc6
bolexs <65228354+bolexs@users.noreply.github.com>
2020-06-04 02:58:21 +0100

    Update 04095-bolex.py

--- scripts/04095-bolex.py
+++ scripts/04095-bolex.py
@@ -1,3 +1,3 @@
 #python
 
-print ('hello world, this is adeojo bolu with HNGi7 ID 04095 using python for stage 2 task boluwatufeadeojo@gmail.com')
+print ('hello world, this is adeojo bolu with HNGi7 ID--04095 using python for stage 2 task boluwatufeadeojo@gmail.com')
