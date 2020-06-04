d0864cb3384817aa3e7b73a474b15b78e4d4f67e
Vicwin13 <vnwimo13@gmail.com>
2020-06-04 23:25:11 +0100

    made required adjustments

--- scripts/00359-winner.js
+++ scripts/00359-winner.js
@@ -3,7 +3,7 @@ let ID = "HNG-00359";
 let lang = "JavaScript"
 let email = "vnwimo13@gmail.com"
 
-let task = `Hello World, this is ${member} with HNGi7 ID ${ID} and email ${email} using ${lang} for stage 2 task.`;
+let task = `Hello World, this is ${member} with HNGi7 ID ${ID} using ${lang} for stage 2 task ${email}`;
 
 console.log(task);
 
