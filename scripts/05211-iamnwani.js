cfc8a2f9540862cdf51d71f8625b81d582f0b61e
victory <vickywane@gmail.com>
2020-06-05 00:51:30 +0100

    Update 05211-iamnwani.js

--- scripts/05211-iamnwani.js
+++ scripts/05211-iamnwani.js
@@ -1,6 +1,7 @@
 let Myname = 'Victory Nwani';
 let MyHNGID = 'HNG-05211';
+let personal_mail= 'vickywane@gmail.com'
 
 console.log(
-  `Hello World, this is ${Myname} with HNGi7 ID ${MyHNGID} using JavaScript for stage 2 task`
-);
+  `Hello World, this is ${Myname} with HNGi7 ID ${MyHNGID} using JavaScript for stage 2 task ${personal_mail}`
+  )
