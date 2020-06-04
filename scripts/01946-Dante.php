fb99f72dbd75648817f8994a5b602674f01b51e0
David Dante Frank <45837897+DanteFrank@users.noreply.github.com>
2020-06-04 20:46:22 +0100

    Changes To Name
    My script ran failed. Made some changes

--- scripts/01946-Dante.php
+++ scripts/01946-Dante.php
@@ -1,5 +1,5 @@
 <?php
 
-$myData = "Hello World, this is David Dante Frank and my HNG Id is HNG-01946 and I am using PHP for stage 2 task davidfrankoziwo@gmail.com";
+$myData = "Hello World, this is Dante Frank and my HNG ID is HNG-01946 and I am using PHP for stage 2 task davidfrankoziwo@gmail.com";
 
 echo $myData;
