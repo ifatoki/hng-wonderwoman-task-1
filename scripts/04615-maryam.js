8cf3f742016f289087565d1d0f10ef09d2ee9e7f
Maryam Awesu <maryamawesu29@gmail.com>
2020-06-04 22:51:34 +0100

    task 2 modified

--- scripts/04615-maryam.js
+++ scripts/04615-maryam.js
@@ -6,8 +6,4 @@ const _id = `HNG-04615`;
 
 const _language_ = `JavaScript`;
 
-<<<<<<< HEAD
 console.log(`Hello World, this is ${_name} with HNGi7 ID ${_id} and using ${_language_} for stage 2 task ${_email}`);
-=======
-console.log(`Hello World, this is ${_name} with HNGi7 ID ${_id} using ${_language_} for stage 2 task email ${_email}`);
->>>>>>> 11348a6f05fbdae576074a082bcd7d385e77f076
