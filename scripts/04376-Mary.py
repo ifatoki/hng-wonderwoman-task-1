5df3693626504455391fcc6a76506175fea0e7e5
Bilewu Mary <bilewumary@gmail.com>
2020-06-04 16:38:00 +0100

    my script

---
+++ scripts/04376-Mary.py
@@ -0,0 +1,6 @@
+>>> My_name = "Mary OgoOluwa Bilewu"
+>>> Lan_used = "Python"
+>>> HNGi7_ID = "HNG-04376"
+>>> 
+>>> print(f"Hello World, this is {My_name} with HNGi7 ID {HNGi7_ID} using {Lan_used} for stage 2 task", flush=True)
+Hello World, this is Mary OgoOluwa Bilewu with HNGi7 ID HNG-04376 using Python for stage 2 task
--- scripts/04376-Mary.py.py
+++
@@ -1,4 +0,0 @@
-Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('Hello World, this is Mary OgoOluwa Bilewu with HNGi7 ID HNG-04376 using python for stage 2 task bilewumary@gmail.com' )
-Hello World, this is Mary OgoOluwa Bilewu with HNGi7 ID HNG-04376 using python for stage 2 task bilewumary@gmail.com
