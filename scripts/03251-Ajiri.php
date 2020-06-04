47880311aa8bda00dca29a9633207e74a75c37fb
unknown <ajiriosiobe@gmail.com>
2020-06-04 19:22:59 +0100

    updated 03251-Ajiri

--- scripts/03251-Ajiri.php
+++ scripts/03251-Ajiri.php
@@ -1,5 +1,5 @@
 <?php
 
-    echo"Hello World, this is OSIOBE ENOCH AJIRI with HNGi7 ID 03251 using php for stage 2 task";
+    echo"Hello World, this is OSIOBE ENOCH AJIRI with HNGi7 ID HNG-03251 using php for stage 2 task";
 
 ?>
