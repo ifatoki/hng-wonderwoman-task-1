3ff53a8726956c33e01bc7ca14ce260797b1e52e
Kehinde Adebayo <33664118+004Kenny@users.noreply.github.com>
2020-06-04 21:10:42 +0100

    made changes to hng name.

--- scripts/03101-Kehinde.js
+++ scripts/03101-Kehinde.js
@@ -3,6 +3,6 @@ const lastName = "Adebayo";
 const email_address= "adebayo.kenny240@gmail.com";
 
 const val_id = 03101
-
-console.log("Hello World, this is" + firstName + " " + lastName + "with HNG-ID:"+val_id + 
+const val_id_2 = "HNG-"
+console.log("Hello World, this is" + firstName + " " + lastName + "with HNG-ID:"val_id_2+val_id + 
 	"using Javascript for stage 2 task. " + email_address);
