d0ef87a500216f968da91f41387eb3d40d197578
jaybarnes33 <ohenesetwumasi@gmail.com>
2020-06-04 09:43:55 +0000

    Apply requested changes

--- scripts/02459-barnes.js
+++ scripts/02459-barnes.js
@@ -2,9 +2,9 @@
 const name = "Oduro Twumasi John Barnes";
 const hngID = "HNG-02459";
 const email = "ce-jbodurotwumasi4619@st.umat.edu.gh";
-
-function task (name, hngID, email){
-  console.log("Hello World, this is " + name + " with HNGi7 ID " + hngID + " and email " + email + " using javascript for stage 2 task");
+const language = "javascript"
+function task (name, hngID, email,language){
+  console.log("Hello World, this is " + name + " with HNGi7 ID " + hngID  + " using "+ language + " for stage 2 task " + email);
 }
 
-task(name,hngID, email);
+task(name,hngID, email,language);
