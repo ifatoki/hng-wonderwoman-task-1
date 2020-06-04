236768e79c2b6d9c66747eccb6e765750838a48d
jude okoroafor <judeokoroafor@gmail.com>
2020-06-05 06:49:14 +0800

    Update

--- scripts/01442-judeokoroafor.php
+++ scripts/01442-judeokoroafor.php
@@ -5,6 +5,6 @@ $language = "php";
 $stageNum = 2;
 $email = "judeokoroafor@gmail.com";
 
-echo ("Hello World, this is " .$fullName. " with HNGi7 ID " .$id. " using " .$language . " for stage " .$stageNum . " task" . $email);
+echo ("Hello World, this is " .$fullName. " with HNGi7 ID " .$id. " using " .$language . " for stage " .$stageNum . " task " . $email);
 
 ?>
