9831f42c1bb23e0be226235e6f2dd82dec135bfb
Carol Musyoka <carolmusyoka127@gmail.com>
2020-06-04 10:22:02 +0300

    Delete 01810-Carol.py

--- scripts/01810-Carol.py
+++
@@ -1,7 +0,0 @@
-name = "Carol Musyoka"
-ID = "HNG-01810"
-language = "Python"
-email = "carolmusyoka127@gmail.com"
-twitter = "@carolmusyoka_"
-
-print("Hello World, this is " + name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task " + email + twitter "on twitter" )
