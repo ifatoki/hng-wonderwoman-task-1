e0e9ef392caf7c8413290c6dc0f42b9ae0d3fd04
maryaamox <sulaiman.maryam@yahoo.co.uk>
2020-06-04 11:11:06 +0100

    Update 03774-Maryam.c

--- scripts/03774-Maryam.c
+++ scripts/03774-Maryam.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-printf("Hello world, this is Maryam Sulaiman with HNGi7 ID HNG-03774 and email sulaiman.maryam@yahoo.co.uk using C for stage 2 task");
+printf("Hello world, this is Maryam Sulaiman with HNGi7 ID HNG-03774 using C for stage 2 task sulaiman.maryam@yahoo.co.uk");
 return 0;
 }
