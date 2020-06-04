55920077e20c5f3b39fe770b4bc0ee83b43240cf
someone <someone@someplace.com>
2020-06-04 14:13:28 +0000

    addiny my script for stage two

---
+++ scripts/HNG-04036.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$fullName = "kwaku antwi";
+$id = "HNG-04036";
+$programming_language = "PHP";
+$email =  "kwaku.takyi@gmail.com";
+
+ $output = "Hello World, this is $fullName with HNGi7 ID $id using $programming_language for stage 2 task. $email";
+
+ print $output;
+
+
+
+
+?>
