22018e4f626303beb963711c88e2a6da7e8e4dc6
JoyKui33 <45483359+JoyKui33@users.noreply.github.com>
2020-06-04 20:05:28 +0300

    Correction

--- scripts/02274-joywan.js
+++ scripts/02274-joywan.js
@@ -1,10 +1,11 @@
 let name = "Joy Wangui";
 let id = "HNG-02274";
 let language ="Javascript";
+let email = "feliwangui@gmail.com";
 
 
 function showMessage(){
-    console.log('Hello World, this is '+name+' with HNGi7 ID '+id+' using'+language+' for stage 2 task feliwangui@gmail.com')
+    console.log('Hello World, this is '+name+' with HNGi7 ID '+id+' using '+language+' for stage 2 task '+email);
 }
 showMessage();
 
