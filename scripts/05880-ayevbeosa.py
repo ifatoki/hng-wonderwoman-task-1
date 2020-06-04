280570c8b7633cb51e5b31d0cdbaab693ebddecd
Ayevbeosa Iyamu <ayevbeosa.j@gmail.com>
2020-06-04 12:39:53 +0100

    Removed unnecessary period

--- scripts/05880-ayevbeosa.py
+++ scripts/05880-ayevbeosa.py
@@ -1,1 +1,1 @@
-print('Hello World, this is {} with HNGi7ID {} using {} for stage 2 task {}.'.format('Ayevbeosa Iyamu', 'HNG-05880', 'Python', 'ayevbeosa.j@gmail.com'))
+print('Hello World, this is {} with HNGi7ID {} using {} for stage 2 task {}'.format('Ayevbeosa Iyamu', 'HNG-05880', 'Python', 'ayevbeosa.j@gmail.com'))
