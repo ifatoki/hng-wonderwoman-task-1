4a2d1bb926e528890eac70cf140ce00219dac062
TheKmama <kanyinb@gmail.com>
2020-06-04 12:46:24 +0100

    Create 02140-ItsKmama.py

---
+++ scripts/02140-ItsKmama.py
@@ -0,0 +1,6 @@
+full_name = 'Olukanyinsola Olomi'
+ID = 'HNG-02140'
+Language = "Python"
+Email = 'kanyinb@gmail.com'
+
+print("Hello world, this is", full_name, "with HNGi7 ID", ID, "using", Language, "for stage 2 task.", Email, )
