20b854a1fe7771ea0ff90f8afbcecaf9ca495feb
oyindamola23 <49127453+oyindamola23@users.noreply.github.com>
2020-06-04 14:48:21 -0700

    Create 00825-Oyindamola.js

---
+++ 00825-Oyindamola.js
@@ -0,0 +1,7 @@
+var fullname = "Kareem Taiwo";
+var ID = "HNG-00825"
+var language = "Javascript";
+var email = "oyindamolataiwo23@gmail.com";
+
+
+console.log("Hello World, this is " + fullname + " "  + "with HNGi7 ID " +  ID + " using " + language + " for stage 2 task " + email);
