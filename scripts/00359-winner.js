e824582430c502d7f2b1ddd9d1ef821a4837de6b
Vicwin13 <vnwimo13@gmail.com>
2020-06-04 12:56:14 +0100

    addition of email

--- scripts/00359-winner.js
+++ scripts/00359-winner.js
@@ -1,8 +1,9 @@
 let member = "Victor Nwimo";
 let ID = "HNG-00359";
 let lang = "JavaScript"
+let email = "vnwimo13@gmail.com"
 
-let task = `Hello World this is ${member} with HNGi7 ID ${ID} using ${lang} for stage 2 task.`;
+let task = `Hello World, this is ${member} with HNGi7 ID ${ID} and email ${email} using ${lang} for stage 2 task.`;
 
 console.log(task);
 
