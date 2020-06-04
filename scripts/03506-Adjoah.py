6090a9ab56f30ba88ee40137a5f22a4e76e79f25
Adjoah <adwoaserwaaboafo@gmail.com>
2020-06-04 10:42:02 +0000

    created first commit with first script

---
+++ scripts/03506-Adjoah.py
@@ -0,0 +1,8 @@
+# declaring variables
+
+name='Adwoa Serwaa Boafo'
+my_id='03506'
+email_add='adwoaserwaaboafo@gmail.com'
+
+#print statement
+print("Hello World, this is ",name + " with "+ my_id +" using Python for stage 2 task "+ email_add)
