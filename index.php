b2bc99222afe17ae5ad48308b9143da8c0f1d4a2
Onifs10 <onifadebolu64@gmail.com>
2020-06-04 11:37:22 +0100

    update

--- index.php
+++ index.php
@@ -1,33 +1,58 @@
 <?php
-// $pattern = '/Hello World, this is [.*\s*]+with HNGi7 ID HNG-[\d*] using [.*] for stage 2 task/';
-$pattern = '/Hello World, this is (.{1,}\s)+with HNGi7 ID HNG-\d{5} using (.*) for stage 2 task (.+)@(.+).(com|COM)$/';
+header('Content-Type: application/json');
+function refine($output, $pattern, $filename)
+{
+    $filename_array = explode('/', $filename);
+    $filename = $filename_array[1];
+    $pass = preg_match($pattern, $output);
+    if ($pass) {
+        $status = "pass";
+        $array_of_sentence = explode(" ", $output);
+        $email = end($array_of_sentence);
+    } else {
+        $status = "fail";
+        $email = 'nil';
+    }
+    $id_search = preg_match('/HNG-\d{5}/', $output, $id_array);
+    if ($id_search) {
+        $hng_id = $id_array[0];
+    } else {
+        $hng_id = 'nill';
+    }
+    $name_search = preg_match('/\bis (.{1,}\s)+with/', $output, $name_array);
+    if ($name_search) {
+        $name = $name_array[1];
+    } else {
+        $name = 'nill';
+    }
+    $lang_search = preg_match('/using (.*) for/', $output, $lang_array);
+    if ($lang_search) {
+        $language = $lang_array[1];
+    } else {
+        $language = 'nill';
+    }
+    $result = array('file' => $filename, 'output' => $output, 'name' => $name, 'id' => $hng_id, 'email' => $email, 'language' => $language, 'status' => $status);
+    return $result;
+}
+$pattern = '/Hello World, this is (.{1,}\s)+with HNGi7 ID HNG-\d{5} using (.*) for stage 2 task (.+)@(.+).(com|COM)$/i';
+$data = array();
 foreach (glob('scripts/*.js') as $filename) {
-    $r = shell_exec("node $filename");
-    $parts = preg_match($pattern, $r);
-    echo $r . "</br>" . $parts . "</br>";
+    $output = shell_exec("node $filename");
+    $output_array = refine($output, $pattern, $filename);
+    array_push($data, $output_array);
 }
 
 foreach (glob('scripts/*.py') as $filename) {
-    $r = shell_exec("python3 $filename");
-    $parts = preg_match($pattern, $r);
-    echo $r . "</br>" . $parts . "</br>";
+    $output = shell_exec("python3 $filename");
+    $output_array = refine($output, $pattern, $filename);
+    array_push($data, $output_array);
 }
 foreach (glob('scripts/*.php') as $filename) {
-    $r = shell_exec("php $filename");
-    $parts = preg_match($pattern, $r);
-    echo $r . "</br>" . $parts . "</br>";
+    $output = shell_exec("php $filename");
+    $pass = preg_match($pattern, $output);
+    $output_array = refine($output, $pattern, $filename);
+    array_push($data, $output_array);
 }
 
-$sample = 'Hello World, this is Onifade Boluwatife with HNGi7 ID HNG-12345 using python for stage 2 task';
-$test = preg_match($pattern, $sample);
-echo ($test);
-// $pattern = "/[.*\s*]+/";
-// $text = "My favourite colors are red, green and blue";
-// $parts = preg_match($pattern, $text);
-
-// echo $parts;
-
-// $pattern = "/ca[kf]e/";
-// $text = "He was eating cake in the cafe.";
-// $matches = preg_match_all($pattern, $text, $array);
-// echo $matches . " matches were found."
+$encodedJSON = json_encode($data, JSON_PRETTY_PRINT);
+echo $encodedJSON;
