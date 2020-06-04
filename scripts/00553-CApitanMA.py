3a35b70c0347b3f6c698e25a2f0fa76e5df7b619
salami17 <muhahid18@gmail.com>
2020-06-04 05:23:40 +0100

    Task 2 submission

--- scripts/00553-CApitanMA.py
+++ scripts/00553-CApitanMA.py
@@ -2,4 +2,4 @@ name='Muhammad-ahid Abdulsalam'
 ID='HNG-00553'
 language='python'
 email='muhahid18@gmail.com'
-print("Hello world,this is %s with HNGI7 ID %s and email %s using %s for stage 2 task "%(name,ID,email,language))
+print("Hello world,this is %s with HNGI7 ID %s using %s for stage 2 task.%s "%(name,ID,language,email,))
