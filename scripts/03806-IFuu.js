84e5273fed2732f6d9b159772390d0370e9056a9
Eucharia Ifunanya <60736117+ifuu16@users.noreply.github.com>
2020-06-04 23:27:06 +0100

    Create 03806-IFuu.js
    js printing with js

---
+++ scripts/03806-IFuu.js
@@ -0,0 +1,10 @@
+//variable declaration
+let fullName = "Chiezie Ifunanya Eucharia";
+let hngId = "HNG-03806"
+let language = "javascript";
+let email = "chiezieifunanya08@gmail.com";
+
+//actual code
+
+output = "Hello World, this is" + " "+  fullName + " " + " with HNGi7 ID" +" " + hngId + " using" +" " + language +" " + " for stage 2 task" +" " + email;
+console.log(output);
