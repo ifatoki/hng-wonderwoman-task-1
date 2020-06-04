c79f6ca587ac89a99ee6b13b99477706e1e83138
Goodness chris-ugari <goodnesschrisugari@yahoo.com>
2020-06-04 23:04:32 +0100

    task 2

---
+++ scripts/05563-onyinyebeks.js
@@ -0,0 +1,5 @@
+let name = "Goodness Chris-Ugari";
+let id = "HNG-05563";
+let email = "goodnesschrisugari@yahoo.com";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using JavaScript for stage 2 task ${email}`);
