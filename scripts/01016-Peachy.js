2f7080d589dede9b506c1de2cf6d183f7c7c8a85
Peachy <temmynladejesu@gmail.com>
2020-06-04 12:10:49 +0100

    New Update 01016-Peachy.js

--- scripts/01016-Peachy.js
+++ scripts/01016-Peachy.js
@@ -1,7 +1,6 @@
 const fullName = 'Temilade Opanuga';
 const hngId = "HNG-01016";
 const lang = "JavaScript";
-const email = "temmynladejesu.gmail.com"
-
+const email = "temmynladejesu.gmail.com";
 const result = (`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${lang} for stage 2 task ${email}`);
 console.log (result);
