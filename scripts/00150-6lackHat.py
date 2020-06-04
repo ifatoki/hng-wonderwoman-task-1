0956409015605e4f74fd149d23fc92aed4316229
Paul Ogolla <paulotieno2@gmail.com>
2020-06-04 20:59:09 +0300

    add email to script

--- scripts/00150-6lackHat.py
+++ scripts/00150-6lackHat.py
@@ -1,8 +1,10 @@
-def Hello(full_name, ID, language):
-    return "Hello World, this is {} with HNG ID {} using {} for stage 2 task".format(full_name, ID, language)
+def Hello(full_name, ID, language, email):
+    return "Hello World, this is {} with HNG ID {} using {} for stage 2 task".format(full_name, ID, language,email)
 
 if __name__ == '__main__':
     full_name = "Paul Ogolla"
     ID = "HNG-00150"
     language = "Python"
-    print(Hello(full_name, ID, language), flush=True)
+    email = "paulotieno2@gmail.com"
+    print(Hello(full_name, ID, language, email), flush=True)
+    
