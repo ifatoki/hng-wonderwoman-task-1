9e9d2f03bdfe6e543a82bb59c13505755e3fe71c
Umohesther10 <59563888+Umohesther10@users.noreply.github.com>
2020-06-04 16:20:45 +0100

    modified codes

--- scripts/03407-Esther.js
+++ scripts/03407-Esther.js
@@ -1,15 +1,18 @@
-const name = "Umoh Esther";
-const hng_id = "HNG-03407";
-const lang = "JavaScript";
-const email = "umohesther08@gmail.com";
 
-console.log(
-  "Hello World, this is " +
-    name +
-    " with HNGi7 ID " +
-    hng_id +
-    " using " +
-    lang +
-    " for stage 2 task " +
-    email
-);
+function show(){
+
+var details =
+`{
+    "name": "Umoh Esther",
+    "hng_id": "HNG-03407",
+    "language": "JavaScript",
+    "email":"umohesther08@gmail.com"
+}`
+
+var json = JSON.parse(details);
+
+console.log(`Hello World, this is ${json.name} with HNGi7 ID ${json.hng_id} using ${json.language} for stage 2 task and ${json.email}`);
+
+}
+
+show();
