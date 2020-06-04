79b6886476d5d31dfb8e59b16613d446a4e3dfc4
Stellamarissomto <stellamarissomto@gmail.com>
2020-06-04 18:16:15 +0100

    adding my script file

---
+++ scripts/02425-maris.js
@@ -0,0 +1,8 @@
+const myName = 'ikeonyia Somtochukwu Stellamaris';
+const id = 'HNG-12425';
+const email = 'stellamarissomto@gmail.com';
+const lang = 'javascript';
+
+console.log(`Hello World, this is ${myName} with HNGI7 id ${id} using ${lang} for stage 2 task ${email}`);
+
+
