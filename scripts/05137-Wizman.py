82f2a7e9c612c0f5d99e52c28bde054760e47eab
angels21 <solotinted@yahoo.com>
2020-06-04 22:00:40 +0100

    my hng7i task one first commit

---
+++ scripts/05137-Wizman.py
@@ -0,0 +1,6 @@
+name = 'Solomon Nuhu Abe'
+id = 'HNG-05137'
+language = 'python'
+email = 'solotinted@yahoo.com'
+output = f'Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task {email}'
+print(output)
