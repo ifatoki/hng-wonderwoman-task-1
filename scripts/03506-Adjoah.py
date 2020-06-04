4fd490329e950aee97a6efea1461426e9e9126d9
Adjoah <adwoaserwaaboafo@gmail.com>
2020-06-04 11:36:36 +0000

    updated output for the task

--- scripts/03506-Adjoah.py
+++ scripts/03506-Adjoah.py
@@ -5,4 +5,4 @@ my_id='03506'
 email_add='adwoaserwaaboafo@gmail.com'
 
 #print statement
-print("Hello World, this is ",name + " with "+ my_id +" using Python for stage 2 task "+ email_add)
+print("Hello World, this is "+name + " with HNGi7 ID "+ my_id +" using Python for stage 2 task "+ email_add)
