1700e2184ce4d8a8f1668c509f6dcaa0b1241381
Victor Samuel <54833975+sholance@users.noreply.github.com>
2020-06-04 13:37:13 +0100

    Word "email" removed

--- scripts/05530-shola.js
+++ scripts/05530-shola.js
@@ -4,4 +4,5 @@ let id ="HNG-05530";
 let name ="Kolade Victor";
 let output =`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`;
 
+
 console.log(output);
