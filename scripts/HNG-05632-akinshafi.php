1c6d11509ebab6cee1ecbd8bc5870d4d62e2da4b
Shafi Akinropo <sakinropo@gmail.com>
2020-06-04 15:36:26 +0100

    Update HNG-05632-akinshafi.php

--- scripts/HNG-05632-akinshafi.php
+++ scripts/HNG-05632-akinshafi.php
@@ -2,9 +2,10 @@
 	$fullname = "Shafi Akinropo";
 	$id = "HNG-05632";
 	$language = "PHP";
+	$email = "sakinropo@gmail.com";
 
 
-echo ("Hello World, this is " . $fullname. " with HNGi7 ID " .$id. " using " .$language . " for stage 2 task");
+echo ("Hello World, this is " . $fullname. " with HNGi7 ID " .$id. " using " .$language . " for stage 2 task ".$email);
 
 ?>
 
