5a2f6ccb2208d56ee1e50b76353946fa01545aec
Oluwaseun Oyebade <motdde@users.noreply.github.com>
2020-06-04 09:51:58 +0100

    02442-motdde.js for Oluwaseun Oyebade

---
+++ scripts/02442-motdde.js
@@ -0,0 +1,8 @@
+let name = "Oluwaseun Oyebade";
+let HNGID = "HNG-02442";
+let email = "telloluwaseunnow@yahoo.com";
+let languege = "JavaScript";
+
+console.log(
+  `Hello World, this is ${name} with HNGi7 ID ${HNGID} using ${languege} for stage 2 task ${email} `
+);
