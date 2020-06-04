5ef5bb91ef4b43141731f40fc962f59623aca3a2
Vikas Rathore <55776424+VikasRathore162@users.noreply.github.com>
2020-06-04 13:46:05 +0530

    Delete Vikas.php

--- scripts/Vikas.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $name = "Vikas Rathore";
-    $id = "HNG-00296";
-    $language = "PHP";
-
-
-echo("Hello World, this is " . $name. " with HNGi7 ID " .$id. " using " .$language . " for stage 2 task");
-
-?>
