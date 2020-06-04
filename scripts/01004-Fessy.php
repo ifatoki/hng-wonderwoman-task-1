300ffb9bbaad7a103a4b1dbb886ce8597d537290
unknown <you@example.com>
2020-06-04 12:27:09 -0700

    second task mi

---
+++ scripts/01004-Fessy.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = 'Festus Oluwaseyi Ogundele';
+$id_no= 'HNG-01004';
+$email = 'festusogundele9@gmail.com';
+$language = 'PHP';
+
+
+
+    echo 'Hello World, this is '.$name.' with HNGi7 ID '.$id_no.' using '.$language.' for stage 2 task '.$email.'';
+
+
+?>
