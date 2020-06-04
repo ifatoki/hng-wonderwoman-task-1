b47abe50c78da7812bd959dda177f3b8de816f5f
mabbrembah <35960444+mabbrembah@users.noreply.github.com>
2020-06-04 11:24:34 +0000

    updated Hello world message

--- scripts/03936-Brembie.py
+++ scripts/03936-Brembie.py
@@ -3,5 +3,4 @@ hngid='03936'
 language="Python"
 mymail="adwoabrembah@gmail.com"
 
-print("Hello World! This is ")
-print(fullname ,"with HNGi7 ID ",hngid,"using",language,"for stage 2 task",mymail)
+print("Hello World, This is ",fullname ,"with HNGi7 ID ",hngid,"using",language,"for stage 2 task",mymail)
