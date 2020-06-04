969639b10c541ccddfac6e189fa8ced7a061ce30
Nyakinyua <55144168+Nyakinyua@users.noreply.github.com>
2020-06-04 18:51:47 +0300

    add email to the output

--- scripts/04393-nyakinyua.py
+++ scripts/04393-nyakinyua.py
@@ -1,4 +1,5 @@
 name = "Joyce Nyakinyua"
 my_id = "HNG-04393"
 language = "Python"
-print(f"Hello World, this is {name} with HNGi7 ID {my_id} using {language} for stage 2 task")
+email = "wanyakinyua968@gmail.com"
+print(f"Hello World, this is {name} with HNGi7 ID {my_id} using {language} for stage 2 task {email}")
