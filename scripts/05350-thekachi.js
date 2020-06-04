9987867f2e31b1ab0d9887f906100fa4847cc55d
Kachi <nzebenflorence@gmail.com>
2020-06-04 23:15:03 +0100

    feature: added script

---
+++ scripts/05350-thekachi.js
@@ -0,0 +1,8 @@
+const myFullName = "Onyedikachi Nze-Ben";
+const myId = "HNG-005350";
+const myLanguage = "Javascript";
+const myEmail = "nzebenflorence@gmail.com";
+
+console.log(
+  `Hello World, this is ${myFullName} with HNGi7 ID ${myId} using ${myLanguage} for stage 2 task.`
+);
