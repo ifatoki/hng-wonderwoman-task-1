e24cf115cc2836340b465e47fa0c4d8c963dfa81
Shafi Akinropo <sakinropo@gmail.com>
2020-06-04 18:40:43 +0100

    update

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $outputJSON = [];
 $data = [];
 $success = 0;
 $failure = 0;
+$pattern = '/^Hello World, this is (.{1,}\s)+with HNGi7 ID HNG-\d{5} using (.*) for stage 2 task (?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/i';
 
 foreach ($files as $file) {
   $filename = $file;
@@ -35,8 +36,18 @@ foreach ($files as $file) {
     $data[$extension[0]]->name = $extension[0];
 
     // Get email
-    $array_of_sentence = explode(" ", $f);
-    $email = trim(end($array_of_sentence));
+    $pass = preg_match($pattern, $f);
+    if ($pass) {
+        $status = "pass";
+        $array_of_sentence = explode(" ", $f);
+        $email = trim(end($array_of_sentence));
+    } else {
+        $status = "fail";
+        $email = 'nil';
+    }
+
+    // $array_of_sentence = explode(" ", $f);
+    // $email = trim(end($array_of_sentence));
 
     // Get name
     $name_search = preg_match('/\bis (.{1,}\s)+with/', $f, $name_array);
