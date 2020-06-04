fb3083b018553c6e1e82b4c3915325821724909a
Ritika-Agrawal811 <65513097+Ritika-Agrawal811@users.noreply.github.com>
2020-06-04 17:39:48 +0530

    Update 00436-Ritika.php

--- scripts/00436-Ritika.php
+++ scripts/00436-Ritika.php
@@ -4,7 +4,7 @@
     
    $id = "HNG-00436";
     
-    echo "Hello World, this is Ritika Agrawal with HNGi7 ID $id using php for stage 2 task ritikaagrawal339@gmail.com"."<br>";
+    echo "Hello World, this is Ritika Agrawal with HNGi7 ID $id using php for stage 2 task ritikaagrawal339@gmail.com";
     
    ?>
    
