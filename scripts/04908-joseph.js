c3a9c38339b709c22e82aa94baf2aa3406134a78
TittyLover <66315523+TittyLover@users.noreply.github.com>
2020-06-04 12:16:28 +0100

    Delete 04908-joseph.js

--- scripts/04908-joseph.js
+++
@@ -1,7 +0,0 @@
-const fullName = "Afolabi Joseph Ajani";
-const Hngi7_Id = "HNG-04908";
-const email = "ask4joseph";
-let language = "JavaScript";
-
-message = "Hello World, this is ${fullName} with HNGI7 ID ${Hngi7_Id} using ${language} for stage 2 task ${email}";
-Console.log(message);
