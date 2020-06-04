fe26dec7c3434714545e4ec22aa90af30ec7fe74
wonderwoman23 <60629862+wonderwoman23@users.noreply.github.com>
2020-06-04 16:36:13 -0700

    Create 02227-KimayaUrdhwareshe.py

---
+++ scripts/02227-KimayaUrdhwareshe.py
@@ -0,0 +1,8 @@
+Name = "Kimaya Urdhwareshe"
+ID = "HNG-02227"
+Language = "Python"
+Email = "kimaya23@gmail.com"
+
+
+print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task {3}".format(Name, ID, Language, Email))
+
