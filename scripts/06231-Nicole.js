488cef491fd57b1d7edfa33a232bb35979f4dba3
Olaniyi Oshunbote <olaniyi.oshunbote@gloopro.com>
2020-06-04 22:34:42 +0100

    hng id corrected

--- scripts/06231-Nicole.js
+++ scripts/06231-Nicole.js
@@ -3,6 +3,6 @@ let fullName = "Adedoyin Adeyemi"
 let HNG_ID = "HNG-00004"
 let language = "Javascript"
 let email = "dedoyinnicole@gmail.com"
-console.log("Hello World, this is " + fullName + " with HNGi7 " + HNG_ID + " using " + language + " for stage 2 task " + email)
+console.log("Hello World, this is " + fullName + " with HNGi7 ID " + HNG_ID + " using " + language + " for stage 2 task " + email)
 };
 stage();
