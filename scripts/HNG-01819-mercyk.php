0432db3c422289da0ffff7005573bbf4b3209824
unknown <mercyjemosop@gmail.com>
2020-06-05 01:07:56 +0300

    Task 1 team wonderwoman

--- scripts/HNG-01819-mercyk.php
+++ scripts/HNG-01819-mercyk.php
@@ -5,8 +5,8 @@ $hngid='HNG-01819';
 $name='Mercy Kipyegon';
 $email="mercyjemosop@gmail.com";
 $language="PHP";
-$hello= "Hello world, this is ".$name.", Email ".$email." with HNGi7 ID ".$hngid. " using ".$language. " for stage 2 task, ";
-$str=array($hello, "name"=>$name, "id"=>$hngid,  "email"=>$email, "language"=>$language);
-print json_encode($str);
+$hello= "Hello world, this is ".$name." with HNGi7 ID ".$hngid. " using ".$language. " for stage 2 task ".$email;
+
+print json_encode($hello);
 
 ?>
