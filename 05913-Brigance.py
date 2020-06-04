1ebbc20f44dd5c9dc1765e75c7413bff846e1eb0
SamuelBriggs <64214300+SamuelBriggs@users.noreply.github.com>
2020-06-04 21:34:09 +0100

    Create 05913-Brigance.py

---
+++ 05913-Brigance.py
@@ -0,0 +1,5 @@
+name = "Alawode Tolulope Samuel"
+hng_id = "HNG-05913"
+email = "tolulope.alawode@gmail.com"
+lang = "Python"
+print('Hello World, this is {} with HNG i7 ID {} using {} for stage 2 task {}'. format(name, hng_id, lang, email))
