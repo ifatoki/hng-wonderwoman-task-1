f3458d2638125fa8f536a1f4854b9fc21284e784
adenicole <66355939+adenicole@users.noreply.github.com>
2020-06-04 19:10:05 +0100

    Update 00004-Nicole.js

--- 00004-Nicole.js
+++ 00004-Nicole.js
@@ -3,6 +3,6 @@ let fullName = "Adedoyin Adeyemi"
 let HNG_ID = "HNG-00004"
 let language = "Javascript"
 let email = "dedoyinnicole@gmail.com"
-return "Hello World, this is " + fullName + " with HNGi7 " + HNG_ID + " using " + language + " for stage 2 task " + email
+console.log("Hello World, this is " + fullName + " with HNGi7 " + HNG_ID + " using " + language + " for stage 2 task " + email)
 };
-y = stage();
+stage();
