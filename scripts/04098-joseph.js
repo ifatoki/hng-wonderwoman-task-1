7b653d819fe4928d800bbb2c1999cce92f150381
TittyLover <66315523+TittyLover@users.noreply.github.com>
2020-06-04 14:24:56 +0100

    Create 04098-joseph.js
    HNG stage  1 task

---
+++ scripts/04098-joseph.js
@@ -0,0 +1,8 @@
+const fullName = "Afolabi Joseph Ajani";
+const Hngi7_Id = "HNG-04908";
+const email = "ask4josef@gmail.com";
+let language = "JavaScript";
+
+
+information = `Hello World, this is ${fullName} with HNGI7 ID ${Hngi7_Id} using ${language} for stage 2 task ${email}`;
+Console.log(information);
