8eebcb18f2021fd3e72f2db66f734a40cb1031c9
Umohesther10 <59563888+Umohesther10@users.noreply.github.com>
2020-06-04 05:23:44 +0100

    first commit

---
+++ scripts/03407-Esther.Umoh.js
@@ -0,0 +1,16 @@
+let name = "Umoh Esther";
+let hng_id = "HNG-03407";
+let lang = "JavaScript";
+let email = "umohesther08@gmail.com";
+
+console.log(
+  "Hello World, this is " +
+    name +
+    " with HNGi7 ID " +
+    hng_id +
+    " and email " +
+    email +
+    " using " +
+    lang +
+    " for stage 2 task "
+);
