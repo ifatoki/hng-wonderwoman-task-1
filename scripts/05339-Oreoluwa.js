1d981c8b062ddd048f4c842452b77116fe6ab27a
ore-cathy <ogunlajaoreoluwa@gmail.com>
2020-06-04 14:33:51 +0100

    corrected a typo

--- scripts/05339-Oreoluwa.js
+++ scripts/05339-Oreoluwa.js
@@ -1,8 +1,7 @@
-let myFullName = "Oreoluwa Catherine Ogunlaja";
-let myHngi7_id = "HNG-05339";
-let programmingLanguage = "Javascript";
-let myEmail = "Ogunlajaoreoluwa@gmail.com";
+let name = "Oreoluwa Catherine Ogunlaja";
+let ID = "HNG-05339";
+let language = "Javascript";
+let email = "Ogunlajaoreoluwa@gmail.com";
+let output = `Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task ${email}`;
 
-console.log(
-  `Hello World, this is ${myFullName} with HNGi7 ID ${myHngi7_id} using ${programmingLanguage} for stage 2 task ${myEmail}`
-);
+console.log(output);
