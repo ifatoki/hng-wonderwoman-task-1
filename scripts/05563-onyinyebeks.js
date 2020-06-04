b21987c125243beca37641542de203dd30f98d75
djfemz <oladejifemi00@gmail.com>
2020-06-04 18:55:45 +0100

    Delete 05563-onyinyebeks.js

--- scripts/05563-onyinyebeks.js
+++
@@ -1,5 +0,0 @@
-let name = "Goodness Chris-Ugari";
-let id = "HNG-05563";
-let email = "goodnesschrisugari@yahoo.com";
-
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using JavaScript for stage 2 task`);
