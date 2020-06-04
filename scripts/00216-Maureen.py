b69b2324caaadaa079275248f59a510417aa31cb
Thamar <maureenthamar@gmail.com>
2020-06-04 03:53:39 +0300

    my complete task

---
+++ scripts/00216-Maureen.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+
+
+
+=======
+>>>>>>> 67c8ac6afe993c7ac05017d94b9aa701d562fe3d
+name = "Maureen Thama"
+ID = "HNG-00216"
+email = "maureenthamar@gmail.com"
+
+
+<<<<<<< HEAD
+print("Hello World, this is {0} with HNGi7 ID {1} using Python for stage 2 task {2}".format(name, ID, email))
+=======
+print("Hello World, this is {0} with HNGi7 ID {1} using Python for stage 2 task {2}".format(name, ID, email))
+>>>>>>> 67c8ac6afe993c7ac05017d94b9aa701d562fe3d
