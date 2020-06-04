cb1da08cf8c770bbf004532df1bcb8ef37f6e540
Nusaybah <63037712+Nusaybah516@users.noreply.github.com>
2020-06-04 22:22:06 +0100

    hng task one

---
+++ scripts/00031-Nusaybah.js
@@ -0,0 +1,10 @@
+function details() {
+    let me={
+        FullName: "Nusaybah Fuad-Gafar",
+        hngId:"HNG-00031",
+        Language:"Javascript",
+        EmailAddress:"fuadgafarnusaybah@gmail.com"
+    }
+    return 'Hello World,this is ${me.FullName} with HNGi7 ID ${me.hngId} using ${me.Language} for stage 2 task ${me.EmailAddress}'
+}
+console.log(details());
