ac862d65105f8003c71158982dfe70932648912c
Bilewu Mary <bilewumary@gmail.com>
2020-06-04 16:46:31 +0100

    my script

--- scripts/04376-Mary.py
+++ scripts/04376-Mary.py
@@ -3,4 +3,3 @@
 >>> HNGi7_ID = "HNG-04376"
 >>> 
 >>> print(f"Hello World, this is {My_name} with HNGi7 ID {HNGi7_ID} using {Lan_used} for stage 2 task", flush=True)
-Hello World, this is Mary OgoOluwa Bilewu with HNGi7 ID HNG-04376 using Python for stage 2 task
