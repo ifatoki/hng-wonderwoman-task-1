162d77286d15daec7e376541382df75d743c2223
Umohesther10 <59563888+Umohesther10@users.noreply.github.com>
2020-06-04 12:41:58 +0100

    requested changes effected

--- scripts/03407-Esther.js
+++ scripts/03407-Esther.js
@@ -8,8 +8,9 @@ console.log(
     name +
     " with HNGi7 ID " +
     hng_id +
+    " and email " +
+    email +
     " using " +
-    language +
-    " for stage 2 task " +
-    email
+    lang +
+    " for stage 2 task "
 );
