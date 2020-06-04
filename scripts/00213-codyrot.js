b1915d3e3cec214be8e8bbeda034f5e47dafe969
John Rotgak <info2rotgak@gmail.com>
2020-06-05 00:01:33 +0100

    my hng7i task one for team wonderwoman

--- scripts/00213-codyrot.js
+++ scripts/00213-codyrot.js
@@ -1,3 +1,11 @@
-var message = "Hello world, this is John Rotgak with HNGi7 ID HNG-00213 and email info2rotgak@gmail.com, using Javascript for stage 2";
+var fname = "John";
+var lname = "Rotgak";
+var ID = "HNG-00213"
+var language = "javascript";
+var email = "info2rotgak@gmail.com";
 
-console.log(message);
+
+console.log("Hello World, this is " + fullname + " " + " with HNGi7 ID " +  ID + " using " + language + " for stage 2 task " + " " + email);
+
+
+
