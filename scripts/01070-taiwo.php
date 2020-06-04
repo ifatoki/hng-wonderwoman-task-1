5d0bc63e2a2516730292ad04cca7cfad8892c206
Taiwo <akinbile6@gmail.com>
2020-06-04 23:32:05 +0300

    Email Update

--- scripts/01070-taiwo.php
+++ scripts/01070-taiwo.php
@@ -2,4 +2,5 @@
 $fullname = 'Taiwo Akinbile';
 $ID = 'HNG01070';
 $language = 'PHP';
+$email = 'akinbile6@gmail.com';
 echo("Hello World, this is " . $fullname. " with HNGI ID " . $ID . " using " . $language . " for the stage 2 task " . $email);
