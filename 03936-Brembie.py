4b11b878eb976b2c3f15c1287626ca1477f8591b
mabbrembah <35960444+mabbrembah@users.noreply.github.com>
2020-06-04 11:07:53 +0000

    created first script

--- 03936-Brembie.py
+++
@@ -1,7 +0,0 @@
-fullname="Maame Adwoa Brembah"
-hngid='03936'
-language="Python"
-mymail="adwoabrembah@gmail.com"
-
-print("Hello World! This is ")
-print(fullname ,"with HNGi7 ID ",hngid,"using",language,"for stage 2 task",mymail)
