f72ecd3936ff186ad13575d2881ebc410ecf66f4
Eirene Oyakhilome <33825632+Peace99@users.noreply.github.com>
2020-06-04 20:03:01 +0100

    Add files via upload
    new details uploades

---
+++ 03068-eirene.py
@@ -0,0 +1,9 @@
+#using python 3.7
+
+my_id = "HNG-03068"
+full_name = "EIRENE OYAKHILOME OISE"
+language = "python"
+email = "mhizeirene@gmail.com"
+details = f'Hello World, this is {full_name} with HNGi7 id {my_id} using language {language} for stage 2 task.{email}
+print(details)
+
