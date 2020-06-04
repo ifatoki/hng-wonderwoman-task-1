8bb1f306c1e1df782950a8e84015fb2d455d5523
JhyboMob <jibo.areo@gmail.com>
2020-06-04 19:01:45 +0100

    add jhybomob script to scripts

---
+++ scripts/04368-jhybomob.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 18:03:39 2020
+
+@author: user
+"""
+
+print("Hello World, this is Ajibola Areo with HNGi7 ID HNG-04368 using python for stage 2 task jibo.areo@gmail.com")
