8ccbebc5206886aff862c51bf40da402ca01e8fa
akpe2003 <aes_akpe@yahoo.ca>
2020-06-04 15:44:26 +0100

    Update 00918-AISOSA.php

--- scripts/00918-AISOSA.php
+++ scripts/00918-AISOSA.php
@@ -1,12 +1,8 @@
-<!Doctype html>
-<html lang="en">
-<head>
-<title>HNGi7 Intern</title>
-<meta charset="utf-8" />
-</head>
-<body>
 <?php
-echo "Hello World!, this is Aisosa Akpevweoghene Ogheneowho Ugono with HNGi7 ID HNG7-00918 using both HTML and PHP for stage 2 task";
+$full_name="Aisosa Ugono";
+$id="HNG-00918";
+$language="PHP";
+$email="akpeugono@gmail.com";
+echo "Hello World, this is $full_name with HNGi7 ID $id using $language for stage 2 task  $email";
 ?>
-</body>
-</html>
+
