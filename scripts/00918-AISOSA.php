047f75e3127b232c5c81f10d7f0e6bbbc21e4723
Shafi Akinropo <sakinropo@gmail.com>
2020-06-04 10:49:07 +0100

    Update 00918-AISOSA.php

--- scripts/00918-AISOSA.php
+++ scripts/00918-AISOSA.php
@@ -1,12 +1,5 @@
-<!Doctype html>
-<html lang="en">
-<head>
-<title>HNGi7 Intern</title>
-<meta charset="utf-8" />
-</head>
-<body>
+
 <?php
-echo "Hello World!, this is Aisosa Akpevweoghene Ogheneowho Ugono with HNGi7 ID HNG7-00918 using both HTML and PHP for stage 2 task";
+echo "Hello World!, this is Aisosa Akpevweoghene Ogheneowho Ugono with HNGi7 ID HNG7-00918 using both HTML and PHP for stage 2 task aisosa@gmail.com";
 ?>
-</body>
-</html>
+
