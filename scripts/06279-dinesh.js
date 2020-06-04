69aae435622c31dccb587620e2044a4946da35a3
dinesh99639 <dinesh99639@github.com>
2020-06-04 16:17:59 +0530

    Task 1 completed

---
+++ scripts/06279-dinesh.js
@@ -0,0 +1,7 @@
+var name = "Dinesh";
+var surname = "Somaraju";
+var id = "HNG-06279"
+var lang = "javascript";
+
+var message = "Hello World, this is "+name+" "+surname+" with HNGi7 ID "+id+" using "+lang+" for stage 2 task";
+console.log(message);
