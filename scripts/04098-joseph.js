19f45e2e169e776d5083fed3b90a189f7c3d68b9
TittyLover <66315523+TittyLover@users.noreply.github.com>
2020-06-04 13:34:26 +0100

    Update 04098-joseph.js
    email updated

--- scripts/04098-joseph.js
+++ scripts/04098-joseph.js
@@ -1,8 +1,8 @@
 const fullName = "Afolabi Joseph Ajani";
 const Hngi7_Id = "HNG-04908";
-const email = "ask4joseph";
+const email = "ask4josef@gmail.com";
 let language = "JavaScript";
 
 
-message = "Hello World, this is ${fullName} with HNGI7 ID ${Hngi7_Id} using ${language} for stage 2 task ${email}";
-Console.log(message);
+information = "Hello World, this is ${fullName} with HNGI7 ID ${Hngi7_Id} using ${language} for stage 2 task ${email}";
+Console.log(information);
