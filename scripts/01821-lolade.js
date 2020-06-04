35576fece878c0f668eae6fced29e5898cd36535
Ololade <49575651+lolyparty@users.noreply.github.com>
2020-06-04 02:38:55 +0100

    updated 01821-lolade.js

--- scripts/01821-lolade.js
+++ scripts/01821-lolade.js
@@ -1,12 +1,13 @@
 //HNG task
 function hngTask(){
-    var hngId, fullName, language
+    var hngId, fullName, language, email
 
     hngId = "HNG-01821";
     fullName = "OLayiwola Ololade";
     language = "javascript";
+    email = "ololadeolayiwola01@gmail.com";
 
-    console.log("Hello World, this is " + fullName + " with HNGi7 ID " + hngId + " using " + language + " for stage 2 task ololadeolayiwola01@gmail.com") 
+    console.log("Hello World, this is " + fullName + " with HNGi7 ID " + hngId + " using " + language + " for stage 2 task " + email) 
 }
 
 hngTask();
