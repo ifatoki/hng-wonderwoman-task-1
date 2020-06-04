b6359444927a2537f556809fad4a0cfa06730e4f
Daniel Tobi Onipe <dexterousguru49@yahoo.com>
2020-06-04 19:20:02 +0100

    task output changed to lowercase

--- scripts/04548-Dexter.py
+++ scripts/04548-Dexter.py
@@ -2,8 +2,8 @@ internName = "Daniel Tobi Onipe"
 
 internID = "HNG-04548"
 
-languageUsed = "Python"
+languageUsed = "python"
 
 internEmail = "dexterousguru49@gmail.com"
 
-print(("Hello World, this is " + internName +" with HNGi7 ID " + internID +" using " + languageUsed + " for Stage 2 Task " + internEmail), flush=True)
+print(("Hello World, this is " + internName +" with HNGi7 ID " + internID +" using " + languageUsed + " for stage 2 task " + internEmail), flush=True)
