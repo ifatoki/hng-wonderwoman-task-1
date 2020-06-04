61c2930444d9fcc8317e0728d08e02a81fc74a00
Amadikwa Joy N <treasurechristain@gmail.com>
2020-06-04 10:25:47 +0100

    Update HNG-03437-sweetheart.php
    Merge my request, Updated HNG-03437-sweetheart.php

--- scripts/HNG-03437-sweetheart.php
+++ scripts/HNG-03437-sweetheart.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h3>
-        <?php  echo("Hello World, This is Amadikwa Joy N with HNGi7 ID: HNG-03437 using PHP for stage2 task")
+        <?php  echo("Hello World, This is Amadikwa Joy N with HNGi7 ID HNG-03437 using PHP for stage2 task")
  ?>
     </h3>
   
