7147619672023fcd2d3c14e7070a1af70dbded2f
nwanyibihe <47614251+nwanyibihe@users.noreply.github.com>
2020-06-04 20:58:26 +0100

    add script file

---
+++ scripts/03230-Nwanyibihe.js
@@ -0,0 +1,5 @@
+const name = "Nwanyibihe Uhegbu";
+const HNGID = "HNG-03230";
+const email = "nwanyibihe@gmail.com"
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${HNGID} using javascript for stage 2 task ${email}`);
