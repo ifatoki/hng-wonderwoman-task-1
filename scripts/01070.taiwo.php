c7cd5006def8eccc24c7016822384d28c3bdc7e5
Taiwo <akinbile6@gmail.com>
2020-06-04 15:14:29 +0300

    Delete 01070.taiwo.php

--- scripts/01070.taiwo.php
+++
@@ -1,7 +0,0 @@
-<?php
-$fullname = 'Taiwo Akinbile';
-$ID = 'HNG01070';
-$language = 'PHP';
-    echo("Hello World, this is " .$fullname
-    ." with HNGI ID" .$ID." using ".$language." for the stage 2 task ");
-?>
