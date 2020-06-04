360c48b548a198a1e4e48400fd76656877863455
angelnagaba <restyangel143@gmail.com>
2020-06-04 15:49:11 +0300

    Angel Nagaba editted script

--- scripts/03520-AngelNagaba.js
+++ scripts/03520-AngelNagaba.js
@@ -2,5 +2,4 @@ var Name = "AngelNagaba";
 var Hngi7_ID = "HNG-03520";
 var Lang = "Javascript";
 
-console.log("Hello World,this is "+Name+" "+"with HNGi7 ID "+Hngi7_ID+" "+"using "+Lang+" "+"for stage 2 task")
-console.log("angelnagaba99@gmail.com");
+console.log("Hello World,this is "+Name+" "+"with HNGi7 ID "+Hngi7_ID+" "+"using "+Lang+" "+"for stage 2 task angelnagaba99@gmail.com")
