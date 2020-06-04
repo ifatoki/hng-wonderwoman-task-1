dc7495a43a27a7f1c83ce92e348ba6c9282a95b6
khal33d-hub <oyelekekhaleed@gmail.com>
2020-06-04 20:02:35 +0100

    Update 02893-KHALEED.py

--- scripts/02893-KHALEED.py
+++ scripts/02893-KHALEED.py
@@ -1,6 +1,6 @@
 def introduction():
 	Name="khaleed Oyeleke"
 	ID = "HNG-02893"
-	Email="Oyelekekhaleed@gmail.com"
-	print("Hello World, this is {a} with HNG ID {b} using python for stage 2 task {c}".format(a=Name, b=ID, c=Email))
+	email="oyelekekhaleed@gmail.com"
+	print("Hello World, this is {a} with HNGi7 ID {b} using python for stage 2 task {c}".format(a=Name, b=ID, c=email))
 introduction()
