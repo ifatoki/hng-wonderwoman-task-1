c2f1ecf9fb43905fa4e99625d94695b88279b6d9
Daniel Tobi Onipe <dexterousguru49@yahoo.com>
2020-06-04 18:37:09 +0100

    Dexter HNG-04548

---
+++ scripts/04548-Dexter.py
@@ -0,0 +1,9 @@
+internName = "Daniel Tobi Onipe"
+
+internID = "HNG-04548"
+
+languageUsed = "Python"
+
+internEmail = "dexterousguru49@gmail.com"
+
+print(("Hello World, this is " + internName +" with HNGi7 ID " + internID +" using " + languageUsed + " for Stage 2 Task " + internEmail), flush=True)
