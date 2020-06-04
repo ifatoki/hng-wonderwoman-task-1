b4284d45e0285e5414a7e77513ff173466e8025b
Timothy Adegbasa <timothyadegbasa@gmail.com>
2020-06-04 21:49:11 +0100

    Add files via upload

---
+++ scripts/02162-TimothyAdegbasa.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello world, this is Timothy Adegbasa with HNGi7 ID HNG-02162 using python for stage 2 task timothyadegbasa@gmail.com\n"
+     ]
+    }
+   ],
+   "source": [
+    "name= 'Timothy Adegbasa'\n",
+    "id= 'HNG-02162'\n",
+    "language= 'python'\n",
+    "email= 'timothyadegbasa@gmail.com'\n",
+    "print('Hello world, this is {} with HNGi7 ID {} using {} for stage 2 task {}'.format(name,id,language,email))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
