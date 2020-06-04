50d44d2d38a00e4d29c48200f58c05884c160117
JoyKui33 <45483359+JoyKui33@users.noreply.github.com>
2020-06-04 20:09:03 +0300

    New changes

--- scripts/02274-joywan.js
+++ scripts/02274-joywan.js
@@ -3,9 +3,6 @@ let id = "HNG-02274";
 let language ="Javascript";
 let email = "feliwangui@gmail.com";
 
-
-function showMessage(){
     console.log('Hello World, this is '+name+' with HNGi7 ID '+id+' using '+language+' for stage 2 task '+email);
-}
-showMessage();
+
 
