536bf0b62e0f90224208a44f1e44d1ccac242c39
Muhammed Saifudeen Salaudeen <mr.salaudeen.official@gmail.com>
2020-06-05 06:19:52 +0100

    added my email

--- scripts/01999-muhammed.php
+++ scripts/01999-muhammed.php
@@ -1,3 +1,3 @@
 <?php
-    echo "Hello World, this is Muhammed Saifudeen Salaudeen with HNGi7 ID HNG-01999 using PHP for stage 2 task";
+    echo "Hello World, this is Muhammed Saifudeen Salaudeen with HNGi7 ID HNG-01999 using PHP for stage 2 task mr.salaudeen.official@gmail.com";
 ?>
