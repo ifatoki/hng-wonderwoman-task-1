37f06d5de726fa22ba3138ada289fb10e900e1dc
Oloye001 <50100483+Oloye001@users.noreply.github.com>
2020-06-05 19:51:10 +0100

    Create 04192-oloye.py
    process or stage 2

---
+++ scripts/04192-oloye.py
@@ -0,0 +1,7 @@
+full_name = "Mercy Oyeniran"
+internship_id = "HNG-04192"
+language = "python"
+email = "oyenimiercy@gmail.com"
+print(
+   f"Hello World, this is {full_name} with HNGi7 ID {internship_id} using {language} for task 2 {email}."
+)
