9f36c74dd4c7d4fceb1c64e1561e17e04f64df13
Michelle Kyalo <kyalomichelle06@gmail.com>
2020-06-05 11:30:22 +0300

    michelle-HNG-05467 script

--- scripts/05467-michelle.py
+++ scripts/05467-michelle.py
@@ -1,6 +1,6 @@
 full_name = "Michelle Kyalo"
-hngid = "HNG-05467"
+ID = "HNG-05467"
 language = "Python"
-email = "kyalomichelle06@gmail.com"
-y = "Hello World, this is {full_name} with HNGi7 ID{hngid} using {language} for stage 2 task {email}"
+email_address = "kyalomichelle06@gmail.com"
+y = f"Hello World, this is {full_name} with HNGi7 ID {ID} using {language} for stage 2 task {email_address}"
 print(y)
