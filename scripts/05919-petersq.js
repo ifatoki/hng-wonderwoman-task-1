964d01611b8215d5d85a50d8b7b7fbeb064156b0
awe peter <awe.petersq@gmail.com>
2020-06-05 01:38:57 +0100

    first task

--- scripts/05919-petersq.js
+++ scripts/05919-petersq.js
@@ -3,5 +3,4 @@ const My_id = "HNG-05919";
 const Email = "Awe.petersq@gmail.com";
 const Language  = "Javascript";
 
-
 console.log(`Hello World, this is ${Name} with HNGi7 ID ${My_id} using ${Language} for stage 2 task ${Email}`);
