3c989b55650c9feac1988930985f9b10436171f5
unknown <emmanueldmlr@gmail.com>
2020-06-05 04:45:20 +0100

    Solved HNG task 2 question

---
+++ scripts/00177-Drebakare.py
@@ -0,0 +1,9 @@
+def printHngDetails():
+    username = "Bakare Damilare"
+    internship_id = "HNG-00177"
+    language = "python"
+    email = "emmanueldmlr@gmail.com"
+    return "Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task %s"%(username,internship_id,language,email)
+
+
+print(printHngDetails())
