65a10ac2e79ec2379c71241eeca0e7faad62514a
Giye <onengiyeamadi@gmail.com>
2020-06-05 02:45:11 +0100

    Giye script for Stage 1 task

---
+++ scripts/03279-Giye.js
@@ -0,0 +1,50 @@
+let user = {
+    name: "Amadi Onengiye Williams",
+    id: "HNG-03279",
+    language: "JavaScript",
+    email: "onengiyeamadi@gmail.com"
+
+}
+console.log( `Hello World, this is ${user.name} with HNGi7 ID with ${user.id} using ${user.language} for stage 2 task ${user.email}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
