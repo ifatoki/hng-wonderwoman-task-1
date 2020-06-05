25c98ac915f72f1ba469f2563608e02cade962f5
Amadikwa Joy N <treasurechristain@gmail.com>
2020-06-05 15:47:42 +0100

    HNG-03437-sweetheart.php again
    removed braces

--- scripts/HNG-03437-sweetheart.php
+++ scripts/HNG-03437-sweetheart.php
@@ -4,6 +4,6 @@ $language = "PHP";
 $email = "amadikwajoyn@gmail.com"
             
                 
-echo("Hello World, this is ".$fullname." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email);
+echo "Hello World, this is ".$fullname." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email;
            
 
