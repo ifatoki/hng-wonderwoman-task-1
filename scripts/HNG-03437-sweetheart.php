e84b5cacef7c1e401733fbd0cb1b90a0954ec8c2
Amadikwa Joy N <treasurechristain@gmail.com>
2020-06-05 15:16:37 +0100

    Removed the webpage HNG-03437-sweetheart.php
    I removed the entire code from the webpage to a script

--- scripts/HNG-03437-sweetheart.php
+++ scripts/HNG-03437-sweetheart.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Amadikwa Joy N</title>
-</head>
-<body>
-    <h3>
+
 <?php
             $fullname = "Amadikwa Joy";
             $id = "HNG-03437";
@@ -17,7 +9,4 @@
             echo("Hello World, this is ".$fullname." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email);
            ?>
            
-    </h3>
-  
-</body>
-</html>
+
