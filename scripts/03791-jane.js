e8c181c5f9babd7dfbec1f77d3af50779200a947
Chimdi Jane Samuel <61986766+JaneSamuel@users.noreply.github.com>
2020-06-05 12:36:27 +0100

    Create 03791-jane.js

---
+++ scripts/03791-jane.js
@@ -0,0 +1,5 @@
+const name = 'Chimdi Jane Samuel';
+const id = 'HNG-03791';
+const email = 'janesamuel308@gmail.com';
+const language = 'Javascript';
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task " + email);
