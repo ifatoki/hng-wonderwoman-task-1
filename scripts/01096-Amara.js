026e24f1660c1d61e5b86a42f1a734b159ce4480
Iheanacho Amarachi Sharon <58210919+Iheanacho-ai@users.noreply.github.com>
2020-06-05 08:02:27 +0100

    Update 01096-Amara.js
    Updated

--- scripts/01096-Amara.js
+++ scripts/01096-Amara.js
@@ -1,10 +1,11 @@
 const fullName = 'Iheanacho Amarachi Sharon';
 const ID = 'HNG-01096';
 const language = 'JavaScript';
+const email = 'amarachi2812@gmail.com'
 
 
 const greet = () =>{
-    return `Hello World, this is ${fullName} with Hngi7 ID ${ID} using ${language} for stage 2 task`;
+    return `Hello World, this is ${fullName} with Hngi7 ID ${ID} using ${language} for stage 2 task ${email}`;
 }
 
 greet();
