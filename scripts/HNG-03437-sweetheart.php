ce36ea0e457231c05833b09951ef8aaaca43b38d
Amadikwa Joy N <treasurechristain@gmail.com>
2020-06-05 16:05:16 +0100

    HNG-03437-sweetheart.php
    added semi colon in my email

--- scripts/HNG-03437-sweetheart.php
+++ scripts/HNG-03437-sweetheart.php
@@ -1,7 +1,7 @@
 $fullname = "Amadikwa Joy";
 $id = "HNG-03437";
 $language = "PHP";
-$email = "amadikwajoyn@gmail.com"
+$email = "amadikwajoyn@gmail.com";
             
                 
 echo "Hello World, this is ".$fullname." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email;
