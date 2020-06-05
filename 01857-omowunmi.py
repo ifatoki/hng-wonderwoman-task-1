4807021cb9c8d095edf493a49e1c66db77ebc833
faozziyyah <65113702+faozziyyah@users.noreply.github.com>
2020-06-05 07:05:00 +0100

    Updated changes

--- 01857-omowunmi.py
+++ 01857-omowunmi.py
@@ -1,1 +1,1 @@
-print('hello world, this is faozziyyah Daud opeyemi with HNGi7 ID HNG-01857 using python language for stage 2 task omowunmidaud1@gmail.com')
+print('hello world, this is faozziyyah Daud opeyemi with HNGi7 ID HNG-01857 using python for stage 2 task omowunmidaud1@gmail.com')
