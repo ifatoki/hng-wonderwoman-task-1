ee7f18a0c2e05ab1f12c66548bebe71cdfecf683
Petersheg <43689575+Petersheg@users.noreply.github.com>
2020-06-05 10:18:24 +0100

    Update 05916-oluwasegun.js

--- scripts/05916-oluwasegun.js
+++ scripts/05916-oluwasegun.js
@@ -1,5 +1,4 @@
 
 const name="Peter Oluwasegun",id="HNG-05916",lang="JavaScript",email="petersheg@gmail.com";
 
-console.log(`
-        Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task ${email}`);
+console.log(`Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task ${email}`);
