429b93e2146f4e9d58cbbf12265c0b2daf4aaf5f
elemson <toluwhani@gmail.com>
2020-06-05 20:44:58 +0300

    added email

--- scripts/00485-Toluwani.js
+++ scripts/00485-Toluwani.js
@@ -5,10 +5,10 @@ const id = "HNG-00485";
 const language = "Javascript";
 const email = "toluwanielemosho@gmail.com";
 
-const firstTask = (name, id, language) => {
+const firstTask = (name, id, language, email) => {
   console.log(
-    `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`
+    `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`
   );
 };
 
-firstTask(fullname, id, language);
+firstTask(fullname, id, language, email);
