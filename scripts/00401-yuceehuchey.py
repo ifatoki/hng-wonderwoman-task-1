438d54dd4f6238391e717a2f8b10e251bf674ef4
yuceehuchey <66208213+yuceehuchey@users.noreply.github.com>
2020-06-05 04:10:53 -0700

    Update 00401-yuceehuchey.py

--- scripts/00401-yuceehuchey.py
+++ scripts/00401-yuceehuchey.py
@@ -1,1 +1,1 @@
-"Hello World, this is Nduanya Uchenna with HNGi7 ID HNG-00401 using python for stage 2 task yuceehuchey@gmail.com"
+print("Hello World, this is Nduanya Uchenna with HNGi7 ID HNG-00401 using python for stage 2 task yuceehuchey@gmail.com")
