c7c8995d62ea5f8117d826aedf0ba8d5cc88d1f7
angelnagaba <restyangel143@gmail.com>
2020-06-05 05:49:56 +0300

    fixed failed script

--- scripts/03520-AngelNagaba.js
+++ scripts/03520-AngelNagaba.js
@@ -1,5 +1,5 @@
-var Name = "AngelNagaba";
+var Name = "Angel Nagaba";
 var Hngi7_ID = "HNG-03520";
 var Lang = "Javascript";
 
-console.log("Hello World,this is "+Name+" "+"with HNGi7 ID "+Hngi7_ID+" "+"using "+Lang+" "+"for stage 2 task angelnagaba99@gmail.com")
+console.log("Hello World, this is "+Name+" "+"with HNGi7 ID "+Hngi7_ID+" "+"using "+Lang+" "+"for stage 2 task angelnagaba99@gmail.com")
