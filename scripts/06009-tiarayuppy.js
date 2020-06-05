cabae5c054e3bfa7de922d8b25ae746a1d618be1
tiarayuppy <38207742+tiarayuppy@users.noreply.github.com>
2020-06-05 19:50:42 +0100

    updated_error_

--- scripts/06009-tiarayuppy.js
+++ scripts/06009-tiarayuppy.js
@@ -2,5 +2,6 @@ let name = "Miracle Joseph";
 let hngId = "HNG-06009";
 let programming_lang = "JavaScript";
 let personal_email = "tiarayuppy@gmail.com";
-let send = (`Hello Everyone, this is ${name} with HNGi7 ID ${hngId} using ${programming_lang} for stage 2 task reach me with this email; ${email}`);
+let send = (`Hello Everyone, this is ${name} with HNGi7 ID ${hngId} using ${programming_lang} for stage 2 task reach me 
+with this email; ${personal_email}`);
 console.log (send);
