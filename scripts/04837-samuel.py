7e7a10e0d4215e8a82781a1417324ffb5d2b4fa4
oni-samuel <61780462+oni-samuel@users.noreply.github.com>
2020-06-05 12:23:15 +0100

    my hng1 task with team wonderwoman

---
+++ scripts/04837-samuel.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Hello World, this is Oni\nSamuel with HNGi7 ID HNG-04837\nusing python for stage 2 task\nonis784@gmail.com")
+
+
+# In[ ]:
+
+
+
+
