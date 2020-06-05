efc22ddd930e7357a7042ea09ba1bab7b3545d25
okorochristen <okoroaforchristian8@gmail.com>
2020-06-04 23:07:52 -0700

    updating email

--- HNG-05244.py
+++ HNG-05244.py
@@ -6,4 +6,4 @@ MyId="HNGi7 ID 05244"
 language ="Python Programming language"
 
 email ="chrisnonsookoroafor@gmail.com"
-print("Hello World ,this is ",name," with ",MyId," using for",language,"stage 2 task",email)
+print("Hello World ,this is ",name," with",MyId," using for",language,"stage 2 task",email)
