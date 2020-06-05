fa40463dc044fa9be792c7614f3b736ff85d5dee
Vivyanne-504 <66225228+Vivyanne-504@users.noreply.github.com>
2020-06-05 07:05:51 +0100

    Update 00494-vivyanne.py

--- scripts/00494-vivyanne.py
+++ scripts/00494-vivyanne.py
@@ -4,4 +4,4 @@ language= 'python '
 email= 'viviannwosu05@gmail.com'
 
 print('Hello World, this is '+ full_name + ' with HNGi7_ID ' + HNG_ID
-      +' using ' + language + 'for stage 2 task.' + email)
+      +' using ' + language + 'for stage 2 task. ' + email)
