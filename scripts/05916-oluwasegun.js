bd600afe79ceb3add60b1cdf896b0720c353c7d1
Petersheg <43689575+Petersheg@users.noreply.github.com>
2020-06-05 10:13:06 +0100

    Final pull request
    My Script failed so I modified , thanks

--- scripts/05916-oluwasegun.js
+++ scripts/05916-oluwasegun.js
@@ -1,12 +1,5 @@
-/*
-    A javascript file that output my dedails as shown bellow
-    "Hello world, this is Peter Oluwasegun with HNGi7 ID 192511 using Javascript for stage 2 task."
-*/ 
 
-function outPutDetails(name,id,lang,email){
+const name="Peter Oluwasegun",id="HNG-05916",lang="JavaScript",email="petersheg@gmail.com";
 
-    console.log(`
-        Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task ${email}.
-    `);
-}
-outPutDetails('Peter Oluwasegun','HNG-05916','JavaScript','Petersheg@gmail.com');
+console.log(`
+        Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task ${email}`);
