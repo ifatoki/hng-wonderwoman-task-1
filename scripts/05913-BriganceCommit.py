56dd6ccfe4367b26e287a5b5c0002294804b8e70
SamuelBriggs <64214300+SamuelBriggs@users.noreply.github.com>
2020-06-05 11:56:36 +0100

    Delete 05913-BriganceCommit.py

--- scripts/05913-BriganceCommit.py
+++
@@ -1,6 +0,0 @@
-
-name = "Alawode Samuel"
-hng_id = "HNG-05913"
-email = "tolulope.alawode@gmail.com"
-lang = "Python"
-print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task {}'. format(name, hng_id, lang, email))
