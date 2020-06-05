fc8368dd87a60ad0c8f2a0859f2eb071c941b9c4
Kairaba224 <kairabakeita97@gmail.com>
2020-06-05 16:05:21 +0200

    print changed

--- scripts/04373-kairabakeita.js
+++ scripts/04373-kairabakeita.js
@@ -11,12 +11,5 @@ const mail = "kairabakeita97@gmail.com"
 //language used
 let codeLang = "javascript";
 
-/*
--> print with alert to use alert box to display data
-alert(`Hello World, this is ${slackName} with HNGi7 ID ${hngId} using ${codeLang} for stage 2 task ${mail}`);
--> print with console for debugging purposes
+/*-> print with console for debugging purposes*/
 console.log(`Hello World, this is ${slackName} with HNGi7 ID ${hngId} using ${codeLang} for stage 2 task ${mail}`);
-*/
-
-//For testing purposes
-document.write(`Hello World, this is ${slackName} with HNGi7 ID ${hngId} using ${codeLang} for stage 2 task ${mail}`);
