d2adcabd065cbf19e25e7941c6698b73c501ad6e
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-05 20:57:41 +0300

    Update 01008-Tolu.py

--- scripts/01008-Tolu.py
+++ scripts/01008-Tolu.py
@@ -1,1 +1,1 @@
-print("Hello world, this is Tolulope Mosuro with HNGi7 ID 01008 using Python for stage 2 task mosurotolulope@gmailcom")
+print("Hello World, this is Tolulope Mosuro with HNGi7 ID HNG-01008 using Python for stage 2 task mosurotolulope@gmailcom")
