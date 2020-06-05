97545c77aab7964b576dcb7763f36d819d77e09d
miah222222 <50325427+miah222222@users.noreply.github.com>
2020-06-05 18:09:16 +0100

    Update 06605-ameliaoneh.js

--- scripts/06605-ameliaoneh.js
+++ scripts/06605-ameliaoneh.js
@@ -2,4 +2,4 @@
 //Plain Javascript and Node.js is supported
 // html/css is not supported here 
 
-console.log("Hello world, this is Amelia Oneh with HNG ID 06605 using JS language for stage 2 task")
+console.log("Hello world, this is Amelia Oneh with HNGi7 ID 06605 using JS language for stage 2 task aimieisaac@yahoo.com")
