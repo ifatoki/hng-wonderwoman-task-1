4fa789803d7d9e6f9b3aa741d5b2d0ec04ee9185
patrickevah <65024682+patrickevah@users.noreply.github.com>
2020-06-05 14:12:08 +0100

    Update 03306-patrickevah.py

--- scripts/03306-patrickevah.py
+++ scripts/03306-patrickevah.py
@@ -1,1 +1,1 @@
-print("Hello World, this is Evah Patrick with HNGi7 ID HNG-0336 using python for stage 2 task patrickevah4@gmail.com")
+print("Hello World, this is Evah Patrick with HNGi7 ID HNG-03306 using python for stage 2 task patrickevah4@gmail.com")
