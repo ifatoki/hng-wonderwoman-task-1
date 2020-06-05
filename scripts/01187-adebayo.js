2c7a7a7da29dfb03880b4fd09d1da66c6ce5ef58
olajuwon74 <48512736+olajuwon74@users.noreply.github.com>
2020-06-05 08:07:13 +0100

    added email to display message

--- scripts/01187-adebayo.js
+++ scripts/01187-adebayo.js
@@ -1,1 +1,1 @@
-Console.log("Hello world!, I am Adebayo Solomon with HNG internship code HNG-01187 and I would be using Javascript for my stage two task");
+console.log("Hello world!, I am Adebayo Solomon with HNG internship code HNG-01187 and email adebayosolomon74@gmail.com. I will be using Javascript for my stage two task");
