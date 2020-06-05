4d1b5e60be5a84ec8c4b9823e44eaab9cc4f06c7
tiarayuppy <38207742+tiarayuppy@users.noreply.github.com>
2020-06-05 19:24:03 +0100

    pushed_script

---
+++ scripts/06009-tiarayuppy.js
@@ -0,0 +1,6 @@
+let name = "Miracle Joseph";
+let hngId = "HNG-06009";
+let programming_lang = "JavaScript";
+let personal_email = "tiarayuppy@gmail.com";
+let send = (`Hello Everyone, this is ${name} with HNGi7 ID ${hngId} using ${programming_lang} for stage 2 task reach me with this email; ${email}`);
+console.log (send);
