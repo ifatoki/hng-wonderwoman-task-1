15a5c531c7d5f1d71abd3d15a73037686534b6f6
walikoo <walikosichinga@gmail.com>
2020-06-05 11:23:52 +0200

    Create 03968-Waliko.js

---
+++ scripts/03968-Waliko.js
@@ -0,0 +1,6 @@
+var name = "Walikonadi Sichinga";
+var ID = "HNG-03968";
+var coding_language = "javascript";
+var emailaddress = "likosichinga@yahoo.com";
+
+console.log ("Hello World, this is " + name + " with HNGi7 " + ID + " using " + coding_language + " for stage 2 task " + emailaddress);
