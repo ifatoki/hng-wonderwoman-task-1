f37c3907766d7673753c568bc8523e93ac4fdbd2
miah222222 <50325427+miah222222@users.noreply.github.com>
2020-06-05 11:24:21 +0100

    Add files via upload

---
+++ scripts/06605-ameliaoneh.js
@@ -0,0 +1,5 @@
+//Node.js 10.14.0
+//Plain Javascript and Node.js is supported
+// html/css is not supported here 
+
+console.log("Hello world, this is Amelia Oneh with HNG ID 06605 using JS language for stage 2 task")
