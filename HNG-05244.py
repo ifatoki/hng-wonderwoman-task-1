32a4f1daad34d59bda43f459aa7e4955c01aab25
okorochristen <okoroaforchristian8@gmail.com>
2020-06-04 23:37:34 -0700

    updating email

--- HNG-05244.py
+++ HNG-05244.py
@@ -1,3 +1,3 @@
 # a program that displays name, Id language email
 
-print("Hello World, this is Okoroafor Christian with HNGi7 ID 05244 using Python Programming language for, stage2 task chrisnonsookoroafor@gmail.com.")
+print("Hello World, this is Okoroafor Christian with HNGi7 ID 05244 using Python Programming language for stage 2 task chrisnonsookoroafor@gmail.com")
