da040fc9501340e48f355f78aa3049f9361cf2d7
Esther955 <66220349+Esther955@users.noreply.github.com>
2020-06-05 17:46:21 +0100

    Add files via upload
    HNG-02194-ESTHER955
    MY PYTHON PROJECT

---
+++ HNG-02194-Esther955.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {
+    "scrolled": true
+   },
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'Hello World, this is Esther Adesunloye with HNGi7 ID HNG-02194 using python for stage 2 task with email as estheradesunloye96@gmail.com'"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "'Hello World, this is Esther Adesunloye with HNGi7 ID HNG-02194 using python for stage 2 task with email as estheradesunloye96@gmail.com'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
