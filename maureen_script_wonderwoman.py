9364737d0f2170754131f9cadbce6a224eefe03c
oluwamaureen <66250841+oluwamaureen@users.noreply.github.com>
2020-06-05 15:59:30 +0100

    Update and rename maureen_script_wonderwoman.py to 01726-maureen.py

--- maureen_script_wonderwoman.py
+++
@@ -1,6 +0,0 @@
-full_name = "Maureen Onabajo"
-HNGi_ID = "01726"
-language_used = "Python"
-email = "isetire@gmail.com"
-msg = f'Hello world, this is {full_name} with HNGi7 ID {HNGi_ID} using {language_used} for stage 2 task. Email address- {email}'
-print(msg)
