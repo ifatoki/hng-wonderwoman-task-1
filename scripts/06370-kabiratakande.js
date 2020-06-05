23452f2802c5c0cba903896b54e01aa8c9af1123
Kabiratakande <66366195+kabiratakande@users.noreply.github.com>
2020-06-05 17:09:52 +0100

    Update 06370-kabiratakande.js

--- scripts/06370-kabiratakande.js
+++ scripts/06370-kabiratakande.js
@@ -6,4 +6,3 @@ const mail = 'kabiratakandefolake@gmail.com';
 
 const bio = (`Hello World, this is ${name} with HNGi7 ID ${hngid} using ${lang} for stage 2 task ${mail}`);
 console.log(bio);
-© 2020 GitHub, Inc.
