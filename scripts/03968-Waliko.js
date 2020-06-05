1851ef0d67d7ef599c74b709b52fc589468f3c8b
walikoo <walikosichinga@gmail.com>
2020-06-05 10:40:33 +0200

    Revert "Create 03968-Waliko.js"
    This reverts commit b8e1e2ec17fe9270e1d7b4230bebda8c8862a2e2.

--- scripts/03968-Waliko.js
+++
@@ -1,6 +0,0 @@
-var name = "Walikonadi Sichinga";
-var ID = "HNG-03968";
-var coding_language = "javascript";
-var emailaddress = "likosichinga@yahoo.com";
-
-console.log ("Hello World, this is " + name + " with HNGi7" + ID + " using " + coding_language + " for stage 2 task" + emailaddress);
