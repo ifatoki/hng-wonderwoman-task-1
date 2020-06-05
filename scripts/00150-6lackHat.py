e1e1db9bec1fae9639b9a18e598ec354b40eac3b
Paul Ogolla <paulotieno2@gmail.com>
2020-06-05 12:23:57 +0300

    update script

--- scripts/00150-6lackHat.py
+++ scripts/00150-6lackHat.py
@@ -1,5 +1,5 @@
 def Hello(full_name, ID, language, email):
-    return "Hello World, this is {} with HNG ID {} using {} for stage 2 task {}".format(full_name, ID, language,email)
+    return "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task {}".format(full_name, ID, language,email)
 
 if __name__ == '__main__':
     full_name = "Paul Ogolla"
