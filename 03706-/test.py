55001d0dd66e39678a04cf762edd6091b252125c
SarahAnym <66218932+SarahAnym@users.noreply.github.com>
2020-06-05 21:29:17 +0100

    Update test.py

--- 03706-/test.py
+++ 03706-/test.py
@@ -1,5 +1,12 @@
-print("Hello World, this is Sarah Anyim with HNGi7 ID 03706 using Python for stage 2 task")
-
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/SARAH AYIM/AppData/Local/Programs/Python/Python38-32/Index.php.py
+Hello World, this is Sarah Anyim with HNGi7 ID 03706 using Python for stage 2 task
+>>> 
+= RESTART: C:/Users/SARAH AYIM/AppData/Local/Programs/Python/Python38-32/Index.php.py
+Hello World, this is Sarah Anyim with HNGi7 ID 03706 using Python for stage 2 task anyimsarah@gmail.com
+>>> 
 <?php 
 $dir = getcwd(); 
 
@@ -21,4 +28,5 @@ if (is_dir($dir)){
   }
 }
 ?>
+
 
