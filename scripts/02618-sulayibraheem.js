87d28800a5c4a2344a5904e68f8e2582455a7bd9
Sule Ibrahim <sulayibraheem@gmail.com>
2020-06-05 08:30:17 +0100

    made changes to my file name.

---
+++ scripts/02618-sulayibraheem.js
@@ -0,0 +1,9 @@
+/*my details*/
+var myName = "Ibrahim Sule";
+var myHngID = "HNG-02618";
+var stage = 2;
+var programLanguage = "JavaScript";
+var myEmail = "sulayibraheem@gmail.com";
+/*console*/
+console.log(`Hello World, this is ${myName} 
+with HNGi7 ID ${myHngID} using ${programLanguage} for stage ${stage} task ${myEmail}`);
