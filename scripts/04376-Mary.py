e0331786b4dc67c87a26e3a5d05f8638c98fd112
Bilewu Mary <bilewumary@gmail.com>
2020-06-05 08:37:26 +0100

    my script with email

--- scripts/04376-Mary.py
+++ scripts/04376-Mary.py
@@ -1,5 +1,6 @@
 >>> My_name = "Mary OgoOluwa Bilewu"
 >>> Lan_used = "Python"
 >>> HNGi7_ID = "HNG-04376"
->>> 
->>> print(f"Hello World, this is {My_name} with HNGi7 ID {HNGi7_ID} using {Lan_used} for stage 2 task", flush=True)
+>>> Email = "bilewumary@gmail.com"
+
+>>> print(f"Hello World, this is {My_name} with HNGi7 ID {HNGi7_ID} using {Lan_used} for stage 2 task {Email}", flush=True)
