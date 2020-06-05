392e823885cb1e75f63580a6254e4d09aa6dd2eb
Iheanacho Amarachi Sharon <58210919+Iheanacho-ai@users.noreply.github.com>
2020-06-05 09:08:27 +0100

    Update 01096-Amara.js
    I changed return to console.log

--- scripts/01096-Amara.js
+++ scripts/01096-Amara.js
@@ -3,9 +3,11 @@ const ID = 'HNG-01096';
 const language = 'JavaScript';
 const email = 'amarachi2812@gmail.com'
 
+function greet(){
+    console.log (`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task ${email}`);
+}   
 
-const greet = () =>{
-    return `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task ${email}`;
-}
+
+
 
 greet();
