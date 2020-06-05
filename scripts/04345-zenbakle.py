0ab9eaa7d41a2194cda8fa5235a06d110efdaa24
zenbakle <zenretbakle@gmail.com>
2020-06-05 08:50:40 +0100

    zenbakle

--- scripts/04345-zenbakle.py
+++ scripts/04345-zenbakle.py
@@ -1,2 +1,2 @@
 
-print("Hello World, this is Zenret Orse Bakle with HNGi7 ID HNG-04345 using python for stage 2 task")
+print("Hello World, this is Zenret Orse Bakle with HNGi7 ID HNG-04345 using python for stage 2 task zenretbakle@gmail.com")
