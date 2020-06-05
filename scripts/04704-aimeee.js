ea69085f69fe3b209d801405c3dcfc6341f9bcad
Emidowojo <opaluwaamy@gmail.com>
2020-06-05 01:27:07 +0100

    Changed few things

--- scripts/04704-aimeee.js
+++ scripts/04704-aimeee.js
@@ -3,6 +3,6 @@ let name = "Opaluwa Emidowo-ojo";
 let id = "HNG-04704";
 let language = "Javascript";
 let email = "opaluwaamy@gmail.com";
-let output = 'Hello World, this is ${full name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}';
+let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`;
 
 console.log (output);
