ffa0c1fe60173cc4c6c4298a6cd72bef5ec8676d
Merit Oriaghemuoria <59958826+Toriiaa@users.noreply.github.com>
2020-06-05 16:12:43 +0100

    Update 00824.js

--- scripts/00824.js
+++ scripts/00824.js
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HNG PROJECT</title>
-  </head>
-  <body>
-    <script>
+
+    
       const myName = "Oriaghemuoria Merit";
       const myId = "00824";
       const myEmail = "meritoriaghe@gmail.com";
       const myLanguage = "javascript";
 
       let Merit;
-      Merit = `My name is ${myName}  and my HNG Id is ${myId}  my email is ${myEmail} i am using ${myLanguage}`;
+      Merit = `Hello World, My name is ${myName}  and my HNG Id is ${myId}  my email is ${myEmail} i am using ${myLanguage} for stage2`;
       console.log("Merit");
-      document.body.innerHTML = Merit;
-    </script>
-  </body>
-</html>
+      
+    
+  
