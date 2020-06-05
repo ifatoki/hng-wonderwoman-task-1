715199c84d0ea85c850c1730a6ef373f59348683
dinesh99639 <dinesh99639@github.com>
2020-06-05 11:13:12 +0530

    Update 06279-dinesh.js

--- scripts/06279-dinesh.js
+++ scripts/06279-dinesh.js
@@ -4,5 +4,5 @@ var id = "HNG-06279"
 var lang = "javascript";
 var email = "dinesh99639@gmail.com";
 
-var message = "Hello World, this is "+name+" "+surname+" with HNGi7 ID "+id+" using "+lang+" for stage 2 task with email "+email;
+var message = "Hello World, this is "+name+" "+surname+" with HNGi7 ID "+id+" using "+lang+" for stage 2 task "+email;
 console.log(message);
