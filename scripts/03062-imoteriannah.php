f3654bff904a3e9f05d75a2856a0635ab2633de8
Iannah Imoter Paul <imoteriannah@gmail.com>
2020-06-05 01:29:52 +0100

    Create 03062-imoteriannah.php

---
+++ scripts/03062-imoteriannah.php
@@ -0,0 +1,6 @@
+<?php 
+$name = "Iannah Imoter Paul";
+$id = "HNG-03062";
+$lan = "PHP";
+echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$lan} for stage 2 task imoteriannah@gmail.com";
+ ?>	
