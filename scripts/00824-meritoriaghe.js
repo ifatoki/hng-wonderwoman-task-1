3ee8e3a6c58f40250e792659165d59cd9cd39dd1
Merit Oriaghemuoria <59958826+Toriiaa@users.noreply.github.com>
2020-06-05 16:22:11 +0100

    Update 00824-meritoriaghe.js

--- scripts/00824-meritoriaghe.js
+++ scripts/00824-meritoriaghe.js
@@ -1,12 +1,12 @@
 
     
       const myName = "Oriaghemuoria Merit";
-      const myId = "00824";
+      const myId = "HNG-00824";
       const myEmail = "meritoriaghe@gmail.com";
       const myLanguage = "javascript";
 
       let Merit;
-      Merit = `Hello World, My name is ${myName}  and my HNG Id is ${myId}  my email is ${myEmail} i am using ${myLanguage} for stage2`;
+      Merit = `Hello World, My name is ${myName}  and my HNGi7 ID is ${myId}  my email is ${myEmail} i am using ${myLanguage} for stage2`;
       console.log("Merit");
       
     
