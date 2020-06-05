060a6936189c153be46e7e70c57c5455be141775
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-05 21:59:47 +0300

    Update 04192-oloye.py

--- scripts/04192-oloye.py
+++ scripts/04192-oloye.py
@@ -3,5 +3,5 @@ internship_id = "HNG-04192"
 language = "python"
 email = "oyenimiercy@gmail.com"
 print(
-   f"Hello World, this is {full_name} with HNGi7 ID {internship_id} using {language} for task 2 {email}."
+   f"Hello World, this is {full_name} with HNGi7 ID {internship_id} using {language} for task 2 {email}"
 )
