9be99568f307a16281820eb7d60b5a7b804f8be5
Wisdom Obinna <39682482+zibest@users.noreply.github.com>
2020-06-05 09:25:41 +0100

    Delete HNG-05481-wisdom.py

--- scripts/HNG-05481-wisdom.py
+++
@@ -1,7 +0,0 @@
-
-full_name = "WISDOM OBINNA"
-HNGi7_ID = "HNG-05481"
-email = "kcwisdom7@gmail.com"
-language = "Python"
-
-print("Hello World, this is", full_name, "with HNGi7 ID", HNGi7_ID, "using", language, "for stage 2 task", email)
