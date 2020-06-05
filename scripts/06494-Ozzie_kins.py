b825263083ec77b2c6a4d3d7246389eb4b3b34a3
Ozziekins <nenubariozioma@gmail.com>
2020-06-05 22:22:43 +0300

    06494-Ozzie_kins

--- scripts/06494-Ozzie_kins.py
+++ scripts/06494-Ozzie_kins.py
@@ -1,1 +1,1 @@
-print("Hello World, this is Ozioma Okonicha with HNGi7 ID 06494 using python for stage 2 task nenubariozioma@gmail.com")
+print("Hello World, this is Ozioma Okonicha with HNGi7 ID HNG-06494 using python for stage 2 task nenubariozioma@gmail.com")
