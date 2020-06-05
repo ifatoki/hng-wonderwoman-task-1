93f9727cb3d578c49cb8495330dd23d10a2d0d52
Amadikwa Joy N <treasurechristain@gmail.com>
2020-06-05 15:28:01 +0100

    HNG-03437-sweetheart.php removed php tag
    Forgot to remove the php tag inside the php script

--- scripts/HNG-03437-sweetheart.php
+++ scripts/HNG-03437-sweetheart.php
@@ -1,12 +1,9 @@
-
-<?php
-            $fullname = "Amadikwa Joy";
-            $id = "HNG-03437";
-            $language = "PHP";
-            $email = "amadikwajoyn@gmail.com"
+$fullname = "Amadikwa Joy";
+$id = "HNG-03437";
+$language = "PHP";
+$email = "amadikwajoyn@gmail.com"
             
                 
-            echo("Hello World, this is ".$fullname." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email);
-           ?>
+echo("Hello World, this is ".$fullname." with HNGi7 ID ".$id." using ".$language." for stage 2 task ".$email);
            
 
