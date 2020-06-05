655bea68c88e3f0771f20cf5c55188be078d744c
Merit Oriaghemuoria <59958826+Toriiaa@users.noreply.github.com>
2020-06-05 15:50:15 +0100

    Create 00824.js

---
+++ scripts/00824.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HNG PROJECT</title>
+  </head>
+  <body>
+    <script>
+      const myName = "Oriaghemuoria Merit";
+      const myId = "00824";
+      const myEmail = "meritoriaghe@gmail.com";
+      const myLanguage = "javascript";
+
+      let Merit;
+      Merit = `My name is ${myName}  and my HNG Id is ${myId}  my email is ${myEmail} i am using ${myLanguage}`;
+      console.log("Merit");
+      document.body.innerHTML = Merit;
+    </script>
+  </body>
+</html>
