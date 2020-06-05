e1b939f729d0009e93002b0b33f53b1ca36bb171
isane-hash <60034249+isane-hash@users.noreply.github.com>
2020-06-05 16:02:46 +0200

    Create 04090-Isane.js

---
+++ scripts/04090-Isane.js
@@ -0,0 +1,7 @@
+var Name = "Isane Mphela";
+var ID = "HNG-04090";
+var Used_Language = "javascript";
+var Task = 2;
+var Email = "isanejossy@gmail.com";
+
+console.log(`Hello World, this is ${Name} with HNGi7 ID ${ID} using ${Used_Language} for stage ${Task} task ${Email}`);
