4701d82ddc995ebd34e7e070c64b1684a0554fa7
Damiilol <66218754+Damiilol@users.noreply.github.com>
2020-06-05 09:03:58 +0100

    Create 01378-Oluwadamilola.js

---
+++ scripts/01378-Oluwadamilola.js
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<script type="text/javascript">
+	document.writeln("Hello world,this is Oluwadamilola with HNGi7 ID HNG-01378 using javascript for stage 2 task oluwadamilolaadejumo10@gmail.com.");
+</script>
+</body>
+</html>
