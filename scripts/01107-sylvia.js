69046d5e6a7bc80e2cca50c0cb3194e40f73e15d
sylviapsnkosi <66219585+sylviapsnkosi@users.noreply.github.com>
2020-06-05 17:31:57 +0200

    Update 01107-sylvia.js
    Changes made

--- scripts/01107-sylvia.js
+++ scripts/01107-sylvia.js
@@ -1,1 +1,1 @@
-Console.log("Hello World, this is Sylvia Nkosi with HNGi7 ID HNG-01107 using javascript for stage 2 task sylviapsnkosi@gmail.com")
+console.log(' Hello World, this is Sylvia Nkosi with HNGi7 ID HNG-01107 using javascript for stage 2 task sylviapsnkosi@gmail.com ')
