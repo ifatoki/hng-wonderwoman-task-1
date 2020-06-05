4b6a88182f10e52bf64240af47361810ffdc8c2d
egjallow10 <egjallow10@gmail.com>
2020-06-05 08:20:58 +0000

    My task 2 commit

---
+++ scripts/03843-ejallow.js
@@ -0,0 +1,7 @@
+let string = "Hello World, this is Ebrima G. Jallow with HNGi7 ID HNG-03843 using javascript for stage 2 task egjallow10@gmail.com";
+
+function taskTow(name){
+    console.log(name);
+}
+
+taskTow(string);
