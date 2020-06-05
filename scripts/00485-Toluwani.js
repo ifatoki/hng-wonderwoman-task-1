670a0a24a116e7200b5484829ffb5915254a4587
elemson <toluwhani@gmail.com>
2020-06-05 19:12:10 +0300

    commit by Toluwani elemosho

---
+++ scripts/00485-Toluwani.js
@@ -0,0 +1,14 @@
+const firstname = "Toluwani";
+const surname = "Elemosho";
+const fullname = `${firstname} ${surname}`;
+const id = "HNG-00485";
+const language = "Javascript";
+const email = "toluwanielemosho@gmail.com";
+
+const firstTask = (name, id, language) => {
+  console.log(
+    `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`
+  );
+};
+
+firstTask(fullname, id, language);
