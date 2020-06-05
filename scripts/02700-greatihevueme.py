484d1d30d3fe17f52e36a88e6b3fcf58b7b58619
Great <56001640+Great-droid@users.noreply.github.com>
2020-06-05 12:17:17 +0100

    Create 02700-greatihevueme.py
    Updated

---
+++ scripts/02700-greatihevueme.py
@@ -0,0 +1,7 @@
+full_name = 'great ihevueme'
+hng_id = 'HNG-02700'
+lang = 'python'
+email = 'ihevuemeg@gmail.com'
+details = ('Hello world, this is ' + full_name.title() + ' with HNGi7 ID ' + hng_id + 
+' using ' + lang + ' for stage 2 task ' + email)
+print(details)
