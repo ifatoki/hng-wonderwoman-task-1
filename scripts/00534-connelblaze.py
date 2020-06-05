c1fc43977773388afc3607c50795b6e82edbde3a
connelevalsam <connelblaze@gmail.com>
2020-06-05 15:10:21 +0100

    including connelblaze's file

---
+++ scripts/00534-connelblaze.py
@@ -0,0 +1,8 @@
+fullname = "Connel Evalsam Asikong"
+hng_id = "00534"
+lang = "Python"
+
+def hello():
+    print("Hello World, this is " +fullname+ " with HNGi7 ID " +hng_id+ " using " +lang+ " for stage 2 task")
+
+hello()
