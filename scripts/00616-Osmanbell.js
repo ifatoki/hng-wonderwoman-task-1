8c132d3f22a5b8fb1828f673a8d4d1cce3ac0498
Bello Usman <60566011+Osmanbell@users.noreply.github.com>
2020-06-05 02:51:47 +0100

    my hng7i task done

---
+++ scripts/00616-Osmanbell.js
@@ -0,0 +1,11 @@
+const name = "Bello Usman Abiodun";
+const id = "HNG-00616";
+const language = "Javascript";
+const email = "husmanbell@gmail.com";
+
+function task(name,id,language,email){
+
+   
+   console.log (`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`)
+};
+task(name,id,language,email)
