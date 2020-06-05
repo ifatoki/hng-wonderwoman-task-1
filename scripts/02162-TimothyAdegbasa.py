2e4d28e6deade48a0b626c3ff0026deef592782c
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-05 20:37:09 +0300

    Updated file

--- scripts/02162-TimothyAdegbasa.py
+++ scripts/02162-TimothyAdegbasa.py
@@ -2,4 +2,4 @@ name= 'Timothy Adegbasa'
 id= 'HNG-02162'
 language= 'python'
 email= 'timothyadegbasa@gmail.com'
-print('Hello world, this is {} with HNGi7 ID {} using {} for stage 2 task {}'.format(name,id,language,email))
+print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task {}'.format(name,id,language,email))
