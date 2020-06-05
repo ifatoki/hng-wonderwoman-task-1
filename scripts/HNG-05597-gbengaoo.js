8fcbbe770bce1029fbb25fb39a020b1d9d0bf3c7
Gbenga Odedele <odedeleg@gmail.com>
2020-06-05 05:52:40 -0500

    changes made

--- scripts/HNG-05597-gbengaoo.js
+++ scripts/HNG-05597-gbengaoo.js
@@ -3,7 +3,7 @@ const lastname = "Odedele";
 const id = "HNG-05597";
 const language = "javascript";
 const email = "odedeleg@gmail.com";
+const output =
+  "Hello World, this is ${firstname} ${lastname} with HNGi7 ID ${id} using ${language} for stage2 task ${email}";
 
-console.log(
-  "Hello World, this is ${firstname} ${lastname} with HNGi7 ID ${id} using ${language} for stage2 task ${email}"
-);
+console.log(output);
