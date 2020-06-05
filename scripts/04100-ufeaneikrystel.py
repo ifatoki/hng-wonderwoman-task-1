428d633e2e82ea0e143e9e4c7744232a9d42adad
krystufe <krysdom5@gmail.com>
2020-06-05 06:35:24 +0100

    task 1

--- scripts/04100-ufeaneikrystel.py
+++ scripts/04100-ufeaneikrystel.py
@@ -7,5 +7,5 @@ internID = "HNG-04100"
 language = "Python"
 email_address = "krystelufeanei@gmail.com"
 
-print("Hello World, this is % s % s with HNGi7 ID % s using % s for stage 2 task. % s" 
+print("Hello World, this is % s % s with HNGi7 ID % s using % s for stage 2 task % s" 
 % (firstName, lastName, internID, language, email_address))
