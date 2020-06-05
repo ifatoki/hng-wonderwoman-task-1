7f38394488960a8722ba1354b8b9f3caff301774
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-05 21:10:23 +0300

    Delete codyrot.js

--- 00213-codyrot.js
+++
@@ -1,7 +0,0 @@
-var myFullName = 'John Rotgak';
-var myHngId = 'HNG-00213';
-var myProgLanguage = 'javascript';
-var myEmail = 'info2rotgak@gmail.com'
-var introMessage = 'Hello World, this is ' + myFullName + ' with HNGi7 ID ' + myHngId + ' using ' + myProgLanguage + ' for stage 2 task ' + myEmail;
-
-console.log(introMessage);
