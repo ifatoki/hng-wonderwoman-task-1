4a89fa7ce007d6083fd56cf6acb25ddad2347e05
Blossom-Babs <saphashb@gmail.com>
2020-06-05 08:47:23 +0100

    commit to remove full stop

--- scripts/05511-Blossom.js
+++ scripts/05511-Blossom.js
@@ -4,5 +4,5 @@ const languageUsed = "Javascript";
 const emailAddress = "saphashb@gmail.com"
 
 console.log(`
-Hello World, this is ${myFullName} with HNGi7 ID ${myID}, using ${languageUsed} for stage 2 task. ${emailAddress}
+Hello World, this is ${myFullName} with HNGi7 ID ${myID}, using ${languageUsed} for stage 2 task ${emailAddress}
 `);
