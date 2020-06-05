fd97c4f555f6d20d2a6d1dcf35a6c669a1a8b016
SamuelBriggs <64214300+SamuelBriggs@users.noreply.github.com>
2020-06-05 12:02:01 +0100

    Update 05913-Brigance.py

--- scripts/05913-Brigance.py
+++ scripts/05913-Brigance.py
@@ -1,5 +1,5 @@
-name = "Alawode Tolulope Samuel"
+name = "Alawode Samuel"
 hng_id = "HNG-05913"
 email = "tolulope.alawode@gmail.com"
 lang = "Python"
-print('Hello World, this is {} with HNG i7 ID {} using {} for stage 2 task {}'. format(name, hng_id, lang, email))
+print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task {}'. format(name, hng_id, lang, email))
