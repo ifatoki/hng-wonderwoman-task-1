a53cf59d4b930c12d07b5648fb4e0b102cb031fe
dijayst <45679945+dijayst@users.noreply.github.com>
2020-06-05 16:02:10 +0100

    my correction

--- scripts/03205-olubiyisther.py
+++ scripts/03205-olubiyisther.py
@@ -1,6 +1,6 @@
-hello="Hello world"
+hello="Hello World"
 name="Olubiyi Imoleayo Esther"
-hng ="HNGI7 ID HNG-03205"
+hng ="HNGi7 ID HNG-03205"
 using="python"
 mail="olubiyisther@gmail.com"
-print(hello +" this is "+ name + " with " + hng + " using " + using + " for stage 2 task " + mail )
+print(hello +", this is "+ name + " with " + hng + " using " + using + " for stage 2 task " + mail )
