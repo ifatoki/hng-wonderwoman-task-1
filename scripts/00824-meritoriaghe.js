6e1a58110e8f25578112f64404bb20d06ec2f8a7
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-05 21:19:02 +0300

    Update 00824-meritoriaghe.js

--- scripts/00824-meritoriaghe.js
+++ scripts/00824-meritoriaghe.js
@@ -1,13 +1,10 @@
+const myName = "Oriaghemuoria Merit";
+const myId = "HNG-00824";
+const myEmail = "meritoriaghe@gmail.com";
+const myLanguage = "javascript";
 
-    
-      const myName = "Oriaghemuoria Merit";
-      const myId = "HNG-00824";
-      const myEmail = "meritoriaghe@gmail.com";
-      const myLanguage = "javascript";
-
-      let Merit;
-      Merit = `Hello World, this is ${myName} with HNGi7 ID ${myId} using ${myLanguage} for stage2 task ${myEmail}`;
-      console.log(Merit);
-      
+let Merit;
+Merit = `Hello World, this is ${myName} with HNGi7 ID ${myId} using ${myLanguage} for stage2 task ${myEmail}`;
+console.log(Merit);
     
   
