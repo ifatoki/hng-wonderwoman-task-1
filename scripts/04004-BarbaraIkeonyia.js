6eba3c23c2c181c271cdefaedda27d6e632af69c
BarbaraIkeonyia <ikeonyiaijeoma@gmail.com>
2020-06-05 17:04:34 +0100

    my script file

---
+++ scripts/04004-BarbaraIkeonyia.js
@@ -0,0 +1,7 @@
+const Name = 'Ikeonyia Barbara Ijeoma';
+const id = 'HNG-04004';
+const email = 'ikeonyiaijeoma@gmail.com';
+const lang = 'javascript';
+
+console.log(`Hello World, this is ${Name} with HNGi7 id ${id} using ${lang} for stage 2 task ${email}`);
+
