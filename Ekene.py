6f9eab82aa172670f4adff6942ee1bfeeb05524d
EkeneU <62336228+EkeneU@users.noreply.github.com>
2020-06-06 00:12:27 +0100

    Add files via upload

---
+++ Ekene.py
@@ -0,0 +1,8 @@
+def HNG_project():
+    name = 'Ekene uzodinma'
+    HNG_ID = '03096'
+    lang= 'Python'
+    print('Hello world, this is ' + name + ' with HNGi7 ID ' + HNG_ID + ' using ' + lang + ' for stage 2 task.')
+
+HNG_project()
+
