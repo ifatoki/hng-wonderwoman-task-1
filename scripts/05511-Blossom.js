e9a8fce12700c3c989d9a75e575d2df194537dcd
Blossom-Babs <saphashb@gmail.com>
2020-06-05 12:05:03 +0100

    commas removed

--- scripts/05511-Blossom.js
+++ scripts/05511-Blossom.js
@@ -4,4 +4,4 @@ const language = "Javascript";
 const email = "saphashb@gmail.com";
 
 console.log(`
-Hello World, this is ${name} with HNGi7 ID ${id}, using ${language} for stage 2 task ${email}`);
+Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`);
