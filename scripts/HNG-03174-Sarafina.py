f10711f9ffb2c11e23ed03ca24880ee65a5d22c3
Sara-ndeshi <66229021+Sara-ndeshi@users.noreply.github.com>
2020-06-06 08:49:11 +0200

    HNG-03174-Sarafina

---
+++ scripts/HNG-03174-Sarafina.py
@@ -0,0 +1,5 @@
+fullName = "Sarafina Weyulu"
+ID = "HNG-03174"
+email = "sarafinandeshy@gmail.com"
+lang = "python"
+print("Hello World, this is " + fullName + " with HNGi7 ID " + ID + " using " + lang + " for stage 2 task " + email)
