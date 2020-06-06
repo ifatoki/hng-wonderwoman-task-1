0ec077bb6a9ecd99f6e6c171c1ddffd3c2fac048
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 09:12:20 +0300

    Update and move

---
+++ scripts/01944-jared.js
@@ -0,0 +1,8 @@
+//variables!
+const full_name='Jared Okumu';
+const id='HNG-01944';
+const lang='Javascript';
+const email='okumujaro@gmail.com';
+
+//output!
+console.log('Hello World,this is ' + full_name + 'with HNGi7 ID ' + id + ' using ' + lang + 'for stage two tusk ' +email);
