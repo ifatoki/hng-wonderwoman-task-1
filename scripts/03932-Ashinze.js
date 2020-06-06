bca6147fe14b99a451c77ecaf6255f91f8d09fc5
Mystique1337 <66253355+Mystique1337@users.noreply.github.com>
2020-06-06 14:51:38 +0100

    Update 03932-Ashinze.js

--- scripts/03932-Ashinze.js
+++ scripts/03932-Ashinze.js
@@ -1,6 +1,7 @@
 let name = "Ashinze Emmanuel";
 let HNGID = "03932";
+let email= "ashinzechidi@gmail.com" ;
 
 console.log(
-  `Hello World, this is ${name} with HNGi7 ID ${HNGID} using JavaScript for stage 2 task`
+  `Hello World, this is ${name} with HNGi7 ID ${HNGID} using JavaScript for stage 2 task ${email}`
 );
