e5f34425c0577ee5fca07fdb352bfe5b9d0c04e0
Adebola <onedebos@gmail.com>
2020-06-06 21:42:55 +0100

    Revert "06118-asif0010.js"

--- scripts/06118-asif0010.js
+++
@@ -1,9 +0,0 @@
-/* Task 1 solution */
-
-  var fullName="Ogunwole Olusegun James";
-  var hngId="HNG-06118";
-  var taskStage=2;
-  var technologyUsed="JavaScript";
-  var email="ogunwolejamesolusegun1@gmail.com";
-  
-  console.log("Hello World, this is " + fullname + " "  + "with HNGi7 ID " +  hngId + " using " + technologyUsed + " for stage " + taskStage + " task " + email);
