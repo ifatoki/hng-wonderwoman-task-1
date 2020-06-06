e7689597910af207d8288238270ceeec93e271f9
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 12:33:29 +0300

    Updated 05643-babarich.php

--- scripts/05643-babarich.php
+++ scripts/05643-babarich.php
@@ -4,13 +4,6 @@ $id="HNG-05643";
 $email="richnaldcharles1995@gmail.com";
 $language="PHP";
 
-echo "Hello World,"." "."this is"." " . $fullname ." ". "with HNGi7 ID"." " .$id." "."and email"." ".$email." "."using"." ".$language." "."for stage 2 task";
-
-
-
-
-
-
-
+echo "Hello World,"." "."this is"." " . $fullname ." ". "with HNGi7 ID"." " .$id." "."using"." ".$language." "."for stage 2 task ".$email;
 
 ?>
