77a618f5b606adda62d05b56f95cadd9a29cc056
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 16:07:27 +0300

    Update 02561-Tesfamariam.py

--- scripts/HNG-02561-Tesfamariam.py
+++
@@ -1,7 +0,0 @@
-firstName="Tesfamariam"
-lastName="Teklemariam"
-ID ="HNG-02561"
-language="Python"
-email="wegemuluneh@gmail.com"
-
-print("Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}" .format (firstName, lastName, ID, language, email), flush=True)
