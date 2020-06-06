f3d62ecfcca052423890db3cc9bd994482fcdd63
DayoJigida <66283242+DayoJigida@users.noreply.github.com>
2020-06-06 13:42:08 +0100

    my stage 2 task

---
+++ scripts/05115-DayoJigida.js
@@ -0,0 +1,6 @@
+var internName = "Adedayo Jigida"
+var HngID = "HNG-05115"
+var programmingLanguage = "javascript"
+var email = "dayojigida@gmail.com"
+
+console.log(` Hello world, this is ${internName} with HNGi7 ID ${HngID} using language ${programmingLanguage} for stage 2 task ${email} `)
