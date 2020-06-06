bf8d73aee32f6c3612bbc8595173f4a2e3caeaac
hallyG <66381325+peggylicious97@users.noreply.github.com>
2020-06-06 16:02:55 +0100

    Create 06527-gracehalliday.py

---
+++ scripts/06527-gracehalliday.py
@@ -0,0 +1,6 @@
+#this is gracehallidays  scripts 
+
+name=grace halliday
+Hngid=HNG-06527
+email=gracehally7@gmail.com
+Print("Hello world,this is",name,"with HNG ID",Hngid,"email address",email,"using python for Task 2")
