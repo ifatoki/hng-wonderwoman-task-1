acdebeede1094cef6e8d9d2223bcd4e633c76127
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 13:00:19 +0300

    Deleted 02373-Daniella.py

--- 02373-Daniella.py
+++
@@ -1,5 +0,0 @@
-name =  'Daniella Mkpandiok'
-id= 'HNG-02573'
-Language ='Python'
-email = 'daniellaamba01@gmail.com'
-print( 'Hello World, this is' + ' ' + name +' ' + 'with HNGi7 ID' + ' ' + id + ' ' +' using' + ' ' + Language + ' ' + 'for stage 2 task.')
