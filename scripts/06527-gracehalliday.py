f17720e0c9a5c4224a00b80833416b18d73660cb
hallyG <66381325+peggylicious97@users.noreply.github.com>
2020-06-06 21:46:07 +0100

    Update 06527-gracehalliday.py

--- scripts/06527-gracehalliday.py
+++ scripts/06527-gracehalliday.py
@@ -1,4 +1,4 @@
-name="grace halliday"
-email="gracehally7@gmail.com"
-Hngid="HNG-06527"
-Print("Hello World,this is",name,"with HNG ID",Hngid,"using python for stage2 task",email)
+name='grace halliday'
+email='gracehally7@gmail.com'
+Hngid='HNG-06527'
+Print('Hello World,this is',name,'with HNG ID',Hngid,'using python for stage 2 task',email)
