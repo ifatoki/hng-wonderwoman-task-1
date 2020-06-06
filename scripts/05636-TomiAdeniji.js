4c225e89a5caf93d519742a5df75c280aba5e0d6
TomiAdeniji <tominiji17@gmail.com>
2020-06-06 13:36:12 +0100

    Update 05636-TomiAdeniji.js

--- scripts/05636-TomiAdeniji.js
+++ scripts/05636-TomiAdeniji.js
@@ -1,6 +1,6 @@
 let name = 'Ifeoluwatomi Adeniji';
 const id = 'HNG-05636';
-let email = 'ifeoluwatomi@gmail.com'
+let email = 'ifeoluwatomi.adeniji@gmail.com'
 function taskOne(){
   console.log(
     `Hello World, this is ${name} with HNGi7 ID ${id} using javascript for stage 2 task ${email}`
