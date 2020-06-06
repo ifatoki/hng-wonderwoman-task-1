a463414d1f61acedc1ca8b279f7e345d30348578
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 09:20:01 +0300

    Updated and moved 04686-ubani.js

--- scripts/04686-ubani.js
+++ scripts/04686-ubani.js
@@ -6,9 +6,8 @@ const HNG_id='HNG-04686';
 const email='dimowoisaac@yahoo.com'
 
 function outputToConsole() {
-	const output = `Hello World, this is [${firstname}] [${middename}] [${surname}] 
-	with HNG7_id [${HNG_id}] using [${language}] for stage two task. 
-	${email}`;
+	const output = `Hello World, this is ${firstname} ${middlename} ${surname} with HNGi7 ID ${HNG_id} using ${language} for stage 2 task ${email}` ;
 	return output;
 };
+
 console.log(outputToConsole());
