ed0bef8e278c6a2716037ea9b9dd31522e8bb99c
Carol Musyoka <carolmusyoka127@gmail.com>
2020-06-06 18:04:28 +0300

    Create 01810-Carol.py

---
+++ scripts/01810-Carol.py
@@ -0,0 +1,6 @@
+name = "Carol Musyoka"
+ID = "HNG-01210"
+language = "python"
+email = "carolmusyoka@gmail.com"
+
+print("Hello World, this is " + name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task " + email )
