97fab2d757594d971b0c92feee44d997fbf68350
okumu <66220008+okumu@users.noreply.github.com>
2020-06-06 16:53:50 +0300

    Create 01944-jared.js

---
+++ scripts/01944-jared.js
@@ -0,0 +1,6 @@
+const FULLNAME='Jared Okumu';
+const HNGi7ID='HNG_01944';
+const LANGUAGE='Javascript';
+const EMAILADDRESS='okumujaro@gmail.com';
+
+console.log('Hello World, this is ' + FULLNAME + ' with HNGi7ID ' + HNGi7ID + ' using ' + LANGUAGE + ' for stage 2 task ' + EMAILADDRESS);
