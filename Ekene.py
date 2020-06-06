03ae5503c065d0c9549a3323fc3a5eff0012b63f
EkeneU <62336228+EkeneU@users.noreply.github.com>
2020-06-06 01:12:04 +0100

    Add files via upload

--- Ekene.py
+++ Ekene.py
@@ -2,7 +2,9 @@ def HNG_project():
     name = 'Ekene uzodinma'
     HNG_ID = '03096'
     lang= 'Python'
-    print('Hello world, this is ' + name + ' with HNGi7 ID ' + HNG_ID + ' using ' + lang + ' for stage 2 task.')
+    email_address='ekeneuzodinma@gmail.com'
+    print('Hello world, this is ' + name + ' with HNGi7 ID ' + HNG_ID + ' using ' + lang + ' for stage 2 task'
+          + email_address)
 
 HNG_project()
 
