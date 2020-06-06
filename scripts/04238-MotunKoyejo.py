3afbc28130519a1f091ebd44fff187bccc073021
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2020-06-06 14:43:03 +0100

    stage 2 task

---
+++ scripts/04238-MotunKoyejo.py
@@ -0,0 +1,5 @@
+full_name = 'Motunrayo Koyejo'
+ID = 'HNG-04238'
+email = 'koyejomotunrayo@gmail.com'
+
+print('Hello World, this is Motunrayo Koyejo with HNGi7 ID', ID, 'using python for stage 2 task', email) 
