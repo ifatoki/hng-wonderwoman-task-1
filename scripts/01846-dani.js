2895bd57371e21a21a23825cf7f390c2a54cf92a
AnyaDaniel <30692059+AnyaDaniel@users.noreply.github.com>
2020-06-06 08:46:36 +0100

    Update and rename Dani_script.js to 01846-dani.js

---
+++ scripts/01846-dani.js
@@ -0,0 +1,11 @@
+
+
+var myfullName = 'Daniel Anya';
+var myID = 'HNG-01846';
+var mylanguage ='javascript';
+Var myemail= 'anyadaniel1@gmail.com' 
+var myintro ='Hello World, this is ' + myfullName +  ' with HNGi7 ID '+ myID  + ' using ' + mylanguage + ' for stage 2 task ' + myemail;
+
+console.log(myintro);
+
+
--- scripts/Dani_script.js
+++
@@ -1,6 +0,0 @@
-
-<script>
-
-alert('Hello World, This is Daniel Anya with HNGi7 ID: HNG-01846 using Javascript for stage 2 task');
-
-</script>
