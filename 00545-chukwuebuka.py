e401afaba637f1e93ae8dfa9536ebcb72db832f1
chukwuebuka35 <66219201+chukwuebuka35@users.noreply.github.com>
2020-06-06 07:23:37 +0100

    00545-chukwuebuka
    00545-chukwuebuka task 2

---
+++ 00545-chukwuebuka.py
@@ -0,0 +1,6 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World, This is Chukwuebuka  Nnakwe with HNGi7 ID HNG-00545 using python for stage 2 task chukwuebuka.nnakwe@gmail.com")
+#it is really fustrating not knowing about python yet
+Hello World, This is Chukwuebuka  Nnakwe with HNGi7 ID HNG-00545 using python for stage 2 task chukwuebuka.nnakwe@gmail.com
+>>>    
