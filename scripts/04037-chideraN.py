9be6a224400402367eceec9468bb156d38068aba
chideraN <66258839+chideraN@users.noreply.github.com>
2020-06-06 20:32:21 +0100

    Update 04037-chideraN.py

--- scripts/04037-chideraN.py
+++ scripts/04037-chideraN.py
@@ -1,5 +1,5 @@
 name="chidera nnamadim"
-ID="HNG-04037
+ID="HNG"-04037
 language="python"
 email="bolunnamadim@gmail.com"
 print ("hello world, this is "+name+" with HNG7id "+ID+" using "+language+" for stage 2 task "+email)
