43fe5ac1b1fc03b68103330f3481e74a1f2cc8e3
Neatwhiskey <44437144+Neatwhiskey@users.noreply.github.com>
2020-06-06 08:24:39 +0200

    Add files via upload

---
+++ jamaldeen opasina.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 20:46:10 2020
+
+@author: BURSARY
+"""
+
+def task2():
+    name= "jamaldden opasina"
+    id= " HNG-05923"
+    Email="opasinajamaldeen@gmail.com"
+    language="python"
+    
+    print("hello world, this is {} with {} and {} using {} stage 2 task".format(name,id,Email,language))
