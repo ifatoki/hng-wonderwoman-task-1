68c050fff9367de50e5d9acb0f2943b317ecabc0
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 12:38:17 +0300

    Update 02797-bluehood.js

--- scripts/02797-bluehood.js
+++ scripts/02797-bluehood.js
@@ -1,6 +1,5 @@
 const fullname = "Samuel Oluwatosin Oniyilo";
 const hng_id = "HNG-02797";
-const language = "Javascript";
+const language = "JavaScript";
 const email = "masterarcher6321@gmail.com";
-console.log("Hello world, this is " + fullname + " with HNGi7 " +
-hng_id + " using " + language + " for stage 2 task " + email);
+console.log("Hello World, this is " + fullname + " with HNGi7 " + hng_id + " using " + language + " for stage 2 task " + email);
