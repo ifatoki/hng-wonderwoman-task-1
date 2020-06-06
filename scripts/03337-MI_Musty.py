26ec2481d586c0004d4e5f4d009103643cc2dace
Elmustyy <49339501+Elmustyy@users.noreply.github.com>
2020-06-06 08:25:54 +0100

    Hng_i7task2_Update

--- scripts/03337-MI_Musty.py
+++ scripts/03337-MI_Musty.py
@@ -1,1 +1,1 @@
-Print("Hello World, this is Mustapha Muhammad Ibrahim with HNGi7 ID HNG-03337 using python for stage 2 task ibrahimmusty100@yahoo.com")
+Print("Hello world, this is Mustapha Muhammad Ibrahim with HNGi7 ID HNG-03337 using python for stage 2 task ibrahimmusty100@yahoo.com")
