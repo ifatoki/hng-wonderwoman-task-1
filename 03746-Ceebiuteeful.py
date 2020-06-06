9cc653f21fcede6c4dc718e73417602890f8bf3c
Francis O'Hara Aidoo <kofiohara@gmail.com>
2020-06-06 06:32:57 +0000

    Update and rename 03746-Ceebiuteeful.py to scripts/03746-Ceebiuteeful.py

--- 03746-Ceebiuteeful.py
+++
@@ -1,1 +0,0 @@
-print ("Hello World, this is Ezeh Chioma with HNGi7 ID HNG-03746 using python for stage 2 task cezeh187@gmail.com")                                                                                                                                           ")
---
+++ scripts/03746-Ceebiuteeful.py
@@ -0,0 +1,1 @@
+print ("Hello World, this is Ezeh Chioma with HNGi7 ID HNG-03746 using python for stage 2 task cezeh187@gmail.com")
