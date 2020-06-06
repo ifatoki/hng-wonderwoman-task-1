016a70651b176c046ff0ddda0c6e5d6a3648ba23
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 10:51:23 +0300

    Updated 01629-zainabaderinmola.py

--- scripts/01629-zainabaderinmola.py
+++ scripts/01629-zainabaderinmola.py
@@ -3,4 +3,4 @@ ID = 'HNG-01629'
 language = 'python'
 email_address = 'zainabaderinmola@gmail.com'
 
-print('Hello World, this is ', full_name,'with HNGi7 ID', ID, 'using' ,language, 'for stage 2 task', email_address)\n
+print('Hello World, this is', full_name,'with HNGi7 ID', ID, 'using' ,language, 'for stage 2 task', email_address)
