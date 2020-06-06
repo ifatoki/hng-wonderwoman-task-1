0eb4b2e32a842aa6459cdde4b8c9b27278c49912
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 12:29:43 +0300

    Updated 06182-Draco.php

--- scripts/06182-Draco.php
+++ scripts/06182-Draco.php
@@ -7,7 +7,7 @@ $email = "abdulrahmanyusuf125@gmail.com";
 
 function show_info(){ 
 	global $name, $hng_id, $language, $email;
-	return "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
+	return "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task $email";
 }
 
 echo(show_info());
