66f910a4b9cb4ff1f1f0dd1bfedcb98b395ad091
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 16:04:52 +0300

    Update 05115-DayoJigida.js

--- scripts/05115-DayoJigida.js
+++ scripts/05115-DayoJigida.js
@@ -3,4 +3,4 @@ var HngID = "HNG-05115"
 var programmingLanguage = "javascript"
 var email = "dayojigida@gmail.com"
 
-console.log(` Hello world, this is ${internName} with HNGi7 ID ${HngID} using language ${programmingLanguage} for stage 2 task ${email} `)
+console.log(`Hello World, this is ${internName} with HNGi7 ID ${HngID} using ${programmingLanguage} for stage 2 task ${email}`) 
