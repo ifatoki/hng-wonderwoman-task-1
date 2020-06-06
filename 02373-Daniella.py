ea8032255a1951a6b57c1eb5a5f56cc04ac64345
Daniella <daniellaamba01@gmail.com>
2020-06-06 10:21:34 +0100

    Create 02373-Daniella.py

---
+++ 02373-Daniella.py
@@ -0,0 +1,5 @@
+name =  'Daniella Mkpandiok'
+id= 'HNG-02573'
+Language ='Python'
+email = 'daniellaamba01@gmail.com'
+print( 'Hello World, this is' + ' ' + name +' ' + 'with HNGi7 ID' + ' ' + id + ' ' +' using' + ' ' + Language + ' ' + 'for stage 2 task.')
