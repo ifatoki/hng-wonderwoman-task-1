c06844b41142c35ab04971b276777aff3d8a661d
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 16:12:22 +0300

    Update 04021-virginiahperiah.py

--- scripts/04021-virginiahperiah.py
+++ scripts/04021-virginiahperiah.py
@@ -2,4 +2,4 @@ full_name = 'Virginiah Periah'
 hng_num = 'HNG-04021'
 used_lang = 'Python'
 email = 'virgyperry@gmail.com'
-print(f"Hello World, this is {full_name} with HNGi7 ID {hng_num}  using {used_lang} for stage 2 task. {email}")
+print(f"Hello World, this is {full_name} with HNGi7 ID {hng_num} using {used_lang} for stage 2 task {email}")
