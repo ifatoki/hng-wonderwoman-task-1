db8f4506379745d701114b2a0467a78788ab7505
Blessing Adetoye Adesiji <38926059+bleso-a@users.noreply.github.com>
2020-06-06 22:32:19 +0100

    renamed my script .py

--- scripts/ babasiji-05674.py
+++
@@ -1,7 +0,0 @@
-Name = 'Blessing Adesiji'
-HNGi7ID = 'HNG-05674'
-PreferredLanguage = 'Python'
-Stage = '2'
-email = 'blessingadesiji96@gmail.com'
-
-print(f"Hello World, this is {Name} with HNGi7 ID {HNGi7ID} using {PreferredLanguage} for stage {Stage} task {email}", flush=True)
