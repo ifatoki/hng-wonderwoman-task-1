bbb03f5e18dbf591a95e31682f22e7c73a704e58
Clinton <57812399+clinton9ice@users.noreply.github.com>
2020-06-06 12:48:46 -0700

    Update 06787-Clinton.js

--- scripts/06787-Clinton.js
+++ scripts/06787-Clinton.js
@@ -1,8 +1,7 @@
 
 var fullname = "Clinton Ufere";
-var ID = "HNG-06787"
+var ID = "HNG-06787";
 var language = "javascript";
 var email = "clintonufere@gmail.com";
-
 
 console.log("Hello World, this is " + fullname + " "  + "with HNGi7 ID " +  ID + " using " + language + " for stage 2 task " + email);
