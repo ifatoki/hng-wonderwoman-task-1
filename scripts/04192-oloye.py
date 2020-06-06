a5a6ea554a692425a792a0ab375765bc0fb986cb
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 09:24:30 +0300

    Updated 04192-oloye.py

--- scripts/04192-oloye.py
+++ scripts/04192-oloye.py
@@ -1,7 +1,8 @@
 full_name = "Mercy Oyeniran"
 internship_id = "HNG-04192"
 language = "python"
-email = "oyenimiercy@gmail.com"
+email = "oyenimercy@gmail.com"
 print(
-   f"Hello World, this is {full_name} with HNGi7 ID {internship_id} using {language} for task 2 {email}"
+   f"Hello World, this is {full_name} with HNGi7 ID {internship_id} using {language} for stage 2 task {email}."
 )
+
