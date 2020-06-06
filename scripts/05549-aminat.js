503f2bee6c70e461f00ca8b16e2b724b23d8870e
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-07 01:17:04 +0300

    Update 05549-aminat.js

--- scripts/05549-aminat.js
+++ scripts/05549-aminat.js
@@ -6,4 +6,4 @@ var language = "JavaScript";
 
 var email = "oahmedaminat@gmail.com";
 
-console.log("Hello world, this is " + name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task " + email);
+console.log("Hello World, this is " + name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task " + email);
