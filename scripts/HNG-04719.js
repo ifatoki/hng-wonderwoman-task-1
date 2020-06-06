21d5c25b4ac62e7f6674195c83e25dde2412178a
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-06 08:54:28 +0300

    Updated 2.0

--- scripts/HNG-04719.js
+++ scripts/HNG-04719.js
@@ -2,4 +2,4 @@ let name="Uzor Obinna Gbenga";
 let ID= "HNG-04719";
 let lang="JavaScript";
 let email="uzorobinna90@gmail.com"
-console.log(`Hello World this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task ${email}`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task ${email}`);
