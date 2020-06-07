7e86e153231493995792f03d5646e673df1cc280
Reemah1 <55090536+Reemah1@users.noreply.github.com>
2020-06-07 16:51:56 +0100

    Update 06651-Reemah.py

--- scripts/06651-Reemah.py
+++ scripts/06651-Reemah.py
@@ -3,4 +3,4 @@ ID = "HNG-06651"
 language = "python"
 email = "ratmat3303@gmail.com"
 
-print("Hello World, this is " + fullName + " with HNGi7 ID " + ID + " using " + language + " for stage 2" + email) 
+print("Hello World, this is " + fullName + " with HNGi7 ID " + ID + " using " + language + " for stage 2 " + email) 
