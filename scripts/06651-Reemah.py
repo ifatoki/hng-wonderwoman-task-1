a97cf996f06ae43ff9396c5636c1042766dd96fc
Reemah1 <55090536+Reemah1@users.noreply.github.com>
2020-06-07 16:51:11 +0100

    Update 06651-Reemah.py

--- scripts/06651-Reemah.py
+++ scripts/06651-Reemah.py
@@ -1,5 +1,6 @@
 fullName = "Bashar Raheemat"
 ID = "HNG-06651"
 language = "python"
+email = "ratmat3303@gmail.com"
 
-print("Hello World, this is " + fullName + " with HNGi7 ID " + ID + " using " + language + " for stage 2") 
+print("Hello World, this is " + fullName + " with HNGi7 ID " + ID + " using " + language + " for stage 2" + email) 
